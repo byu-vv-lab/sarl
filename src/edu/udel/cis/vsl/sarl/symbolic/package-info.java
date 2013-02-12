f2067d5294031f28a74aeb8131f3b90bad2a43f3
siegel <siegel@7d651925-2d04-6a0e-8c93-cd1e20bcaab1>
2013-02-12 16:31:06 +0000

    Deleting the stuff that doesn't work. This seems better than keep non-compiling code in archive.  It can always be ported from old TASS archive later.

--- src/edu/udel/cis/vsl/sarl/symbolic/package-info.java
+++
@@ -1,8 +0,0 @@
-/**
- * Provide classes to create and manipulate different kinds of symbolic
- * expressions. It also provides functionalities to convert a symbolic
- * expression into canonical form.
- * 
- * @author siegel
- */
-package edu.udel.cis.vsl.sarl.symbolic;
