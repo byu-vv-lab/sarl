7f8dcb206f68c0dfd6ae6287623ea72bb809f7e5
mbrahma <mbrahma@7d651925-2d04-6a0e-8c93-cd1e20bcaab1>
2013-09-06 20:16:34 +0000

    New test case for the Simplify Class - Marshilla Brahma

---
+++ test/edu/udel/cis/vsl/sarl/simplify/Simplifytest.java
@@ -0,0 +1,14 @@
+package edu.udel.cis.vsl.sarl.simplify;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class Simplifytest {
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
