047efaecdd412958eb77893e1a4a521bfccadb6f
mbrahma <mbrahma@7d651925-2d04-6a0e-8c93-cd1e20bcaab1>
2013-09-06 20:41:03 +0000

    Added new blank tests and comments

--- test/edu/udel/cis/vsl/sarl/simplify/Simplifytest.java
+++ test/edu/udel/cis/vsl/sarl/simplify/Simplifytest.java
@@ -4,11 +4,39 @@ import static org.junit.Assert.*;
 
 import org.junit.Test;
 
+import edu.udel.cis.vsl.sarl.IF.expr.BooleanExpression;
+import edu.udel.cis.vsl.sarl.preuniverse.IF.PreUniverse;
+
 public class Simplifytest {
 
 	@Test
 	public void test() {
 		fail("Not yet implemented");
+	}
+	
+	/**
+	 * Takes in
+	 * 	-PreUniverse universe,
+	 *	-BooleanExpression assumption
+	 *
+	 * Returns a new IdentitySimplifier
+	 */
+	
+	@Test
+	public void identitySimplifier(){
+		
+	}
+	
+	
+	/**
+	 * Takes in
+	 * 	-PreUniverse universe,
+	 *
+	 * Returns a new IdentitySimplifierFactory
+	 */
+	@Test
+	public void newIdentitySimplifierFactory(){
+		
 	}
 
 }
