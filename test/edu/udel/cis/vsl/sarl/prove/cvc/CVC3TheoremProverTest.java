007b277aa6c3d41be168e7cf8e67073f6b019268
siegel <siegel@7d651925-2d04-6a0e-8c93-cd1e20bcaab1>
2013-09-07 19:38:21 +0000

    Fixed the framework for the CVC3 theorem prover test class so that it can use an expression factory to create expressions rather than going through a PreUniverse.  This allows greater control over the kinds of expressions produced; in particular, the universe will manipulate and simplify expressions, while the expression factory wil leave them exactly as constructed.

--- test/edu/udel/cis/vsl/sarl/prove/cvc/CVC3TheoremProverTest.java
+++ test/edu/udel/cis/vsl/sarl/prove/cvc/CVC3TheoremProverTest.java
@@ -1,90 +1,111 @@
 package edu.udel.cis.vsl.sarl.prove.cvc;
 
-import static org.junit.Assert.*;
+import static org.junit.Assert.assertEquals;
 
 import java.io.PrintStream;
 
 import org.junit.After;
 import org.junit.Before;
-import org.junit.Ignore;
 import org.junit.Test;
 
 import cvc3.Expr;
-import cvc3.ValidityChecker;
 import cvc3.Type;
+import cvc3.ValidityChecker;
 import edu.udel.cis.vsl.sarl.IF.expr.BooleanExpression;
 import edu.udel.cis.vsl.sarl.IF.expr.NumericExpression;
 import edu.udel.cis.vsl.sarl.IF.expr.SymbolicExpression;
+import edu.udel.cis.vsl.sarl.IF.expr.SymbolicExpression.SymbolicOperator;
+import edu.udel.cis.vsl.sarl.IF.type.SymbolicRealType;
 import edu.udel.cis.vsl.sarl.IF.type.SymbolicType;
+import edu.udel.cis.vsl.sarl.expr.IF.ExpressionFactory;
 import edu.udel.cis.vsl.sarl.preuniverse.PreUniverses;
+import edu.udel.cis.vsl.sarl.preuniverse.IF.FactorySystem;
 import edu.udel.cis.vsl.sarl.preuniverse.IF.PreUniverse;
 import edu.udel.cis.vsl.sarl.prove.Prove;
 import edu.udel.cis.vsl.sarl.prove.IF.TheoremProverFactory;
 
 public class CVC3TheoremProverTest {
 
-	PreUniverse universe;
+	// Static fields: instantiated once and used for all tests...
 
-	@SuppressWarnings("unused")
+	private static PrintStream out = System.out;
+
+	private static FactorySystem factorySystem = PreUniverses
+			.newIdealFactorySystem();
+
+	private static PreUniverse universe = PreUniverses
+			.newPreUniverse(factorySystem);
+
+	private static ExpressionFactory expressionFactory = factorySystem
+			.expressionFactory();
+
+	private static SymbolicRealType realType = universe.realType();
+
+	private static NumericExpression two = universe.rational(2);
+
+	private static NumericExpression five = universe.rational(5);
+
+	private static BooleanExpression booleanExprTrue = universe
+			.trueExpression();
+
+	// Instance fields: instantiated before each test is run...
+
 	private TheoremProverFactory proverFactory;
-	BooleanExpression booleanExprTrue;
-	CVC3TheoremProverFactory theoremProver;
-	CVC3TheoremProver cvcProver;
-	private static PrintStream out;
-	private static NumericExpression two;
-	private static NumericExpression five;
 
+	private CVC3TheoremProver cvcProver;
+
+	private ValidityChecker vc;
+
+	/**
+	 * Set up each test. This method is run before each test.
+	 * 
+	 * @throws Exception
+	 */
 	@Before
 	public void setUp() throws Exception {
-		universe = PreUniverses.newPreUniverse(PreUniverses
-				.newIdealFactorySystem());
 		proverFactory = Prove.newCVC3TheoremProverFactory(universe);
-		booleanExprTrue = universe.bool(true);
-		theoremProver= (CVC3TheoremProverFactory)proverFactory;
-		cvcProver = (CVC3TheoremProver)theoremProver.newProver(booleanExprTrue);
-		out = System.out;
-		two = universe.rational(2);
-		five = universe.rational(5);
+		cvcProver = (CVC3TheoremProver) proverFactory
+				.newProver(booleanExprTrue);
+		vc = cvcProver.validityChecker();
 	}
 
 	@After
 	public void tearDown() throws Exception {
 	}
-	
-	
 
 	@Test
 	public void testBoolean() {
 		SymbolicType boolType = universe.booleanType();
-		ValidityChecker v = ValidityChecker.create();
 		Type t = cvcProver.translateType(boolType);
-		assertEquals(t, v.boolType());
+
+		assertEquals(t, vc.boolType());
 	}
-	
+
 	public void testQuotientRemainderPair() {
 
 		SymbolicExpression numerator = universe.rational(1);
 		SymbolicExpression denominator = universe.rational(2);
-				
-	//	cvcProver.getQuotientRemainderPair(numerator, denominator);
+
+		// cvcProver.getQuotientRemainderPair(numerator, denominator);
 	}
-	
+
 	@Test
 	public void testTranslate() {
-		SymbolicExpression expr = universe.divide(
-				universe.rational(1), universe.rational(2));
+		SymbolicExpression expr = universe.divide(universe.rational(1),
+				universe.rational(2));
+
 		out.println(cvcProver.translate(expr));
 	}
-	
-	@Ignore
+
 	@Test
-	//Tests SARL vs. CVC on Multiply.  CVC gives 2*5 while SARL gives 10.
-	public void translateMultiplyTest(){		
-		NumericExpression mulExp = universe.multiply(two, five);
+	public void translateMultiplyTest() {
+		NumericExpression mulExp = (NumericExpression) expressionFactory
+				.expression(SymbolicOperator.MULTIPLY, realType, two, five);
 		Expr expr = cvcProver.translate(mulExp);
 		Expr twoExpr = cvcProver.translate(two);
 		Expr fiveExpr = cvcProver.translate(five);
-		Expr expected = cvcProver.validityChecker().multExpr(twoExpr, fiveExpr); 
+		Expr expected = cvcProver.validityChecker().multExpr(twoExpr, fiveExpr);
+
 		assertEquals(expected, expr);
 	}
 }
