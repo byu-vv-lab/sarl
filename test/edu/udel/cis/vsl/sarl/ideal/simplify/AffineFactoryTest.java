e493191ca9263d010825695a742baaad56e3e681
mbrahma <mbrahma@7d651925-2d04-6a0e-8c93-cd1e20bcaab1>
2013-09-10 01:27:39 +0000

    Marshilla - New test cases for ideal.simplify package

---
+++ test/edu/udel/cis/vsl/sarl/ideal/simplify/AffineFactoryTest.java
@@ -0,0 +1,14 @@
+package edu.udel.cis.vsl.sarl.ideal.simplify;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class AffineFactoryTest {
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
