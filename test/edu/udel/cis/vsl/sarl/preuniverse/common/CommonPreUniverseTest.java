bb2837ba31489fa37ed6ee5696a7f5dac67eb399
jsaints <jsaints@7d651925-2d04-6a0e-8c93-cd1e20bcaab1>
2013-09-10 20:11:27 +0000

    Committing JUnit test class stub file.  (Used @Ignore to hide unimplemented tests!)

---
+++ test/edu/udel/cis/vsl/sarl/preuniverse/common/CommonPreUniverseTest.java
@@ -0,0 +1,864 @@
+package edu.udel.cis.vsl.sarl.preuniverse.common;
+
+import static org.junit.Assert.*;
+
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Ignore;
+import org.junit.Test;
+
+public class CommonPreUniverseTest {
+
+	@BeforeClass
+	public static void setUpBeforeClass() throws Exception {
+	}
+
+	@AfterClass
+	public static void tearDownAfterClass() throws Exception {
+	}
+
+	@Before
+	public void setUp() throws Exception {
+	}
+
+	@After
+	public void tearDown() throws Exception {
+	}
+
+	@Test
+    @Ignore
+	public void testCommonPreUniverse() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+    @Ignore
+	public void testErr() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+    @Ignore
+	public void testIerr() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+    @Ignore
+	public void testCanonicSymbolicExpression() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+    @Ignore
+	public void testExpressionSymbolicOperatorSymbolicTypeSymbolicObjectArray() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+    @Ignore
+	public void testExpressionSymbolicOperatorSymbolicTypeSymbolicObject() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+    @Ignore
+	public void testExpressionSymbolicOperatorSymbolicTypeSymbolicObjectSymbolicObject() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+    @Ignore
+	public void testExpressionSymbolicOperatorSymbolicTypeSymbolicObjectSymbolicObjectSymbolicObject() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+    @Ignore
+	public void testZero() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+    @Ignore
+	public void testHashSet() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+    @Ignore
+	public void testCompatible() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+    @Ignore
+	public void testIncompatible() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+    @Ignore
+	public void testForallIntConcrete() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+    @Ignore
+	public void testExistsIntConcrete() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+    @Ignore
+	public void testTupleUnsafe() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+    @Ignore
+	public void testNumericExpressionFactory() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+    @Ignore
+	public void testCanonicSymbolicObject() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+    @Ignore
+	public void testMake() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+    @Ignore
+	public void testNumberFactory() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+    @Ignore
+	public void testAddIterableOfQextendsNumericExpression() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+    @Ignore
+	public void testAndIterableOfQextendsBooleanExpression() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+    @Ignore
+	public void testAndBooleanExpressionBooleanExpression() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+    @Ignore
+	public void testPureType() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+    @Ignore
+	public void testBooleanType() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+    @Ignore
+	public void testIntegerType() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+    @Ignore
+	public void testHerbrandIntegerType() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+    @Ignore
+	public void testRealType() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+    @Ignore
+	public void testBoundedIntegerType() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+    @Ignore
+	public void testHerbrandRealType() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+    @Ignore
+	public void testCharacterType() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+    @Ignore
+	public void testArrayTypeSymbolicTypeNumericExpression() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+    @Ignore
+	public void testArrayTypeSymbolicType() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+    @Ignore
+	public void testTypeSequenceSymbolicTypeArray() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+    @Ignore
+	public void testTypeSequenceIterableOfQextendsSymbolicType() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+    @Ignore
+	public void testTupleTypeStringObjectSymbolicTypeSequence() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+    @Ignore
+	public void testTupleTypeStringObjectIterableOfQextendsSymbolicType() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+    @Ignore
+	public void testFunctionTypeSymbolicTypeSequenceSymbolicType() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+    @Ignore
+	public void testFunctionTypeIterableOfQextendsSymbolicTypeSymbolicType() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+    @Ignore
+	public void testUnionTypeStringObjectSymbolicTypeSequence() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+    @Ignore
+	public void testUnionTypeStringObjectIterableOfQextendsSymbolicType() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+    @Ignore
+	public void testNumObjects() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+    @Ignore
+	public void testObjectWithId() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+    @Ignore
+	public void testObjects() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+    @Ignore
+	public void testBooleanObject() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+    @Ignore
+	public void testCharObject() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+    @Ignore
+	public void testIntObject() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+    @Ignore
+	public void testNumberObject() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+    @Ignore
+	public void testStringObject() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+    @Ignore
+	public void testSymbolicConstant() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+    @Ignore
+	public void testNullExpression() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+    @Ignore
+	public void testNumberNumberObject() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+    @Ignore
+	public void testIntegerInt() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+    @Ignore
+	public void testRationalDouble() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+    @Ignore
+	public void testRationalIntInt() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+    @Ignore
+	public void testZeroInt() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+    @Ignore
+	public void testZeroReal() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+    @Ignore
+	public void testOneInt() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+    @Ignore
+	public void testOneReal() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+    @Ignore
+	public void testCharacter() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+    @Ignore
+	public void testExtractCharacter() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+    @Ignore
+	public void testStringExpression() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+    @Ignore
+	public void testAddNumericExpressionNumericExpression() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+    @Ignore
+	public void testSubtract() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+    @Ignore
+	public void testMultiplyNumericExpressionNumericExpression() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+    @Ignore
+	public void testMultiplyIterableOfQextendsNumericExpression() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+    @Ignore
+	public void testDivide() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+    @Ignore
+	public void testModulo() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+    @Ignore
+	public void testMinus() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+    @Ignore
+	public void testPowerNumericExpressionIntObject() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+    @Ignore
+	public void testPowerNumericExpressionInt() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+    @Ignore
+	public void testPowerNumericExpressionNumericExpression() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+    @Ignore
+	public void testExtractNumber() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+    @Ignore
+	public void testSubstituteSymbolicConstants() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+    @Ignore
+	public void testSubstituteSymbolicExpressionMapOfSymbolicExpressionSymbolicExpression() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+    @Ignore
+	public void testBoolBooleanObject() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+    @Ignore
+	public void testBoolBoolean() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+    @Ignore
+	public void testOrBooleanExpressionBooleanExpression() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+    @Ignore
+	public void testOrIterableOfQextendsBooleanExpression() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+    @Ignore
+	public void testNot() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+    @Ignore
+	public void testImplies() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+    @Ignore
+	public void testEquiv() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+    @Ignore
+	public void testSubstituteSymbolicExpressionSymbolicConstantSymbolicExpression() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+    @Ignore
+	public void testForallInt() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+    @Ignore
+	public void testExistsInt() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+    @Ignore
+	public void testLessThan() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+    @Ignore
+	public void testLessThanEquals() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+    @Ignore
+	public void testEqualsSymbolicExpressionSymbolicExpression() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+    @Ignore
+	public void testNeq() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+    @Ignore
+	public void testDivides() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+    @Ignore
+	public void testForall() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+    @Ignore
+	public void testExists() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+    @Ignore
+	public void testExtractBoolean() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+    @Ignore
+	public void testLambda() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+    @Ignore
+	public void testApply() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+    @Ignore
+	public void testUnionInject() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+    @Ignore
+	public void testUnionTest() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+    @Ignore
+	public void testUnionExtract() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+    @Ignore
+	public void testArray() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+    @Ignore
+	public void testAppend() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+    @Ignore
+	public void testRemoveElementAt() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+    @Ignore
+	public void testEmptyArray() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+    @Ignore
+	public void testLength() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+    @Ignore
+	public void testArrayRead() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+    @Ignore
+	public void testArrayWrite() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+    @Ignore
+	public void testDenseArrayWrite() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+    @Ignore
+	public void testDenseTupleWrite() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+    @Ignore
+	public void testArrayLambda() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+    @Ignore
+	public void testTuple() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+    @Ignore
+	public void testTupleRead() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+    @Ignore
+	public void testTupleWrite() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+    @Ignore
+	public void testCast() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+    @Ignore
+	public void testCond() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+    @Ignore
+	public void testComparator() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+    @Ignore
+	public void testIntegerLong() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+    @Ignore
+	public void testIntegerBigInteger() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+    @Ignore
+	public void testRationalInt() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+    @Ignore
+	public void testRationalLong() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+    @Ignore
+	public void testRationalBigInteger() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+    @Ignore
+	public void testRationalFloat() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+    @Ignore
+	public void testRationalLongLong() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+    @Ignore
+	public void testRationalBigIntegerBigInteger() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+    @Ignore
+	public void testNumberNumber() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+    @Ignore
+	public void testTrueExpression() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+    @Ignore
+	public void testFalseExpression() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+    @Ignore
+	public void testNumValidCalls() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+    @Ignore
+	public void testNumProverValidCalls() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+    @Ignore
+	public void testIncrementValidCount() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+    @Ignore
+	public void testIncrementProverValidCount() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+    @Ignore
+	public void testBasicCollection() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+    @Ignore
+	public void testReferenceType() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+    @Ignore
+	public void testNullReference() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+    @Ignore
+	public void testDereference() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+    @Ignore
+	public void testReferencedType() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+    @Ignore
+	public void testIdentityReference() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+    @Ignore
+	public void testArrayElementReference() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+    @Ignore
+	public void testTupleComponentReference() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+    @Ignore
+	public void testUnionMemberReference() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+    @Ignore
+	public void testOffsetReference() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+    @Ignore
+	public void testAssign() {
+		fail("Not yet implemented");
+	}
+
+}
