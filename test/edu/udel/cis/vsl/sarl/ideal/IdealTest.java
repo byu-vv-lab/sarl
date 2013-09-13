20e2fcd8e2eb4c4913dec1fbfaca7d6c6e32692a
bcknodel <bcknodel@7d651925-2d04-6a0e-8c93-cd1e20bcaab1>
2013-09-13 19:19:36 +0000

    Added CastToReal to the IdealTests section. Tests are not currently running but it is accessing previous unran code through the manipulations

--- test/edu/udel/cis/vsl/sarl/ideal/IdealTest.java
+++ test/edu/udel/cis/vsl/sarl/ideal/IdealTest.java
@@ -31,16 +31,26 @@ import edu.udel.cis.vsl.sarl.IF.expr.NumericExpression;
 import edu.udel.cis.vsl.sarl.IF.expr.NumericSymbolicConstant;
 import edu.udel.cis.vsl.sarl.IF.expr.SymbolicConstant;
 import edu.udel.cis.vsl.sarl.IF.expr.SymbolicExpression;
+import edu.udel.cis.vsl.sarl.IF.expr.SymbolicExpression.SymbolicOperator;
 import edu.udel.cis.vsl.sarl.IF.number.IntegerNumber;
 import edu.udel.cis.vsl.sarl.IF.number.NumberFactory;
 import edu.udel.cis.vsl.sarl.IF.number.RationalNumber;
 import edu.udel.cis.vsl.sarl.IF.object.IntObject;
+import edu.udel.cis.vsl.sarl.IF.object.NumberObject;
 import edu.udel.cis.vsl.sarl.IF.object.StringObject;
+import edu.udel.cis.vsl.sarl.IF.object.SymbolicObject;
+import edu.udel.cis.vsl.sarl.IF.type.SymbolicType;
+import edu.udel.cis.vsl.sarl.collections.IF.CollectionFactory;
+import edu.udel.cis.vsl.sarl.expr.IF.BooleanExpressionFactory;
+import edu.udel.cis.vsl.sarl.expr.IF.NumericExpressionFactory;
 import edu.udel.cis.vsl.sarl.ideal.IF.Constant;
 import edu.udel.cis.vsl.sarl.ideal.IF.IdealFactory;
 import edu.udel.cis.vsl.sarl.ideal.IF.Monic;
 import edu.udel.cis.vsl.sarl.ideal.IF.Monomial;
 import edu.udel.cis.vsl.sarl.ideal.IF.RationalExpression;
+import edu.udel.cis.vsl.sarl.ideal.common.CommonIdealFactory;
+import edu.udel.cis.vsl.sarl.number.real.RealNumber;
+import edu.udel.cis.vsl.sarl.number.real.RealNumberFactory;
 import edu.udel.cis.vsl.sarl.object.IF.ObjectFactory;
 import edu.udel.cis.vsl.sarl.preuniverse.PreUniverses;
 import edu.udel.cis.vsl.sarl.preuniverse.IF.FactorySystem;
@@ -52,8 +62,12 @@ public class IdealTest {
 	private NumberFactory numberFactory;
 	private ObjectFactory objectFactory;
 	private SymbolicTypeFactory typeFactory;
-	// private CollectionFactory collectionFactory;
+	private CollectionFactory collectionFactory;
+	private NumericExpressionFactory numericExpressionFactory;
 	private IdealFactory idealFactory;
+	private BooleanExpressionFactory booleanFactory;
+	private RealNumberFactory realNumberFactory;
+	private CommonIdealFactory commonIdealFactory;
 
 	private RationalNumber n1; // 3/2
 	private RationalNumber n2; // -1/4
@@ -65,16 +79,34 @@ public class IdealTest {
 	StringObject Xobj; // "X"
 	NumericSymbolicConstant x; // int symbolic constant "X"
 	NumericSymbolicConstant y; // int symbolic constant "Y"
-
+	private NumericExpression five; 		// 5
+	private RationalNumber realFive; 			// 5
+	private NumericExpression three;		// 3
+	private RationalNumber realThree;			// 3
+	NumericExpression e1; 					// 5         		IsReal
+	NumericExpression e2;					// 5 + 3     		Add
+	NumericExpression e3;				 	// 5 > 3, 5, 3    COND
+	NumericExpression e4; // 5 * 3			MULTIPLY
+	NumericExpression e5; // -5				NEGATIVE
+	NumericExpression e6; // 5 ^ 3			POWER
+	NumericExpression e7; // 5 - 3			SUBTRACT
+	NumericExpression e8; //				DEFAULT
+	
 	@Before
 	public void setUp() throws Exception {
 		FactorySystem system = PreUniverses.newIdealFactorySystem();
-
-		objectFactory = system.objectFactory();
 		numberFactory = system.numberFactory();
+		objectFactory = system.objectFactory();
 		typeFactory = system.typeFactory();
-		// collectionFactory = system.collectionFactory();
+		collectionFactory = system.collectionFactory();
+		numericExpressionFactory = system.numericFactory();
 		idealFactory = (IdealFactory) system.numericFactory();
+		booleanFactory = system.booleanFactory();
+		realNumberFactory = (RealNumberFactory) system.numberFactory();
+		commonIdealFactory = new CommonIdealFactory(numberFactory,
+				objectFactory, typeFactory,
+				collectionFactory,
+				booleanFactory); 
 		n1 = numberFactory.rational("1.5");
 		n2 = numberFactory.rational("-.25");
 		n3 = numberFactory.rational("1.25");
@@ -87,8 +119,25 @@ public class IdealTest {
 				typeFactory.integerType()));
 		y = objectFactory.canonic(idealFactory.symbolicConstant(
 				objectFactory.stringObject("Y"), typeFactory.integerType()));
+		
+		
+		realFive = numberFactory.integerToRational(numberFactory.integer(5));
+		five = commonIdealFactory.constant(realFive); 
+		realThree = numberFactory.integerToRational(numberFactory.integer(3)); 
+		three = commonIdealFactory.constant(realThree);
+		e1 = commonIdealFactory.constant(realFive);
+		//e1 = numericExpressionFactory.number(realFive);				// 1         IsReal
+		e2 = commonIdealFactory.expression(SymbolicOperator.ADD, typeFactory.realType(), five, three);
+		//e2 = numericExpressionFactory.expression(SymbolicOperator.ADD, five.type(), five, three);
+		//e2 = numericExpressionFactory.add(five, three); 		// 5 + 3     Add
+		//e3 = numericExpressionFactory.lessThan(five, three); 	// 3 > 2, 3, 2    COND
+		e4 = commonIdealFactory.multiply(five, three); 	// 5 * 3			MULTIPLY
+		e5 = commonIdealFactory.minus(five); 				// -5				NEGATIVE
+		e6 = commonIdealFactory.power(five, three);		// 5 ^ 3			POWER
+		e7 = commonIdealFactory.subtract(five, three); // 5 - 3			SUBTRACT
+		e8 = commonIdealFactory.zeroReal(); //				DEFAULT}
 	}
-
+	
 	@After
 	public void tearDown() throws Exception {
 	}
@@ -193,4 +242,70 @@ public class IdealTest {
 		assertEquals(negativer, minusr);
  	}
  	*/
+	
+	@Test
+	public void castToReal(){
+		SymbolicType real = typeFactory.realType();
+		SymbolicType real2 = numericExpressionFactory.oneReal().type();
+		NumberObject Five = objectFactory.numberObject(numberFactory.number("5"));
+		RationalNumber n5 = numberFactory.rational("5.5");
+		RealNumber realFive = (RealNumber) realNumberFactory.integer(5); //realNumberFactory.integer(fiveLong);
+		//assertEquals(commonIdealFactory.cast(e1, real), n5);
+		
+		//NumberObject
+		
+		Constant one = idealFactory.intConstant(1);
+		Constant two = idealFactory.intConstant(2);
+		Constant three = idealFactory.intConstant(3);
+		
+		IntObject int1 = objectFactory.intObject(5);
+		IntObject int2 = objectFactory.intObject(3);
+		IntObject int3 = objectFactory.intObject(2);
+		//SymbolicCollection collection = collectionFactory.basicCollection(intlist);
+		IntObject[] integers = {int1, int2, int3};
+		//NumericExpression ints = idealFactory.expression(SymbolicOperator.ADD, typeFactory.integerType(), int1, int2, int3);
+		//NumericExpression ints = idealFactory.expression(SymbolicOperator.ADD, typeFactory.integerType(), int1, int2);
+		//NumericExpression ints = idealFactory.expression(SymbolicOperator.ADD, typeFactory.integerType(), int1);
+		NumericExpression ints3 = idealFactory.expression(SymbolicOperator.ADD, typeFactory.integerType(), one, two, three);
+		NumericExpression ints2 = idealFactory.expression(SymbolicOperator.ADD, typeFactory.integerType(), one, two);
+		NumericExpression ints1 = idealFactory.expression(SymbolicOperator.ADD, typeFactory.integerType(), one);
+		NumericExpression ints = idealFactory.expression(SymbolicOperator.ADD, typeFactory.integerType(), one, two, three);
+		NumericExpression xp1 = idealFactory.add(x, idealFactory.intConstant(1));
+		NumericExpression lp2 = idealFactory.add(one, idealFactory.intConstant(2));
+		NumericExpression testn = commonIdealFactory.add(ints3, lp2);
+		out.println("testn: " + testn);
+		commonIdealFactory.cast(testn, real);
+		out.println("testn: " + testn);
+		assertEquals(commonIdealFactory.cast(e1, real), commonIdealFactory.cast(five, real));
+		
+		
+		long eightLong = 9;  // e2 = 3 + 5    = 8
+		RealNumber realAdd = realNumberFactory.integer(eightLong);
+		out.println("e2: " + e2);
+		NumericExpression test3 = commonIdealFactory.expression(SymbolicOperator.ADD, typeFactory.realType(),five, three, three);
+		NumericExpression test2 = commonIdealFactory.add(e1, x);
+		commonIdealFactory.cast(e2, real);
+		commonIdealFactory.cast(test2, real);
+		out.println("test2: " + test2);
+		//assertEquals(test2, realAdd);
+		
+		numericExpressionFactory.cast(testn, real);
+		long fifteenLong = 15; // e4 = 3 * 5  =  15
+		RealNumber realMulitply = realNumberFactory.integer(fifteenLong);
+		//assertEquals(commonIdealFactory.cast(e4, real), realMulitply);
+		
+		long fiveMinusLong = -5; // e5 = -1 * 5  =  -5
+		RealNumber realMinus = realNumberFactory.integer(fiveMinusLong);
+		//assertEquals(commonIdealFactory.cast(e5, real), realMinus);
+		
+		long hundredTwentyLong = 125; // e6 = 5 ^ 3  =  125
+		RealNumber realPower = realNumberFactory.integer(hundredTwentyLong);
+		//assertEquals(commonIdealFactory.cast(e6, real), realPower);
+		
+		long twoLong = 2; // e7 = 5 - 3  =  2
+		RealNumber realSubtract = realNumberFactory.integer(twoLong);
+		//assertEquals(commonIdealFactory.cast(e7, real), realSubtract);
+		
+	}
+	
 }
