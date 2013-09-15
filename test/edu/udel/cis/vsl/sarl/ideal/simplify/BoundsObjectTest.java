857325dad1421357dc3dc88b034d8aa322fe80da
danfried <danfried@7d651925-2d04-6a0e-8c93-cd1e20bcaab1>
2013-09-15 19:10:34 +0000

    Added a slew of tests: coverage on AssertionError's thrown in the code, equality, toString... to name a few.  Also corrected the issue, raised in a ticket, of no assertion to test on being specified in the test file.

--- test/edu/udel/cis/vsl/sarl/ideal/simplify/BoundsObjectTest.java
+++ test/edu/udel/cis/vsl/sarl/ideal/simplify/BoundsObjectTest.java
@@ -2,9 +2,14 @@ package edu.udel.cis.vsl.sarl.ideal.simplify;
 
 import static org.junit.Assert.*;
 
+import java.io.PrintStream;
+
+import org.junit.After;
+import org.junit.Before;
 import org.junit.BeforeClass;
 import org.junit.Ignore;
 import org.junit.Test;
+import static org.hamcrest.CoreMatchers.*;
 
 import edu.udel.cis.vsl.sarl.IF.expr.NumericExpression;
 import edu.udel.cis.vsl.sarl.IF.expr.NumericSymbolicConstant;
@@ -18,18 +23,16 @@ import edu.udel.cis.vsl.sarl.preuniverse.IF.PreUniverse;
 import edu.udel.cis.vsl.sarl.type.IF.SymbolicTypeFactory;
 
 public class BoundsObjectTest {
-/*
-	@Test
-	public void test() {
-		fail("Not yet implemented");
-	}
-*/
-/*	
+	
 	private static FactorySystem system;
+	
+	private static PrintStream out = System.out;
 	
 	private static SymbolicTypeFactory symbFactory;
 	
-	private static SymbolicExpression symbExpr;
+	private static SymbolicExpression symbExpr_xpy;
+	
+	private static SymbolicExpression symbExpr_xy;
 	
 	private static NumericSymbolicConstant x;
 
@@ -45,26 +48,29 @@ public class BoundsObjectTest {
 	
 	private static NumericExpression xpy;
 	
-	private static BoundsObject boundobj;
+	private static NumericExpression xy;
+	
+	private static BoundsObject boundObj;
 	
 	private static NumberFactory numFact;
 	
-	private static Number numBound;
+	//private static Number numBound;
+	private static edu.udel.cis.vsl.sarl.IF.number.Number numBound3;
+	
+	private static edu.udel.cis.vsl.sarl.IF.number.Number numBound5;
 	
 	private static NumericExpression one, two, five;
 	
-	*/
+	
 	
 	@BeforeClass
 	public static void setUpBeforeClass() throws Exception {
-		/*
-		
-		
+		system = PreUniverses.newIdealFactorySystem();
 		preUniv = PreUniverses.newPreUniverse(system);
 		one = preUniv.rational(1); // 1.0
 		two = preUniv.rational(2); // 2.0
 		five = preUniv.rational(5); // 5.0
-		system = PreUniverses.newIdealFactorySystem();
+		//system = PreUniverses.newIdealFactorySystem();
 		symbFactory = system.expressionFactory().typeFactory();
 		realType = preUniv.realType();
 		integerType = preUniv.integerType();
@@ -73,23 +79,75 @@ public class BoundsObjectTest {
 		y = (NumericSymbolicConstant) preUniv.symbolicConstant(
 				preUniv.stringObject("y"), realType);
 		xpy = preUniv.add(x, y);
-		symbExpr = xpy;
-//		xpy.
+		symbExpr_xpy = xpy;
+		xy = preUniv.multiply(x, y);
+		symbExpr_xy = xy;
+		numFact = preUniv.numberFactory();
+		//numBound = preUniv.
 //		numbFact = preUniv.  //numExprFact.numberFactory();
 //		numbBound = preUniv.  //numbFact.rational("three");
-//		
-//		boundobj = BoundsObject.newTightBound(symbExpr, bound)
-		numBound = (Number) numFact.rational("3");
-		
-		*/
-		
+		numBound3 = numFact.rational("3");
+		numBound5 = numFact.rational("5");
+		boundObj = BoundsObject.newTightBound(symbExpr_xpy, numBound3);
 	}
 	
-	@Ignore
+	@Before
+	public void setUp() throws Exception {
+	}
+
+	@After
+	public void tearDown() throws Exception {
+	}
+	
 	@Test
 	public void equals(){
-		/*
-		BoundsObject boundObjEquals = BoundsObject.newTightBound(symbExpr, (edu.udel.cis.vsl.sarl.IF.number.Number)numBound);
-	*/
+		//test on .equals for equal objects
+		BoundsObject boundObjEquals = boundObj;
+		assertEquals(boundObj, boundObjEquals); //expected to pass; both objects are equal
+		assertTrue(boundObj.equals(boundObjEquals));
+		out.println("boundObj = " + boundObj.toString() + " boundObjEquals = " + boundObjEquals.toString());
+		out.println("boundObj(xpy,3) equals? boundObjEquals(xpy, 3): " + boundObj.equals(boundObjEquals));
+		
+		
+		//test on .equals for unequal objects, on basis of expr
+		boundObjEquals = BoundsObject.newTightBound(symbExpr_xy, numBound3);
+		assertThat(boundObj, is(not(boundObjEquals))); //expected to pass, as these objects are not equal
+		assertFalse(boundObj.equals(boundObjEquals));
+		out.println("boundObj = " + boundObj.toString() + " boundObjEquals = " + boundObjEquals.toString());
+		out.println("boundObj(xpy,3) equals? boundObjEquals(xy, 3): " + boundObj.equals(boundObjEquals));
+		
+		//test on .equals for unequal objects, on basis of bound
+		boundObjEquals = BoundsObject.newTightBound(symbExpr_xpy, numBound5);
+		assertThat(boundObj, is(not(boundObjEquals))); //expected to pass, as these objects are not equal
+		assertFalse(boundObj.equals(boundObjEquals));
+		assertFalse(boundObjEquals.equals(boundObj));
+		out.println("boundObj = " + boundObj.toString() + " boundObjEquals = " + boundObjEquals.toString());
+		out.println("boundObj(xpy,3) equals? boundObjEquals(xpy, 5): " + boundObj.equals(boundObjEquals));
+		assertEquals(boundObj.symbolicConstant(), boundObjEquals.symbolicConstant());
+		
+		//test on .equals for unequal objects, on basis of bound and expr
+		boundObjEquals = BoundsObject.newTightBound(symbExpr_xy, numBound5);
+		assertThat(boundObj, is(not(boundObjEquals))); //expected to pass, as these objects are not equal
+		assertFalse(boundObj.equals(boundObjEquals));
+		assertFalse(boundObjEquals.equals(boundObj));
+		
+		//test on isConsistent() method 
+		assertEquals(true, boundObj.isConsistent());
+		
+		//test on .equals method for a second object that is not an instance of BoundsObject
+		assertFalse(boundObj.equals(numBound3));
+		
+		//test on Assertions in BoundsObject, when the constructor receives a null expr
+		boolean gotError = false;
+		try{
+			boundObjEquals = BoundsObject.newTightBound(null, numBound5);
+		}catch(AssertionError e){
+			//expected error
+			//e.addSuppressed(e);
+			out.println(e.getMessage());
+			gotError = true;
+		}
+		assertEquals(true, gotError); 
+
 	}
 }
