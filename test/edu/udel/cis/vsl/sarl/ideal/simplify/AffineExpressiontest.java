125d6fecb23894bd7a0f5025956dedd61e161e9d
mwann <mwann@7d651925-2d04-6a0e-8c93-cd1e20bcaab1>
2013-09-16 03:51:38 +0000

    so yea new affineexpressiontest there seems to be another in here/ maybe someone messed up on commits idk

--- test/edu/udel/cis/vsl/sarl/ideal/simplify/AffineExpressiontest.java
+++ test/edu/udel/cis/vsl/sarl/ideal/simplify/AffineExpressiontest.java
@@ -17,27 +17,49 @@ import edu.udel.cis.vsl.sarl.IF.expr.NumericSymbolicConstant;
 import edu.udel.cis.vsl.sarl.IF.number.Number;
 import edu.udel.cis.vsl.sarl.IF.number.NumberFactory;
 import edu.udel.cis.vsl.sarl.IF.type.SymbolicType;
+import edu.udel.cis.vsl.sarl.collections.IF.CollectionFactory;
+import edu.udel.cis.vsl.sarl.expr.IF.BooleanExpressionFactory;
+import edu.udel.cis.vsl.sarl.expr.IF.NumericExpressionFactory;
+import edu.udel.cis.vsl.sarl.ideal.IF.Constant;
+import edu.udel.cis.vsl.sarl.ideal.IF.IdealFactory;
 import edu.udel.cis.vsl.sarl.ideal.IF.Polynomial;
+import edu.udel.cis.vsl.sarl.ideal.common.CommonIdealFactory;
+import edu.udel.cis.vsl.sarl.number.real.RealNumberFactory;
+import edu.udel.cis.vsl.sarl.object.IF.ObjectFactory;
+import edu.udel.cis.vsl.sarl.preuniverse.PreUniverses;
+import edu.udel.cis.vsl.sarl.preuniverse.IF.FactorySystem;
+import edu.udel.cis.vsl.sarl.type.IF.SymbolicTypeFactory;
 
 public class AffineExpressiontest {
 	private Polynomial pseudo; /* maybe null */
 	private static SymbolicUniverse universe;
-	private NumberFactory numberFactory;
+	private static NumberFactory numberFactory;
 	private static NumericSymbolicConstant x;
 
 	private static NumericSymbolicConstant y;
 
 	private static NumericExpression xpy;
+	///////////////////////////////////////////////////////
+	private static ObjectFactory objectFactory;
+	private static SymbolicTypeFactory typeFactory;
+	private static  CollectionFactory collectionFactory;
+	private static NumericExpressionFactory numericExpressionFactory;
+
+	private static BooleanExpressionFactory booleanFactory;
+	private static RealNumberFactory realNumberFactory;
+	private static CommonIdealFactory commonIdealFactory;
+
+	private static IdealFactory idealFactory;
 
 	private static SymbolicType realType;
-
 	private static SymbolicType integerType;
 
 	private static NumericExpression one, two;
 
-	private Number coefficient; /* not null */
 
-	private Number offset; /* not null */
+
+	private static edu.udel.cis.vsl.sarl.IF.number.Number offset;
+	private static edu.udel.cis.vsl.sarl.IF.number.Number coefficient;
 
 
 	private static PrintStream out = System.out;
@@ -54,6 +76,25 @@ public class AffineExpressiontest {
 		xpy = universe.add(x, y);
 		one = universe.rational(1); // 1.0
 		two = universe.rational(2); // 2.0
+		////////////
+		FactorySystem system = PreUniverses.newIdealFactorySystem();
+		numberFactory = system.numberFactory();
+		objectFactory = system.objectFactory();
+		typeFactory = system.typeFactory();
+		collectionFactory = system.collectionFactory();
+		numericExpressionFactory = system.numericFactory();
+		idealFactory = (IdealFactory) system.numericFactory();
+		booleanFactory = system.booleanFactory();
+		realNumberFactory = (RealNumberFactory) system.numberFactory();
+		commonIdealFactory = new CommonIdealFactory(numberFactory,
+				objectFactory, typeFactory,
+				collectionFactory,
+				booleanFactory); 
+		//////////////////////////////////////////
+		commonIdealFactory = new CommonIdealFactory(numberFactory,
+				objectFactory, typeFactory,
+				collectionFactory,
+				booleanFactory); 
 	}
 
 	@Before
@@ -63,13 +104,19 @@ public class AffineExpressiontest {
 	@After
 	public void tearDown() throws Exception {
 	}
-	@Ignore
+	@Test
 	public void tostringtest() {
-		
-		//offset = numberFactory.rational("2");
+		//SymbolicMap<Monic, Monomial> termMap = commonIdealFactory.emptyMap();
+		//Monomial factorization = idealFactory.monomial(c10, (Monic) x);
+		offset = numberFactory.rational("3");
+		coefficient = numberFactory.rational("3");
+
+
 	//	coefficient =  numberFactory.rational("3");
 		AffineExpression test = new AffineExpression(pseudo, offset,coefficient);
-	    assertEquals(test.toString(),test.toString());
+		AffineExpression test2 = new AffineExpression(pseudo,numberFactory.rational("10"),numberFactory.rational("50"));
+		assertEquals(test.toString(),"3");
+		assertEquals(test2.toString(),"50");
 	}
 
 }
