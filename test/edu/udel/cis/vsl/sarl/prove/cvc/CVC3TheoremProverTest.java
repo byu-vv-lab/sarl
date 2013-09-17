72934c0f50d92da6bb21a7e9cc34f6f843d1ac4f
jleczner <jleczner@7d651925-2d04-6a0e-8c93-cd1e20bcaab1>
2013-09-17 01:03:30 +0000

    added testTranslateType cases, but having more translation problems, so ignored for now

--- test/edu/udel/cis/vsl/sarl/prove/cvc/CVC3TheoremProverTest.java
+++ test/edu/udel/cis/vsl/sarl/prove/cvc/CVC3TheoremProverTest.java
@@ -32,6 +32,7 @@ import edu.udel.cis.vsl.sarl.IF.type.SymbolicIntegerType.IntegerKind;
 import edu.udel.cis.vsl.sarl.IF.type.SymbolicType.SymbolicTypeKind;
 import edu.udel.cis.vsl.sarl.IF.type.SymbolicRealType;
 import edu.udel.cis.vsl.sarl.IF.type.SymbolicType;
+import edu.udel.cis.vsl.sarl.IF.type.SymbolicTypeSequence;
 import edu.udel.cis.vsl.sarl.expr.IF.ExpressionFactory;
 import edu.udel.cis.vsl.sarl.preuniverse.PreUniverses;
 import edu.udel.cis.vsl.sarl.preuniverse.IF.FactorySystem;
@@ -50,20 +51,26 @@ public class CVC3TheoremProverTest {
 			.newPreUniverse(factorySystem);
 	private static ExpressionFactory expressionFactory = factorySystem
 			.expressionFactory();
+	// types
 	private static SymbolicRealType realType = universe.realType();
 	private static SymbolicIntegerType intType = universe.integerType();
 	private static SymbolicType boolType = universe.booleanType();
+	private static SymbolicType intArrayType = universe.arrayType(intType);
+	// expressions
 	private static NumericExpression ten = universe.rational(10);
 	private static NumericExpression five = universe.rational(5);
 	private static NumericExpression four = universe.rational(4);
 	private static NumericExpression two = universe.rational(2);
 	private static NumericExpression zero = universe.rational(0);
-	private static SymbolicConstant e = universe.symbolicConstant(universe.stringObject("e"), intType);
-	private static SymbolicConstant f = universe.symbolicConstant(universe.stringObject("f"), intType);
 	private static BooleanExpression booleanExprTrue = universe
 			.trueExpression();
 	private static BooleanExpression booleanExprFalse = universe
 			.falseExpression();
+	// constants
+	private static SymbolicConstant e = universe.symbolicConstant(universe.stringObject("e"), intType);
+	private static SymbolicConstant f = universe.symbolicConstant(universe.stringObject("f"), intType);
+	
+
 	
 	// Instance fields: instantiated before each test is run...
 	private TheoremProverFactory proverFactory;
@@ -292,6 +299,34 @@ public class CVC3TheoremProverTest {
 //		Expr x1and2Exist = cvcProver.translate(andExpression);
 		expected = vc.existsExpr(vars, x1and2Exist);
 		assertEquals(expected, x1and2Exist);
+	}
+	
+	@Ignore
+	@Test
+	public void testTranslateType() {
+		cvcProver = (CVC3TheoremProver) proverFactory
+				.newProver(booleanExprTrue);
+		
+		// cvc3 int array (with int index type)
+		Type expected = vc.arrayType(vc.intType(), vc.intType());
+		Type translateResult = cvcProver.translateType(intArrayType);
+//		assertEquals(expected, translateResult);
+		
+		// cvc3 tuple
+		List<SymbolicType> typesList = new ArrayList<SymbolicType>();
+		typesList.add(intType);
+		typesList.add(intType);
+		SymbolicTypeSequence types = universe.typeSequence(typesList);
+		StringObject name = universe.stringObject("twoIntTuple");
+		SymbolicType twoIntTupleType = universe.tupleType(name, types);
+		
+		expected = vc.tupleType(vc.intType(), vc.intType());
+		translateResult = cvcProver.translateType(twoIntTupleType);
+		assertEquals(expected.toString(), translateResult.toString()); 
+		// need .toString() to show translation success. equality is determined
+		// by name and type sequence, but there is no name parameter for the 
+		// cvc3 tuple
+				
 	}
 	
 	@Test
