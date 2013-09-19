491b39c36e758467bbebabff3cb28fb6ee36be8c
jsaints <jsaints@7d651925-2d04-6a0e-8c93-cd1e20bcaab1>
2013-09-19 04:00:50 +0000

    Cleaned up the testdenseArrayWrite_*() tests a lot, now that I know how to properly test thrown Exceptions with JUnit. (Which resulted in much much shorter tests!)

--- test/edu/udel/cis/vsl/sarl/preuniverse/common/CommonPreUniverseTest.java
+++ test/edu/udel/cis/vsl/sarl/preuniverse/common/CommonPreUniverseTest.java
@@ -9,6 +9,7 @@ import java.util.Arrays;
 import java.util.Collection;
 import java.util.Comparator;
 import java.util.LinkedList;
+import java.util.List;
 
 import org.junit.After;
 import org.junit.AfterClass;
@@ -48,6 +49,7 @@ public class CommonPreUniverseTest {
 	private static SymbolicType integerType;
 	private static SymbolicType realType;
 	private static SymbolicType arrayType;
+	private static SymbolicType doubleArrayType;
 	// Factories
 	private static ObjectFactory objectFactory;
 	private static ExpressionFactory expressionFactory;
@@ -69,7 +71,8 @@ public class CommonPreUniverseTest {
 		// Types
 		integerType = universe.integerType();
 		realType = universe.realType();
-		arrayType = universe.arrayType(integerType);
+		arrayType = universe.arrayType(integerType); //creates an array of ints
+		doubleArrayType = universe.arrayType(arrayType); //creates an array of int[]s
 		
 		// NumberExpressions
 		two = universe.integer(2);
@@ -775,46 +778,42 @@ public class CommonPreUniverseTest {
 	public void testArrayRead() {
 		fail("Not yet implemented");
 	}
+	
 
-	
+	/*
+	 * Main function that tests for successful completion of denseArrayWrite()
+	 * Written by Jordan Saints on 9/16/13
+	 */
 	@Test
-	/*
-	 * Tests the denseArrayWrite(SymbolicExpression, Iterable) method.
-	 * Written by Jordan Saints on 9/16/13
-	 */
-	public void testDenseArrayWrite() {
-		testDenseArrayWriteSuccess(); //test method for success of usual functionality
-		testDenseArrayWriteIntTypeFailure(); //trigger failure exception 1 of 2
-		testDenseArrayWriteArrayTypeFailure(); //trigger failure exception 2 of 2
-	}
-	
-	/*
-	 * Auxiliary function to testDenseArrayWrite()
-	 * Tests for NORMAL operation of denseArrayWrite()
-	 * Written by Jordan Saints on 9/16/13
-	 */
-	private void testDenseArrayWriteSuccess() {
-		SymbolicExpression symExpression = universe.symbolicConstant(universe.stringObject("symExpression"), arrayType);
-		SymbolicExpression denseResult = universe.denseArrayWrite(
-				symExpression,
-				Arrays.asList(new SymbolicExpression[] { null, null, two, null, two, null, null }));
+	public void testDenseArrayWrite_success() {
+		// The SymbolicExpression, of type ARRAY(int) (array of ints), that we will be writing to
+		SymbolicExpression arrayTypeExpression = universe.symbolicConstant(universe.stringObject("arrayTypeExpression"), arrayType);
+		
+		// A List of expressions to write to new array (in dense format),
+		// where, after calling denseArrayWrite(), an element's index in this List == index in resulting expressions sequence
+		List<SymbolicExpression> expressionsToWrite = Arrays.asList(new SymbolicExpression[] { null, null, two, null, two, null, null });
+		
+		// Call denseArrayWrite()!
+		SymbolicExpression denseResult = universe.denseArrayWrite(arrayTypeExpression, expressionsToWrite);
 		
 		// Test by comparing to normal write operation in arrayWrite()
-		//universe.arrayWrite(array to write to, index in array to write at, value to write @ that index)
-		SymbolicExpression writeResult = universe.arrayWrite(symExpression, two, two); //adds a 2 at index 2 in symExpression array
+		// USAGE: universe.arrayWrite(array to write to, index in array to write at, value to write @ that index)
+		SymbolicExpression writeResult = universe.arrayWrite(arrayTypeExpression, two, two); //adds a 2 at index 2 in arrayTypeExpression array
 		writeResult = universe.arrayWrite(writeResult, four, two); //replace writeResult's entry at index 4 (NumExpr four) with a 2 (NumExpr two) 
 		assertEquals(writeResult, denseResult); //ensure arrays are the same
 		assertTrue(universe.equals(writeResult, denseResult).isTrue()); //use PreUniverse's SymbolicExpression equals() method
 		
-		// Test types
-		assertEquals(universe.arrayType(integerType), arrayType); //test that arrayType is correct
+		// Test denseResult's type
+		assertEquals(universe.arrayType(integerType), arrayType); //check that arrayType is actually correct type to begin with
 		assertEquals(arrayType, denseResult.type()); //check that denseResult is of arrayType
 
-		// Test arguments
-		assertEquals(2, denseResult.numArguments()); //test numArguments
+		// Test denseResult's arguments
+		assertEquals(2, denseResult.numArguments()); //test total numArguments
 		@SuppressWarnings("unchecked")
-		SymbolicSequence<SymbolicExpression> expressions = (SymbolicSequence<SymbolicExpression>) denseResult.argument(1);
+		SymbolicSequence<SymbolicExpression> expressions = (SymbolicSequence<SymbolicExpression>) denseResult.argument(1); //get sequence of expressions
 		assertEquals(5, expressions.size()); //the 2 trailing null SymExprs will be chopped off ("made dense")
+		
+		// Test denseResult's expressions sequence against known values in expressionsToWrite List
 		assertTrue(universe.equals(nullExpression, expressions.get(0)).isTrue());
 		assertTrue(universe.equals(nullExpression, expressions.get(1)).isTrue());
 		assertTrue(universe.equals(two, expressions.get(2)).isTrue());
@@ -823,54 +822,37 @@ public class CommonPreUniverseTest {
 	}
 	
 	/*
-	 * Auxiliary function #1 for testDenseArrayWrite()
-	 * Trigger failure branch 1 of 2; throws SARLException
+	 * Auxiliary function #1 that tests failure branch (1 of 2) of denseArrayWrite()
 	 * Written by Jordan Saints on 9/16/13
 	 */
-	private void testDenseArrayWriteIntTypeFailure() {
-		SymbolicExpression integerTypeExpression = universe.symbolicConstant(universe.stringObject("notInstanceOfSymbolicArrayType"), integerType);
-		SymbolicExpression exception1Result = null;
-		String exception1Message = "Argument 0 of denseArrayWrite must have array type but had type int";
-		try {
-			exception1Result = universe.denseArrayWrite(
-					integerTypeExpression,
-					Arrays.asList(new SymbolicExpression[] { null, null, two, null, two, null, null }));
-			
-			// Test for undesired universe.denseArrayWrite(x,x) success
-			if (exception1Result != null) { //if no exception is thrown...
-				assertTrue(false); //this test has failed (an exception SHOULD be thrown!)
-			}
-		} catch (Exception e) {
-			assertEquals(e.getClass(), SARLException.class); //test class name of thrown exception
-			assertEquals(e.getMessage(), exception1Message); //test message of thrown exception
-			assertTrue(e.getMessage().equals(exception1Message)); //double check
-		}
+	@Test(expected=SARLException.class)
+	public void testDenseArrayWrite_param1Exception() {
+		// Create SymbolicExpression of type INTEGER (no array at all)
+		SymbolicExpression integerTypeExpression = universe.symbolicConstant(universe.stringObject("integerTypeExpression"), integerType);
+		
+		// A List of expressions to write to new array (in dense format)
+		List<SymbolicExpression> expressionsToWrite = Arrays.asList(new SymbolicExpression[] { null, null, two, null, two, null, null });
+		
+		// Will throw a SARLException because input param #1 (SymbolicExpression) must be of type ARRAY
+		// "Argument 0 of denseArrayWrite must have array type but had type int"
+		universe.denseArrayWrite(integerTypeExpression, expressionsToWrite);
 	}
 	
 	/*
-	 * Auxiliary function #2 for testDenseArrayWrite()
-	 * Trigger failure branch 2 of 2; throws SARLException
+	 * Auxiliary function #2 that tests failure branch (2 of 2) of denseArrayWrite()
 	 * Written by Jordan Saints on 9/16/13
 	 */
-	private void testDenseArrayWriteArrayTypeFailure() {
-		SymbolicType doubleArrayType = universe.arrayType(arrayType); //int[]
-		SymbolicExpression arrayTypeExpression = universe.symbolicConstant(universe.stringObject("arrayTypeExpression"), doubleArrayType);
-		SymbolicExpression exception2Result = null;
-		String exception2Message = "Element 2 of values argument to denseArrayWrite has incompatible type.\nExpected: int[]\nSaw: int";
-		try {
-			exception2Result = universe.denseArrayWrite(
-					arrayTypeExpression,
-					Arrays.asList(new SymbolicExpression[] { null, null, two, null, two, null, null }));
-			
-			// Test for undesired universe.denseArrayWrite(x,x) success
-			if (exception2Result != null) { //if no exception is thrown...
-				assertTrue(false); //this test has failed (an exception SHOULD be thrown!)
-			}
-		} catch (Exception e) {
-			assertEquals(e.getClass(), SARLException.class); //test class name of thrown exception
-			assertEquals(e.getMessage(), exception2Message); //test message of thrown exception
-			assertTrue(e.getMessage().equals(exception2Message)); //double check
-		}
+	@Test(expected=SARLException.class)
+	public void testDenseArrayWrite_param2Exception() {
+		// Create SymbolicExpression of type ARRAY(int[]) (an array of int arrays) to fill with new values
+		SymbolicExpression doubleArrayTypeExpression = universe.symbolicConstant(universe.stringObject("doubleArrayTypeExpression"), doubleArrayType);
+		
+		// A List of expressions to write to new array (in dense format)
+		List<SymbolicExpression> expressionsToWrite = Arrays.asList(new SymbolicExpression[] { null, null, two, null, two, null, null });
+		
+		// Will throw a SARLException because input param #2 (List<SymbolicExpressions>) must be of type ARRAY
+		// "Element 2 of values argument to denseArrayWrite has incompatible type.\nExpected: int[]\nSaw: int"
+		universe.denseArrayWrite(doubleArrayTypeExpression, expressionsToWrite);
 	}
 
 	@Test
