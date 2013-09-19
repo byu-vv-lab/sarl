61fffc51204a936c36826bbd30b1314b40fa7430
danfried <danfried@7d651925-2d04-6a0e-8c93-cd1e20bcaab1>
2013-09-19 03:08:45 +0000

    added many more tests to BoundsObject still trying to wrap head around isRational/isReal va isIntegral and "Integral"

--- test/edu/udel/cis/vsl/sarl/ideal/simplify/BoundsObjectTest.java
+++ test/edu/udel/cis/vsl/sarl/ideal/simplify/BoundsObjectTest.java
@@ -9,11 +9,12 @@ import org.junit.Before;
 import org.junit.BeforeClass;
 import org.junit.Ignore;
 import org.junit.Test;
-import static org.hamcrest.CoreMatchers.*;
 
+import static org.hamcrest.CoreMatchers.*;
 import edu.udel.cis.vsl.sarl.IF.expr.NumericExpression;
 import edu.udel.cis.vsl.sarl.IF.expr.NumericSymbolicConstant;
 import edu.udel.cis.vsl.sarl.IF.expr.SymbolicExpression;
+import edu.udel.cis.vsl.sarl.IF.number.Number;
 import edu.udel.cis.vsl.sarl.IF.number.NumberFactory;
 import edu.udel.cis.vsl.sarl.IF.type.SymbolicType;
 import edu.udel.cis.vsl.sarl.expr.IF.NumericExpressionFactory;
@@ -34,6 +35,10 @@ public class BoundsObjectTest {
 	
 	private static SymbolicExpression symbExpr_xy;
 	
+	private static SymbolicExpression symbExpr_xxy;
+	
+	private static SymbolicExpression symbExpr_xyy;
+	
 	private static NumericSymbolicConstant x;
 
 	private static NumericSymbolicConstant y;
@@ -50,9 +55,28 @@ public class BoundsObjectTest {
 	
 	private static NumericExpression xy;
 	
+	private static NumericExpression xyy;
+	
+	private static NumericExpression xxy;
+	
 	private static BoundsObject boundObj;
 	
-	private static BoundsObject boundObj2; //strict upper and lower bounds
+	private static BoundsObject boundObj_Integral;
+	
+	private static BoundsObject boundObj_xxy_L_S; //strict lower bound of -2000 for x*x*y
+	
+	private static BoundsObject boundObj_xxy_L_NS; //non-strict lower bound of -2000 for x*x*y
+	
+	private static BoundsObject boundObj_xxy_U_S; //strict upper bound
+	
+	private static BoundsObject boundObj_xxy_U_NS; //non-strict upper bound
+	
+	private static BoundsObject boundObj_xxy_U_S_0; //boundOBject used to set another's lower and then be expanded on upward
+	
+	private static BoundsObject boundObj_xxy_L_S_0; //boundOBject used to set another's upper and then be expanded on downward
+	
+	
+	/*private static BoundsObject boundObj2; //strict upper and lower bounds
 	
 	private static BoundsObject boundObj3; //strict upper bound, non-strict lower
 	
@@ -67,7 +91,7 @@ public class BoundsObjectTest {
 	private static BoundsObject boundObj8; //no upper bound, strict lower
 	
 	private static BoundsObject boundObj9; //no upper bound, non-strict lower
-	
+*/	
 	private static NumberFactory numFact;
 	
 	//private static Number numBound;
@@ -80,6 +104,14 @@ public class BoundsObjectTest {
 	private static edu.udel.cis.vsl.sarl.IF.number.Number numBoundPos10000;
 	
 	private static edu.udel.cis.vsl.sarl.IF.number.Number numBound0;
+	
+	private static edu.udel.cis.vsl.sarl.IF.number.Number numBound0_int;
+	
+	private static edu.udel.cis.vsl.sarl.IF.number.Number numBound_neg2000_int;
+	
+	private static edu.udel.cis.vsl.sarl.IF.number.Number numBound10000_int;
+	
+	private static edu.udel.cis.vsl.sarl.IF.number.Number numBound10pt5;
 	
 	private static NumericExpression one, two, five;
 	
@@ -104,6 +136,11 @@ public class BoundsObjectTest {
 		symbExpr_xpy = xpy;
 		xy = preUniv.multiply(x, y);
 		symbExpr_xy = xy;
+		xxy = preUniv.multiply(xy, x);
+		symbExpr_xxy = xxy;
+		xyy = preUniv.multiply(xy, y);
+		symbExpr_xyy = xyy;
+		
 		numFact = preUniv.numberFactory();
 		//numBound = preUniv.
 //		numbFact = preUniv.  //numExprFact.numberFactory();
@@ -113,7 +150,18 @@ public class BoundsObjectTest {
 		numBoundNeg2000 = numFact.rational("-2000");
 		numBoundPos10000 = numFact.rational("10000");
 		numBound0 = numFact.rational("0");
+		numBound0_int = numFact.integer("0");
+		numBound_neg2000_int = numFact.integer("-2000");
+		numBound10000_int = numFact.integer("10000");
+		numBound10pt5 =numFact.rational("10.5");
 		boundObj = BoundsObject.newTightBound(symbExpr_xpy, numBound3);
+		boundObj_xxy_L_S = BoundsObject.newLowerBound(symbExpr_xxy, numBoundNeg2000, true);
+		boundObj_xxy_L_NS = BoundsObject.newLowerBound(symbExpr_xxy, numBoundNeg2000, false);
+		boundObj_xxy_U_S = BoundsObject.newUpperBound(symbExpr_xxy, numBoundPos10000, true);
+		boundObj_xxy_U_NS = BoundsObject.newUpperBound(symbExpr_xxy, numBoundPos10000, false);
+		boundObj_Integral= BoundsObject.newLowerBound(symbExpr_xxy, numBoundNeg2000, true); //begins with strict lower
+		boundObj_xxy_U_S_0 = BoundsObject.newUpperBound(symbExpr_xxy, numBound0, true);
+		boundObj_xxy_L_S_0 = BoundsObject.newLowerBound(symbExpr_xxy, numBound0, true);
 	}
 	
 	@Before
@@ -133,6 +181,15 @@ public class BoundsObjectTest {
 		out.println("boundObj = " + boundObj.toString() + " boundObjEquals = " + boundObjEquals.toString());
 		out.println("boundObj(xpy,3) equals? boundObjEquals(xpy, 3): " + boundObj.equals(boundObjEquals));
 		assertEquals(numBound3, boundObj.constant());
+		
+		//test on .equals for unequal objects, on basis of strict vs non-strict (lower bound)
+		assertThat(boundObj_xxy_L_S, is(not(boundObj_xxy_L_NS))); //expected to pass, as these objects are not equal
+		assertFalse(boundObj_xxy_L_S.equals(boundObj_xxy_L_NS));
+		BoundsObject bObjEq_L_S = boundObj_xxy_L_S;
+		BoundsObject bObjEq_L_NS = boundObj_xxy_L_NS;
+		//equals l-bound, Strict
+		assertTrue(boundObj_xxy_L_S.equals(bObjEq_L_S));
+		assertTrue(boundObj_xxy_L_NS.equals(bObjEq_L_NS));
 		
 		//test on .equals for unequal objects, on basis of expr
 		boundObjEquals = BoundsObject.newTightBound(symbExpr_xy, numBound3);
@@ -159,14 +216,85 @@ public class BoundsObjectTest {
 		//test on isConsistent() method 
 		assertEquals(true, boundObj.isConsistent());
 		assertEquals(true, boundObjEquals.isConsistent());
+		assertEquals(true, boundObj_xxy_L_S.isConsistent());
+		assertEquals(true, boundObj_xxy_L_NS.isConsistent());
+		out.println("lower, strict is constant???: " + boundObj_xxy_L_S.isConsistent());
+		out.println("lower, non-strict is constant???: " + boundObj_xxy_L_NS.isConsistent());
 		
 		//test on isReal and isIntegral methods
 		out.println("is real?:  " + boundObj.isReal());
 		assertEquals(false, boundObj.isIntegral());
 		assertEquals(true, boundObj.isReal());
+		out.println("is real?:  " + boundObj_xxy_L_S.isReal());
+		assertEquals(false, boundObj_xxy_L_S.isIntegral());
+		assertEquals(true, boundObj_xxy_L_S.isReal());
+		//BoundsObject boundObjIntegral = BoundsObject.
+		//boundObj_Integral.
 		
 		//test on .equals method for a second object that is not an instance of BoundsObject
-		assertFalse(boundObj.equals(numBound3));
+		assertEquals(false, boundObj.equals(numBound3));
+		
+	//tests to alter bounds for coverage of setting/restricting upper/lower
+		
+	    //boundObj_Integral.enlargeTo(boundObj_xxy_U_S);
+		//out.println("EXPECTED UPPER: " + boundObj_xxy_U_S.upper);
+		//out.println("NEW UPPER: " + boundObj_Integral.upper);
+		boundObj_Integral.restrictUpper(numBoundPos10000, true);
+			//unsure why the below test fails is asserting Equals
+			//good place for javadoc update!!!
+				//assertNotEquals(boundObj_Integral.upper(), boundObj_xxy_U_S.upper());
+		assertEquals(boundObj_Integral.upper(), boundObj_xxy_U_S.upper());
+		
+		//trying to get an integral boundObj
+		BoundsObject boundObjInt = BoundsObject.newUpperBound(xpy, numBound10000_int, true);
+		BoundsObject boundObjInt2 = boundObjInt.clone();
+		assertEquals(boundObjInt, boundObjInt2);
+		boundObjInt.restrictLower(numBound_neg2000_int, true);
+			//numBound0_int = numFact.integer("0");
+			//numBound_neg2000_int = numFact.integer("-2000");
+			//numBound10000_int = numFact.integer("10000");
+		//boundObjInt.restrictLower(numBound_neg2000_int, true);
+		//boundObjInt.restrictUpper(numBound10000_int, true);
+		assertEquals(true, boundObjInt.isReal());
+		
+		//testing coverage for enlargeLower() and restrictLower()
+		BoundsObject boundObjEL = BoundsObject.newLowerBound(xpy, numBound0_int, true);
+		BoundsObject boundObjELower = BoundsObject.newLowerBound(xpy, numBound_neg2000_int, true);
+		out.println("Current boundObjEL bounds: " + boundObjEL.toString());
+		boundObjEL.enlargeTo(boundObjELower);
+		out.println("boundObjEL bounds after enlargement: " + boundObjEL.toString());
+		boundObjEL.restrictLower(numBound0_int, true);
+		out.println("boundObjEL bounds after restriction back to 0: " + boundObjEL.toString());
+		boundObjEL.restrictLower(numBound10000_int, true);
+		out.println("boundObjEL bounds after restriction up to 10000: " + boundObjEL.toString());
+		boundObjEL.makeConstant(numBound0_int);
+		out.println("boundObjEL bounds after making const @ (int)0: " + boundObjEL.toString());
+		boundObjEL.makeConstant(numBound0); //redundant, see above statement
+		out.println("boundObjEL bounds after making const @ (real/rational)0: " + boundObjEL.toString());
+		boundObjEL.makeConstant(numBound10pt5);
+		out.println("boundObjEL bounds after making const @ 10.5: " + boundObjEL.toString());
+		
+		//testing coverage for enlargeUpper()
+		BoundsObject boundObjEU = BoundsObject.newUpperBound(xpy, numBound0_int, true);
+		BoundsObject boundObjEUpper = BoundsObject.newUpperBound(xpy, numBound10000_int, true);
+		out.println();
+		out.println("Current boundObjEU bounds: " + boundObjEU.toString());
+		boundObjEU.enlargeTo(boundObjEUpper);
+		out.println("boundObjEU bounds after enlargement: " + boundObjEU.toString());
+		out.println();
+		
+		//further toString() testing
+		BoundsObject boundObjL = BoundsObject.newLowerBound(xpy, numBound0, false);
+		out.println("Expecting null upper bound.....: " + boundObjL.toString());
+		
+		//assertEquals(true, boundObj_Integral.isIntegral());
+		out.println("Current integral bounds: " + boundObj_Integral.toString());
+		boundObj_Integral.restrictTo(boundObj_xxy_U_S_0);
+		out.println("Attempted integral bounds (after using restrictTo(~U=0): " + boundObj_Integral.toString());
+		boundObj_Integral.enlargeTo(boundObj_xxy_U_S);
+		
+		//boundObj_Integral.restrictTo(boundObj_xxy_U_S);
+		out.println("Attempted integral bounds (after using restrictTo(): " + boundObj_Integral.toString());
 		
 		//test on Assertions in BoundsObject, when the constructor receives a null expr
 		boolean gotError = false;
@@ -201,6 +329,8 @@ public class BoundsObjectTest {
 		assertEquals(numBound3, boundObj.upper());
 		out.println("lower: " + boundObj.strictUpper());
 		assertEquals(false, boundObj.strictUpper());
+		assertEquals(true, boundObj_xxy_L_S.strictLower());
+		assertEquals(true, boundObj_xxy_U_S.strictUpper());
 		//trivial to force new report of coverage
 		
 
