8c4c733d18f51069886a57159ba2f81698b6b448
danfried <danfried@7d651925-2d04-6a0e-8c93-cd1e20bcaab1>
2013-09-24 21:41:46 +0000

    new test class for testing of IdentitySimplifier

---
+++ test/edu/udel/cis/vsl/sarl/simplify/SimplifyCreationTest.java
@@ -0,0 +1,57 @@
+/* Copyright 2013 Stephen F. Siegel, University of Delaware
+ */
+package edu.udel.cis.vsl.sarl.simplify;
+
+import edu.udel.cis.vsl.sarl.ideal.simplify.CommonObjects;
+import static edu.udel.cis.vsl.sarl.ideal.simplify.CommonObjects.*;
+import static org.junit.Assert.*;
+
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+
+import edu.udel.cis.vsl.sarl.ideal.simplify.CommonObjects;
+
+/**
+ * @author danfried
+ *
+ */
+public class SimplifyCreationTest {
+
+	/**
+	 * @throws java.lang.Exception
+	 */
+	@BeforeClass
+	public static void setUpBeforeClass() throws Exception {
+		CommonObjects.setUp();
+	}
+
+	/**
+	 * @throws java.lang.Exception
+	 */
+	@AfterClass
+	public static void tearDownAfterClass() throws Exception {
+	}
+
+	/**
+	 * @throws java.lang.Exception
+	 */
+	@Before
+	public void setUp() throws Exception {
+	}
+
+	/**
+	 * @throws java.lang.Exception
+	 */
+	@After
+	public void tearDown() throws Exception {
+	}
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
