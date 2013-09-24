54dc115784e929cd5da2ae5ad530af10f10b1d01
danfried <danfried@7d651925-2d04-6a0e-8c93-cd1e20bcaab1>
2013-09-24 22:19:09 +0000

    concludes coverage of Simplify.java

--- test/edu/udel/cis/vsl/sarl/simplify/SimplifyCreationTest.java
+++ test/edu/udel/cis/vsl/sarl/simplify/SimplifyCreationTest.java
@@ -4,23 +4,25 @@ package edu.udel.cis.vsl.sarl.simplify;
 
 import edu.udel.cis.vsl.sarl.ideal.simplify.CommonObjects;
 import static edu.udel.cis.vsl.sarl.ideal.simplify.CommonObjects.*;
-import static org.junit.Assert.*;
-
 import org.junit.After;
 import org.junit.AfterClass;
 import org.junit.Before;
 import org.junit.BeforeClass;
 import org.junit.Test;
 
-import edu.udel.cis.vsl.sarl.ideal.simplify.CommonObjects;
+import edu.udel.cis.vsl.sarl.simplify.IF.Simplifier;
+import edu.udel.cis.vsl.sarl.simplify.IF.SimplifierFactory;
 
 /**
  * @author danfried
+ * Tests the class and two methods of Simplify.java
  *
  */
 public class SimplifyCreationTest {
 
 	/**
+	 * Calls the setup() methed of CommonObjects under the 
+	 * test...ideal.simplify package
 	 * @throws java.lang.Exception
 	 */
 	@BeforeClass
@@ -49,9 +51,28 @@ public class SimplifyCreationTest {
 	public void tearDown() throws Exception {
 	}
 
+	/**
+	 * Test on instantiation of a Simplify class object
+	 */
 	@Test
-	public void test() {
-		fail("Not yet implemented");
+	public void testCreation() {
+		Simplify simplifier = new Simplify();
+	}
+	
+	/**
+	 * 
+	 */
+	@Test
+	public void testIdentitySimplifier(){
+		Simplifier simplifier = Simplify.identitySimplifier(getPreUniv(), CommonObjects.getXeq5());
+	}
+	
+	/**
+	 * 
+	 */
+	@Test
+	public void simplifierFactoryTest(){
+		SimplifierFactory simplifierFactory = Simplify.newIdentitySimplifierFactory(getPreUniv());
 	}
 
 }
