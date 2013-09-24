59bb257d264c1d0bf107ce023a9e9ef5cd9b239c
danfried <danfried@7d651925-2d04-6a0e-8c93-cd1e20bcaab1>
2013-09-24 21:17:59 +0000

    added more SARL objects to CommonObjects in ideal.simplify test package

--- test/edu/udel/cis/vsl/sarl/ideal/simplify/CommonObjects.java
+++ test/edu/udel/cis/vsl/sarl/ideal/simplify/CommonObjects.java
@@ -2,15 +2,19 @@ package edu.udel.cis.vsl.sarl.ideal.simplify;
 
 import java.io.PrintStream;
 
+import edu.udel.cis.vsl.sarl.IF.expr.BooleanExpression;
 import edu.udel.cis.vsl.sarl.IF.expr.NumericExpression;
 import edu.udel.cis.vsl.sarl.IF.expr.NumericSymbolicConstant;
 import edu.udel.cis.vsl.sarl.IF.expr.SymbolicExpression;
 import edu.udel.cis.vsl.sarl.IF.number.NumberFactory;
 import edu.udel.cis.vsl.sarl.IF.type.SymbolicType;
 import edu.udel.cis.vsl.sarl.expr.IF.NumericExpressionFactory;
+import edu.udel.cis.vsl.sarl.ideal.Ideal;
+import edu.udel.cis.vsl.sarl.ideal.IF.IdealFactory;
 import edu.udel.cis.vsl.sarl.preuniverse.PreUniverses;
 import edu.udel.cis.vsl.sarl.preuniverse.IF.FactorySystem;
 import edu.udel.cis.vsl.sarl.preuniverse.IF.PreUniverse;
+import edu.udel.cis.vsl.sarl.simplify.IF.Simplifier;
 import edu.udel.cis.vsl.sarl.type.IF.SymbolicTypeFactory;
 
 public class CommonObjects {
@@ -27,9 +31,19 @@ public class CommonObjects {
 	
 	static NumericExpressionFactory numExprFact;
 	
+	static IdealFactory idealFactory;
+	
+	static IdealSimplifierFactory simplifierFactory;
+	
+	static Simplifier simp1ifier_xeq5;
+	
+	static BooleanExpression xeq5;
+	
 	static SymbolicExpression symbExpr_xpy; // x + y
 	
 	static SymbolicExpression symbExpr_xy; // x * y
+	
+	static SymbolicExpression symbExpr_xx; // x^2
 	
 	static SymbolicExpression symbExpr_xxy; // x^2 * y
 	
@@ -38,6 +52,8 @@ public class CommonObjects {
 	static SymbolicExpression symbExpr_xpyInt; // (int)x + (int)y
 	
 	static SymbolicExpression symbExpr_xyInt; // (int)x * (int)y
+	
+	static SymbolicExpression symbExpr_xxInt; // (int)x^2
 	
 	static SymbolicExpression symbExpr_xxyInt; // (int)x^2 * (int)y
 	
@@ -62,10 +78,10 @@ public class CommonObjects {
 	int1, int2, int3, int5; // -1, -2, -3, -5, 1, 2, 3, 5
 	
 	static NumericExpression xpy, xy, xx, x4th, threeX4th, xxy,
-	xyy;
+	xyy, onePxPxSqdP3x4th;
 	
 	static NumericExpression xpyInt, xyInt, xxInt, x4thInt, 
-	threeX4thInt, xxyInt, xyyInt, onePxPxSqdP3x4th;
+	threeX4thInt, xxyInt, xyyInt;
 	
 	static BoundsObject boundObj;
 	
@@ -73,12 +89,16 @@ public class CommonObjects {
 	num5, numNeg2000, num10000, num0, num10pt5;
 	
 	static edu.udel.cis.vsl.sarl.IF.number.Number num3Int,
-	num0Int, numNeg2000Int, num10000Int;
+	num5Int, num0Int, numNeg2000Int, num10000Int;
 
 	static void setUp() {
 		system = PreUniverses.newIdealFactorySystem();
 		preUniv = PreUniverses.newPreUniverse(system);
 		out = System.out;
+		idealFactory = (IdealFactory) system.expressionFactory()
+				.numericFactory();
+		simplifierFactory = (IdealSimplifierFactory) Ideal
+				.newIdealSimplifierFactory(idealFactory, preUniv);
 		ratNeg1 = preUniv.rational(-1);
 		ratNeg2 = preUniv.rational(-2);
 		ratNeg3 = preUniv.rational(-3);
@@ -95,6 +115,55 @@ public class CommonObjects {
 		int2 = preUniv.integer(2);
 		int3 = preUniv.integer(3);
 		int5 = preUniv.integer(5);
+		symbFactory = system.expressionFactory().typeFactory();
+		realType = preUniv.realType();
+		integerType = preUniv.integerType();
+		x = (NumericSymbolicConstant) preUniv.symbolicConstant(
+				preUniv.stringObject("x"), realType);
+		y = (NumericSymbolicConstant) preUniv.symbolicConstant(
+				preUniv.stringObject("y"), realType);
+		xInt = (NumericSymbolicConstant) preUniv.symbolicConstant(
+				preUniv.stringObject("xInt"), integerType);
+		yInt = (NumericSymbolicConstant) preUniv.symbolicConstant(
+				preUniv.stringObject("yInt"), integerType);
+		xeq5 = preUniv.equals(x, rat5);
+		simp1ifier_xeq5 = simplifierFactory.newSimplifier(xeq5);
+		//not sure if xsqd is necessary
+		//xsqd = preUniv.multiply(x, x);
+		xpy = preUniv.add(x, y);
+		symbExpr_xpy = xpy;
+		xy = preUniv.multiply(x, y);
+		symbExpr_xy = xy;
+		xx = preUniv.power(x, 2);
+		x4th = preUniv.power(xx, 2);
+		threeX4th = preUniv.multiply(rat3, x4th);
+		xxy = preUniv.multiply(xy, x);
+		symbExpr_xxy = xxy;
+		xyy = preUniv.multiply(xy, y);
+		symbExpr_xyy = xyy;
+		onePxPxSqdP3x4th = preUniv.add(rat1, preUniv.add(x, preUniv.add(xx, threeX4th)));
+		xpyInt = preUniv.add(xInt, yInt);
+		symbExpr_xpyInt = xpyInt;
+		xxInt = preUniv.multiply(xInt, xInt);
+		symbExpr_xxInt = xxInt;
+		xyInt = preUniv.multiply(xInt, yInt);
+		symbExpr_xyInt = xyInt;
+		xxyInt = preUniv.multiply(xyInt, xInt);
+		symbExpr_xxyInt = xxyInt;
+		xyyInt = preUniv.multiply(xyInt, yInt);
+		symbExpr_xyyInt = xyyInt;
+		numFact = preUniv.numberFactory();
+		num3 = numFact.rational("3");
+		num5 = numFact.rational("5");
+		numNeg2000 = numFact.rational("-2000");
+		num10000 = numFact.rational("10000");
+		num0 = numFact.rational("0");
+		num10pt5 = numFact.rational("10.5");
+		num3Int = numFact.integer("3");
+		num0Int = numFact.integer("0");
+		num5Int = numFact.integer("5");
+		numNeg2000Int = numFact.integer("-2000");
+		num10000Int = numFact.integer("10000");
 	}
 
 }
