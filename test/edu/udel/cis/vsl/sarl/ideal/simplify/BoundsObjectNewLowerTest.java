e160b4184be2d8b5e7733aac20715d066a5cc0d7
danfried <danfried@7d651925-2d04-6a0e-8c93-cd1e20bcaab1>
2013-09-25 22:56:57 +0000

    added new test class for coverage of BoundsObject.newLowerBound

---
+++ test/edu/udel/cis/vsl/sarl/ideal/simplify/BoundsObjectNewLowerTest.java
@@ -0,0 +1,46 @@
+/* Copyright 2013 Stephen F. Siegel, University of Delaware
+ */
+package edu.udel.cis.vsl.sarl.ideal.simplify;
+
+import static org.junit.Assert.*;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+
+/**
+ * @author danfried
+ *
+ */
+public class BoundsObjectNewLowerTest {
+
+	/**
+	 * @throws java.lang.Exception
+	 */
+	@BeforeClass
+	public static void setUpBeforeClass() throws Exception {
+		//CommonObjects.setUp();
+	}
+
+	/**
+	 * @throws java.lang.Exception
+	 */
+	@Before
+	public void setUp() throws Exception {
+	}
+
+	/**
+	 * @throws java.lang.Exception
+	 */
+	@After
+	public void tearDown() throws Exception {
+	}
+
+	@Test
+	public void test() {
+		//out.println("help!");
+		assertEquals(true, true);
+	}
+
+}
