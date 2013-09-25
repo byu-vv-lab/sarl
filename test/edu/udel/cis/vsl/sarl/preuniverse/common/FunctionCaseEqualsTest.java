037fde2a1ca3bc960102160e0e0c624cd3554b9b
gunjan <gunjan@7d651925-2d04-6a0e-8c93-cd1e20bcaab1>
2013-09-25 17:30:47 +0000

    Added a test for equals()  in FunctionCaseEqualsTest.java

--- test/edu/udel/cis/vsl/sarl/preuniverse/common/FunctionCaseEqualsTest.java
+++ test/edu/udel/cis/vsl/sarl/preuniverse/common/FunctionCaseEqualsTest.java
@@ -18,7 +18,9 @@ import org.junit.Test;
 import edu.udel.cis.vsl.sarl.IF.SARLException;
 import edu.udel.cis.vsl.sarl.IF.expr.BooleanExpression;
 import edu.udel.cis.vsl.sarl.IF.expr.NumericExpression;
+import edu.udel.cis.vsl.sarl.IF.expr.NumericSymbolicConstant;
 import edu.udel.cis.vsl.sarl.IF.expr.SymbolicExpression;
+import edu.udel.cis.vsl.sarl.IF.type.SymbolicFunctionType;
 import edu.udel.cis.vsl.sarl.IF.type.SymbolicTupleType;
 import edu.udel.cis.vsl.sarl.IF.type.SymbolicType;
 import edu.udel.cis.vsl.sarl.IF.type.SymbolicType.SymbolicTypeKind;
@@ -33,12 +35,16 @@ public class FunctionCaseEqualsTest {
 	private static PreUniverse universe;
 
 	private static SymbolicType realType, integerType;
+	
+	private static SymbolicExpression addition1, addition2;
+	
+	private static NumericSymbolicConstant x, y, z;
 
 	private static SymbolicType functionType1, functionType2, functionType3;
 	
 	private static SymbolicTypeSequence sequence1, sequence2, sequence3;
 	
-	private static BooleanExpression value1, value2, trueExpr, falseExpr;
+	private static BooleanExpression value1, value2, value, trueExpr, falseExpr;
 	
 	@BeforeClass
 	public static void setUpBeforeClass() throws Exception {
@@ -48,6 +54,10 @@ public class FunctionCaseEqualsTest {
 		realType = universe.realType();
 		trueExpr = universe.trueExpression();
 		falseExpr = universe.falseExpression();
+		addition1 = universe.add(universe.add(x,y),z);
+		addition2 = universe.add(universe.add(y,z), x);
+		
+		
 		
 	}
 
@@ -72,15 +82,26 @@ public class FunctionCaseEqualsTest {
 		sequence1 = tupleType1.sequence();
 		sequence2 = tupleType2.sequence();
 		sequence2 = tupleType3.sequence();
+		
 		functionType1 = universe.functionType(sequence1, realType);
 		functionType2 = universe.functionType(sequence2, integerType);
 		functionType3 = universe.functionType(sequence3, realType);
-		
 		value1 = universe.compatible(functionType1, functionType2);
 		value2 = universe.compatible(functionType1, functionType3);
 		
 		assertEquals(falseExpr, value1);
 		assertEquals(trueExpr, value2);
 	}
+	
+	@Test
+	public void FunctionCaseEqualsTest2(){
+		
+		addition1 = universe.add(universe.add(x,y),z);
+		addition2 = universe.add(universe.add(y,z),x);
+		value = universe.equals(addition1, addition2);
+		assertEquals (trueExpr, value);
+		
+	}
 }
+
 
