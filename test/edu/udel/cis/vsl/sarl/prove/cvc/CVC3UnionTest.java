d942ee1da0601c8f5ea8891ef30d88391c6b067d
siegel <siegel@7d651925-2d04-6a0e-8c93-cd1e20bcaab1>
2013-09-25 17:43:12 +0000

    Fixing union type test.

--- test/edu/udel/cis/vsl/sarl/prove/cvc/CVC3UnionTest.java
+++ test/edu/udel/cis/vsl/sarl/prove/cvc/CVC3UnionTest.java
@@ -47,7 +47,7 @@ public class CVC3UnionTest {
 			.falseExpression();
 	private static SymbolicExpression unionArray;
 	// constants
-	
+
 	// Instance fields: instantiated before each test is run...
 	private TheoremProverFactory proverFactory;
 	private CVC3TheoremProver cvcProver;
@@ -64,77 +64,100 @@ public class CVC3UnionTest {
 		cvcProver = (CVC3TheoremProver) proverFactory
 				.newProver(booleanExprTrue);
 		vc = cvcProver.validityChecker();
-		
+
 		// make a union to test
 		// union of int, real, bool
-		intRealBoolUnion = universe.unionType(universe
-				.stringObject("union1"), Arrays.asList(new SymbolicType[]
-						{intType, realType, boolType}));
+		intRealBoolUnion = universe
+				.unionType(
+						universe.stringObject("union1"),
+						Arrays.asList(new SymbolicType[] { intType, realType,
+								boolType }));
 		// union array type
 		unionArrayType = universe.arrayType(intRealBoolUnion);
-		
+
 		// union array expression to write values to
-		unionArray = universe
-				.symbolicConstant(universe.stringObject("unionArray"),
-						unionArrayType);
+		unionArray = universe.symbolicConstant(
+				universe.stringObject("unionArray"), unionArrayType);
 		// add true bool
-		unionArray = universe.arrayWrite(unionArray, 
-				universe.integer(0), // index of array
-				universe.unionInject(intRealBoolUnion,
-						universe.intObject(2), // 2 is index of type (bool)
+		unionArray = universe.arrayWrite(unionArray, universe.integer(0), // index
+																			// of
+																			// array
+				universe.unionInject(intRealBoolUnion, universe.intObject(2), // 2
+																				// is
+																				// index
+																				// of
+																				// type
+																				// (bool)
 						booleanExprTrue));
 		// add 10.5 real
-		unionArray = universe.arrayWrite(unionArray, 
-				universe.integer(1), // index of array
-				universe.unionInject(intRealBoolUnion,
-						universe.intObject(1), // 1 is index of type (real)
+		unionArray = universe.arrayWrite(unionArray, universe.integer(1), // index
+																			// of
+																			// array
+				universe.unionInject(intRealBoolUnion, universe.intObject(1), // 1
+																				// is
+																				// index
+																				// of
+																				// type
+																				// (real)
 						tenAndHalf));
 		// add 0 int
-		unionArray = universe.arrayWrite(unionArray, 
-				universe.integer(2), // index of array
-				universe.unionInject(intRealBoolUnion,
-						universe.intObject(0), // 0 is index of type (int)
+		unionArray = universe.arrayWrite(unionArray, universe.integer(2), // index
+																			// of
+																			// array
+				universe.unionInject(intRealBoolUnion, universe.intObject(0), // 0
+																				// is
+																				// index
+																				// of
+																				// type
+																				// (int)
 						universe.integer(0)));
 	}
 
 	@After
 	public void tearDown() throws Exception {
 	}
-	
+
 	@Test
 	public void testTranslateUnionInject() {
 		// inject bool false
-		SymbolicExpression injectBoolFalse = universe
-				.unionInject(intRealBoolUnion, universe.intObject(2), booleanExprFalse);
-		
-		SymbolicExpression injectBoolFalse2 = expressionFactory
-				.expression(SymbolicOperator.UNION_INJECT, intRealBoolUnion,
-						universe.intObject(1), tenAndHalf);
-		
+		SymbolicExpression injectBoolFalse = universe.unionInject(
+				intRealBoolUnion, universe.intObject(2), booleanExprFalse);
+
+		SymbolicExpression injectBoolFalse2 = expressionFactory.expression(
+				SymbolicOperator.UNION_INJECT, intRealBoolUnion,
+				universe.intObject(1), tenAndHalf);
+
 		// TODO report/figure out bug related to translate (unknown constructor)
 		Expr translateResult = cvcProver.translate(injectBoolFalse2);
 		out.println(translateResult);
 	}
-	
+
 	@Test
 	public void testTranslateUnionExtract() {
 		// inject statement
-		SymbolicExpression injectedTenandHalf = universe
-				.unionInject(intRealBoolUnion, universe.intObject(1), tenAndHalf);
-		
+		SymbolicExpression injectedTenandHalf = universe.unionInject(
+				intRealBoolUnion, universe.intObject(1), tenAndHalf);
+
 		// extract 10.5 real
-		
+
 		// this version translates as concrete, so no coverage for union extract
-		SymbolicExpression extractReal = universe
-				.unionExtract(universe.intObject(1), injectedTenandHalf);
-		
-		// this version gives an error on translateUnionExtract (unknown constructor)
-		SymbolicExpression extractReal2 = expressionFactory
-				.expression(SymbolicOperator.UNION_EXTRACT, intRealBoolUnion,
-						universe.intObject(1), tenAndHalf);
-		
+		SymbolicExpression extractReal = universe.unionExtract(
+				universe.intObject(1), injectedTenandHalf);
+
+		// this version gives an error on translateUnionExtract (unknown
+		// constructor)
+		// SymbolicExpression extractReal2 = expressionFactory
+		// .expression(SymbolicOperator.UNION_EXTRACT, intRealBoolUnion,
+		// universe.intObject(1), tenAndHalf);
+		cvcProver.translateType(intRealBoolUnion);
+
+		SymbolicExpression extractReal2 = expressionFactory.expression(
+				SymbolicOperator.UNION_EXTRACT, realType,
+				universe.intObject(1), injectedTenandHalf);
+
 		Expr translateResult = cvcProver.translate(extractReal2);
 		out.println("Extracted type: " + translateResult.getType());
-		assertEquals(translateResult.getType(), cvcProver.translateType(realType));
+		assertEquals(translateResult.getType(),
+				cvcProver.translateType(realType));
 	}
 }
