bc50d4e24d3bc4ec6ca566a42d5d73f37d3c84de
danfried <danfried@7d651925-2d04-6a0e-8c93-cd1e20bcaab1>
2013-09-26 21:44:27 +0000

    incorporated BoundsObjectMakeConstantTest into this suite

--- test/edu/udel/cis/vsl/sarl/ideal/simplify/BoundsObjectSuite.java
+++ test/edu/udel/cis/vsl/sarl/ideal/simplify/BoundsObjectSuite.java
@@ -5,7 +5,7 @@ import org.junit.runners.Suite;
 import org.junit.runners.Suite.SuiteClasses;
 
 @RunWith(Suite.class)
-@SuiteClasses({ BoundsObjectIsRealTest.class, BoundsObjectEqualsTest.class, BoundsObjectCloneTest.class, BoundsObjectSetUpperTest.class, BoundsObjectSetLowerTest.class, BoundsObjectNewLowerTest.class, BoundsObjectNewUpperTest.class })
+@SuiteClasses({ BoundsObjectMakeConstantTest.class, BoundsObjectMakeConstantTest.class, BoundsObjectIsRealTest.class, BoundsObjectEqualsTest.class, BoundsObjectCloneTest.class, BoundsObjectSetUpperTest.class, BoundsObjectSetLowerTest.class, BoundsObjectNewLowerTest.class, BoundsObjectNewUpperTest.class })
 public class BoundsObjectSuite {
 
 }
