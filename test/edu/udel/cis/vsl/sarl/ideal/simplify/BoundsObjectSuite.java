ec95dbd175d0058a8052291693b43551d745f372
danfried <danfried@7d651925-2d04-6a0e-8c93-cd1e20bcaab1>
2013-09-26 01:57:54 +0000

    incorporated BoundsObjectSetUpperTest in test classes run by BoundsObjectSuite

--- test/edu/udel/cis/vsl/sarl/ideal/simplify/BoundsObjectSuite.java
+++ test/edu/udel/cis/vsl/sarl/ideal/simplify/BoundsObjectSuite.java
@@ -5,7 +5,7 @@ import org.junit.runners.Suite;
 import org.junit.runners.Suite.SuiteClasses;
 
 @RunWith(Suite.class)
-@SuiteClasses({ BoundsObjectSetLowerTest.class, BoundsObjectNewLowerTest.class, BoundsObjectNewUpperTest.class })
+@SuiteClasses({ BoundsObjectSetUpperTest.class, BoundsObjectSetLowerTest.class, BoundsObjectNewLowerTest.class, BoundsObjectNewUpperTest.class })
 public class BoundsObjectSuite {
 
 }
