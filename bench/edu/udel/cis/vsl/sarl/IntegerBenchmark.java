c3c6fd99d4ee43fec8789d0439ebc4b2f136627a
blutuu <blutuu@7d651925-2d04-6a0e-8c93-cd1e20bcaab1>
2013-09-26 20:37:33 +0000

    Added benchmarks for: forallInt() existsInt()

---
+++ bench/edu/udel/cis/vsl/sarl/IntegerBenchmark.java
@@ -0,0 +1,44 @@
+package edu.udel.cis.vsl.sarl;
+
+import edu.udel.cis.vsl.sarl.IF.SymbolicUniverse;
+import edu.udel.cis.vsl.sarl.IF.expr.BooleanExpression;
+import edu.udel.cis.vsl.sarl.IF.expr.NumericExpression;
+import edu.udel.cis.vsl.sarl.IF.expr.NumericSymbolicConstant;
+import edu.udel.cis.vsl.sarl.IF.expr.SymbolicConstant;
+import edu.udel.cis.vsl.sarl.IF.type.SymbolicType;
+import edu.udel.cis.vsl.sarl.preuniverse.PreUniverses;
+import edu.udel.cis.vsl.sarl.preuniverse.IF.FactorySystem;
+import edu.udel.cis.vsl.sarl.preuniverse.IF.PreUniverse;
+
+public class IntegerBenchmark {
+	static FactorySystem system = PreUniverses.newIdealFactorySystem();
+	private static PreUniverse universe = PreUniverses.newPreUniverse(system);
+	
+	static SymbolicConstant index = universe.symbolicConstant(
+			universe.stringObject("name"), universe.integerType());
+	static NumericExpression low = universe.integer(999);
+	static NumericExpression high = universe.integer(2000);
+	static BooleanExpression trueExp = universe.bool(true);
+
+	public static void main(String[] args) {
+		long startTime = System.nanoTime(), stopTime;
+		double totalTime;
+		
+		// This is a benchmark for the 'forallInt()' method
+		System.out.println("Benchmark for 'forallInt()'");
+		universe.forallInt((NumericSymbolicConstant)index, 
+				low, high, trueExp);
+		stopTime = System.nanoTime();
+		totalTime = ((double) (stopTime - startTime)) / 1000000000.0;
+		System.out.println("Time (s): " + totalTime);
+		
+		// This is a benchmark for the 'existsInt()' method
+		System.out.println("\nBenchmark for 'existsInt()");
+		universe.existsInt((NumericSymbolicConstant)index,
+				low, high, trueExp);
+		stopTime = System.nanoTime();
+		totalTime = ((double) (stopTime - startTime)) / 1000000000.0;
+		System.out.println("Time (s): " + totalTime);
+	}
+
+}
