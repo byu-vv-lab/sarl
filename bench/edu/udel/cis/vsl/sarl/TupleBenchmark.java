eb5b5def7d5a1432552dac77b80258a49d9bb645
malsulmi <malsulmi@7d651925-2d04-6a0e-8c93-cd1e20bcaab1>
2013-09-26 01:28:27 +0000

    Adding TupleBenchmark

---
+++ bench/edu/udel/cis/vsl/sarl/TupleBenchmark.java
@@ -0,0 +1,94 @@
+package edu.udel.cis.vsl.sarl;
+
+import java.util.Arrays;
+import java.util.LinkedList;
+
+import edu.udel.cis.vsl.sarl.IF.SymbolicUniverse;
+import edu.udel.cis.vsl.sarl.IF.expr.NumericExpression;
+import edu.udel.cis.vsl.sarl.IF.expr.SymbolicExpression;
+import edu.udel.cis.vsl.sarl.IF.type.SymbolicArrayType;
+import edu.udel.cis.vsl.sarl.IF.type.SymbolicTupleType;
+import edu.udel.cis.vsl.sarl.IF.type.SymbolicType;
+import edu.udel.cis.vsl.sarl.IF.type.SymbolicUnionType;
+import edu.udel.cis.vsl.sarl.expr.IF.ExpressionFactory;
+import edu.udel.cis.vsl.sarl.preuniverse.PreUniverses;
+import edu.udel.cis.vsl.sarl.preuniverse.IF.FactorySystem;
+
+public class TupleBenchmark {
+	/*
+	 * 
+	 * written by Mohammad Alsulmi
+	 * 
+	 */
+	public final static SymbolicUniverse universe = SARL.newIdealUniverse();
+	public final static FactorySystem system = PreUniverses.newIdealFactorySystem();
+
+	public final static SymbolicType integerType = universe.integerType();
+	
+	public final static ExpressionFactory expressionFactory = system.expressionFactory();
+	
+	public static SymbolicTupleType tupleType;
+	
+	public static SymbolicExpression tuple;
+	
+
+
+	public static void main(String[] args) {
+	
+		LinkedList<SymbolicType> members = null;
+		LinkedList<SymbolicExpression> elements = null;
+		SymbolicType arrayMembers[];
+		NumericExpression arrayElements[];
+		int size;
+		double totalTime;
+		
+		int maxSize =(int) Math.pow(2,20);
+		System.out.println("Testing of tuple creation using linked lists");
+		for(int i = 1; i<=maxSize; i = i*2){
+			members = new LinkedList<>();
+			elements = new LinkedList<>();
+			size = i;
+			long startTime, stopTime;
+			startTime = System.nanoTime();
+			
+			for(int j = 0;j < size ; j++){
+				members.add(integerType);
+				elements.add(universe.integer(j));
+			}
+			tupleType = universe.tupleType(universe.stringObject("type1"), members);
+			tuple = universe.tuple(tupleType, elements);
+			stopTime = System.nanoTime();
+			totalTime =((double) (stopTime - startTime))/1000000000.0;
+			
+			System.out.println("Total Time in seconds: " + totalTime + " for size: "+size);
+			
+		}
+		
+		System.out.println("Testing of tuple creation using arrays");
+		for(int i = 1; i<=maxSize; i = i*2){
+						
+			size = i;
+			arrayMembers = new SymbolicType[size];
+			arrayElements = new NumericExpression[size];
+			long startTime, stopTime;
+
+			startTime = System.nanoTime();
+			
+			for(int j = 0;j < size ; j++){
+				arrayMembers[j] = integerType;
+				arrayElements[j] = universe.integer(j);
+			}
+			tupleType = universe.tupleType(universe.stringObject("type1"), Arrays.asList(arrayMembers));
+			tuple = universe.tuple(tupleType, Arrays.asList(arrayElements));
+			stopTime = System.nanoTime();
+			totalTime =((double) (stopTime - startTime))/1000000000.0;
+			
+			System.out.println("Total Time in seconds: " + totalTime + " for size: "+size);
+			
+		}
+
+
+		
+	}
+
+}
