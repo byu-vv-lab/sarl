f5d2e5de0beee7fb1185637bc02b93d0b470b0d2
alali <alali@7d651925-2d04-6a0e-8c93-cd1e20bcaab1>
2013-09-26 20:56:46 +0000

    Tested CommonSymbolicTypeSequence

--- test/edu/udel/cis/vsl/sarl/type/common/CommonSymbolicTypeSequenceTest.java
+++ test/edu/udel/cis/vsl/sarl/type/common/CommonSymbolicTypeSequenceTest.java
@@ -2,14 +2,25 @@ package edu.udel.cis.vsl.sarl.type.common;
 
 import static org.junit.Assert.*;
 
+import java.util.ArrayList;
+
 import org.junit.After;
 import org.junit.AfterClass;
 import org.junit.Before;
 import org.junit.BeforeClass;
 import org.junit.Test;
 
+import edu.udel.cis.vsl.sarl.IF.type.SymbolicIntegerType.IntegerKind;
+import edu.udel.cis.vsl.sarl.IF.type.SymbolicRealType.RealKind;
+
 public class CommonSymbolicTypeSequenceTest {
 
+	CommonSymbolicTypeSequence typeSequence, typeSequence2;
+	CommonSymbolicIntegerType idealIntKind, idealIntKind2, boundedIntKind;
+	CommonSymbolicRealType idealRealKind, floatRealKind;
+	TypeComparator typeComparator;
+	ArrayList<CommonSymbolicType> types;
+	
 	@BeforeClass
 	public static void setUpBeforeClass() throws Exception {
 	}
@@ -20,6 +31,19 @@ public class CommonSymbolicTypeSequenceTest {
 
 	@Before
 	public void setUp() throws Exception {
+		
+		typeComparator = new TypeComparator();
+		idealIntKind = new CommonSymbolicIntegerType(IntegerKind.IDEAL);
+		boundedIntKind = new CommonSymbolicIntegerType(IntegerKind.BOUNDED);
+		idealRealKind = new CommonSymbolicRealType(RealKind.IDEAL);
+		floatRealKind = new CommonSymbolicRealType(RealKind.FLOAT);
+		types = new ArrayList<CommonSymbolicType>();
+		types.add(idealRealKind);
+		types.add(floatRealKind);
+		types.add(idealIntKind);
+		types.add(boundedIntKind);
+		typeSequence = new CommonSymbolicTypeSequence(types);
+		typeSequence2 = new CommonSymbolicTypeSequence(types);
 	}
 
 	@After
@@ -29,21 +53,51 @@ public class CommonSymbolicTypeSequenceTest {
 
 	@Test
 	public void testComputeHashCode() {
-		//fail("Not yet implemented");
+		assertEquals(typeSequence.computeHashCode(), typeSequence2.computeHashCode());
 	}
-/*
+	
+	@Test
+	public void testNumTypes() {
+		assertEquals(typeSequence.numTypes(), 4);
+	}
+	
+	@Test
+	public void testGetType() {
+		assertTrue(typeSequence.getType(2) instanceof CommonSymbolicIntegerType);
+		assertTrue(typeSequence.getType(3) instanceof CommonSymbolicIntegerType);
+		assertTrue(typeSequence.getType(0) instanceof CommonSymbolicRealType);
+		assertTrue(typeSequence.getType(1) instanceof CommonSymbolicRealType);
+	}
+	
+
 	@Test
 	public void testIntrinsicEquals() {
-		fail("Not yet implemented");
+		assertTrue(typeSequence.intrinsicEquals(typeSequence2));
+		//System.out.println(typeSequence.toString());
 	}
+	
+	
+	/**
+	 * a SymbolicTypeSequence is a finite and ordered sequence of SymbolicTypes
+	 * so the 1 element is < the 2 
+	 * (maybe == if similar elements, but here I added distinct elements)
+	 * and the 2 is < 3
+	 * we can test that using compare() in TypeComparator.
+	 */
+	@Test
+	public void testTypeComparator(){
+		/* Test won't work because the ordering isn't done automatically.
+		assertEquals(typeComparator.compare(typeSequence.getType(0), typeSequence.getType(1)), -1);
+		assertEquals(typeComparator.compare(typeSequence.getType(1), typeSequence.getType(2)), -1);
+		assertEquals(typeComparator.compare(typeSequence.getType(2), typeSequence.getType(3)), -1);
+		assertEquals(typeComparator.compare(typeSequence.getType(3), typeSequence.getType(4)), -1);
 
+	*/
+
+	}
+	/*
 	@Test
 	public void testCanonizeChildren() {
-		fail("Not yet implemented");
-	}
-
-	@Test
-	public void testToString() {
 		fail("Not yet implemented");
 	}
 
@@ -57,20 +111,6 @@ public class CommonSymbolicTypeSequenceTest {
 		fail("Not yet implemented");
 	}
 
-	@Test
-	public void testIterator() {
-		fail("Not yet implemented");
-	}
-
-	@Test
-	public void testNumTypes() {
-		fail("Not yet implemented");
-	}
-
-	@Test
-	public void testGetType() {
-		fail("Not yet implemented");
-	}
 
 	@Test
 	public void testToStringBuffer() {
