34a9043fd7777d5c94852316c46ba821187aac90
siegel <siegel@7d651925-2d04-6a0e-8c93-cd1e20bcaab1>
2013-10-10 22:22:36 +0000

    CollectionComparatorStub.java did not compile.  Commented out code and made it just return 0 as an emergency measure. Come on people, dont' check in code that breaks compilation!

--- test/edu/udel/cis/vsl/sarl/object/common/CollectionComparatorStub.java
+++ test/edu/udel/cis/vsl/sarl/object/common/CollectionComparatorStub.java
@@ -2,18 +2,18 @@ package edu.udel.cis.vsl.sarl.object.common;
 
 import java.util.Comparator;
 
-import edu.udel.cis.vsl.sarl.IF.expr.SymbolicExpression;
-import edu.udel.cis.vsl.sarl.collections.IF.SymbolicCollection;
+import edu.udel.cis.vsl.sarl.collections.IF.ExpressionStub;
 import edu.udel.cis.vsl.sarl.collections.common.BasicCollection;
 
 public class CollectionComparatorStub implements Comparator<BasicCollection> {
 
 	@Override
-	public int compare(BasicCollection, BasicCollection) {
-		String name1 = ((ExpressionStub) o1).toString();
-		String name2 = ((ExpressionStub) o2).toString();
-
-		return name1.compareTo(name2);
+	public int compare(BasicCollection o1, BasicCollection o2) {
+//		String name1 = ((ExpressionStub) o1).toString();
+//		String name2 = ((ExpressionStub) o2).toString();
+//
+//		return name1.compareTo(name2);
+		return 0;
 	}
 
 }
