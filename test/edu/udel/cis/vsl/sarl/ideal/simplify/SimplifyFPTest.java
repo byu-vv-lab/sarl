c2f949cb1e9e6ef7e6c15e1d40db0a95c150454f
danfried <danfried@7d651925-2d04-6a0e-8c93-cd1e20bcaab1>
2013-10-11 17:59:37 +0000

    new test class for idealSimplifier

---
+++ test/edu/udel/cis/vsl/sarl/ideal/simplify/SimplifyFPTest.java
@@ -0,0 +1,54 @@
+/* Copyright 2013 Stephen F. Siegel, University of Delaware
+ */
+package edu.udel.cis.vsl.sarl.ideal.simplify;
+
+import static edu.udel.cis.vsl.sarl.ideal.simplify.CommonObjects.*;
+import static org.junit.Assert.*;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+
+/**
+ * @author danfried
+ *
+ */
+public class SimplifyFPTest {
+
+	/**
+	 * Calls the setUp() method in CommonObjects to make use of consolidated SARL object 
+	 * declarations and initializations for testing of "Simplify" module.
+	 * @throws java.lang.Exception
+	 */
+	@BeforeClass
+	public static void setUpBeforeClass() throws Exception {
+		CommonObjects.setUp();
+		assumption = preUniv.lessThan(xInt, int0);
+				//preUniv.equals(preUniv.multiply(rat5,x), preUniv.multiply(y, y));
+		idealSimplifier = idealSimplifierFactory.newSimplifier(assumption);
+	}
+
+	/**
+	 * @throws java.lang.Exception
+	 */
+	@Before
+	public void setUp() throws Exception {
+	}
+
+	/**
+	 * @throws java.lang.Exception
+	 */
+	@After
+	public void tearDown() throws Exception {
+	}
+
+	@Test
+	public void test() {
+		out.println(idealSimplifier.simplifyExpression(xSqrLess1));
+		out.println(idealSimplifier.apply(xSqrP1));
+		//out.println("xx - 1 :  " + xSqrLess1.toString());
+		//IdealSimplifier.
+	}
+
+}
