171f88e1442382d0dcb4cdacdeadf9dedd3192ff
gunjan <gunjan@7d651925-2d04-6a0e-8c93-cd1e20bcaab1>
2013-10-16 20:32:35 +0000

    updated pureTypeTest.java

--- test/edu/udel/cis/vsl/sarl/preuniverse/common/PureTypeTest.java
+++ test/edu/udel/cis/vsl/sarl/preuniverse/common/PureTypeTest.java
@@ -6,10 +6,6 @@ package edu.udel.cis.vsl.sarl.preuniverse.common;
 
 import static org.junit.Assert.assertEquals;
 
-import java.io.PrintStream;
-import java.util.Arrays;
-import java.util.LinkedList;
-
 import org.junit.Before;
 import org.junit.BeforeClass;
 import org.junit.After;
@@ -17,16 +13,8 @@ import org.junit.AfterClass;
 import org.junit.Ignore;
 import org.junit.Test;
 
-import edu.udel.cis.vsl.sarl.SARL;
-import edu.udel.cis.vsl.sarl.IF.SARLException;
-import edu.udel.cis.vsl.sarl.IF.SymbolicUniverse;
-import edu.udel.cis.vsl.sarl.IF.expr.NumericExpression;
-import edu.udel.cis.vsl.sarl.IF.expr.NumericSymbolicConstant;
-import edu.udel.cis.vsl.sarl.IF.expr.SymbolicExpression;
-import edu.udel.cis.vsl.sarl.IF.type.SymbolicTupleType;
+import edu.udel.cis.vsl.sarl.IF.expr.SymbolicConstant;
 import edu.udel.cis.vsl.sarl.IF.type.SymbolicType;
-import edu.udel.cis.vsl.sarl.IF.type.SymbolicType.SymbolicTypeKind;
-import edu.udel.cis.vsl.sarl.IF.type.SymbolicTypeSequence;
 import edu.udel.cis.vsl.sarl.preuniverse.PreUniverses;
 import edu.udel.cis.vsl.sarl.preuniverse.IF.FactorySystem;
 import edu.udel.cis.vsl.sarl.preuniverse.IF.PreUniverse;
@@ -35,13 +23,7 @@ public class PureTypeTest {
 	
 	private static PreUniverse universe;
 
-	private static SymbolicType realType, integerType;
-
-	private static SymbolicType pureType;
-
-	SymbolicTypeKind three;
-
-	private static PrintStream out = System.out; 
+	private static SymbolicType realType, integerType, booleanType;
 	
 	
 	@BeforeClass
@@ -50,6 +32,8 @@ public class PureTypeTest {
 		universe = new CommonPreUniverse(test);
 		integerType = universe.integerType();
 		realType = universe.realType();
+		booleanType = universe.booleanType();
+		
 	}
 
 	@AfterClass
@@ -70,6 +54,15 @@ public class PureTypeTest {
 		SymbolicType pureType1 = universe.pureType(integerType);
 		SymbolicType pureType2 = universe.pureType(integerType);
 		assertEquals(pureType1.typeKind(), pureType2.typeKind());
+		
+	}
+	
+	@Test
+	public void pureTypeObjectKindTest(){
+		
+		SymbolicType pureType2 = universe.pureType(realType);
+		SymbolicType pureType4 = universe.pureType(booleanType);
+		assertEquals(pureType2.symbolicObjectKind(), pureType4.symbolicObjectKind());
 		
 	}
 	
