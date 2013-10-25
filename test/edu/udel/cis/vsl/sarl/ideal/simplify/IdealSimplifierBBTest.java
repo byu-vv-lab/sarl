1b654e0a1b0e00bcd4fb3a10f0ce50303838b6c1
danfried <danfried@7d651925-2d04-6a0e-8c93-cd1e20bcaab1>
2013-10-25 20:00:41 +0000

    separated grouped tests into individual methods, added javadoc comments, and commented out print statements

--- test/edu/udel/cis/vsl/sarl/ideal/simplify/IdealSimplifierBBTest.java
+++ test/edu/udel/cis/vsl/sarl/ideal/simplify/IdealSimplifierBBTest.java
@@ -49,36 +49,59 @@ public class IdealSimplifierBBTest {
 	public void xGreater0Test() {
 		assumption = preUniv.lessThan(rat0, xNE);
 		idealSimplifier = idealSimplifierFactory.newSimplifier(assumption);
-		out.println(idealSimplifier.simplifyExpression(bigMixedXYTermPoly));
+		//out.println(idealSimplifier.simplifyExpression(bigMixedXYTermPoly));
 		assertEquals("0 < x", idealSimplifier.getReducedContext().toString());
 		assertEquals(idealSimplifier.getReducedContext(), idealSimplifier.getFullContext());
-		
-		assumption = preUniv.equals(ratNeg1, xNE);
+	}
+	
+	/**
+	 * This test involves the use of two separate assumptions that are compounded and
+	 * applied to a mixed-term polynomial to test the simplification on large-term 
+	 * symbolic expressions.
+	 */
+	@Test
+	public void twoStagePolyTest(){
+		//first assumption: x == -1.0
+		assumption = preUniv.equals(ratNeg1, x);
 		idealSimplifier = idealSimplifierFactory.newSimplifier(assumption);
-		out.println(idealSimplifier.apply(bigMixedXYTermPoly));
-		SymbolicExpression noX = idealSimplifier.apply(bigMixedXYTermPoly);
+		//out.println(idealSimplifier.apply(bigMixedXYTermPoly));
+		SymbolicExpression noX = idealSimplifier.apply(bigMixedXYTermPoly); //intermediary symbolic expression
 		
+		//second assumption: y == 1.0
 		assumption = preUniv.equals(rat1, yNE);
 		idealSimplifier = idealSimplifierFactory.newSimplifier(assumption);
-		out.println(idealSimplifier.apply(noX));
+		//out.println(idealSimplifier.apply(noX));
 		//0^3 should = 0...
 		assertEquals(rat0, idealSimplifier.apply(noX));
-		out.println(idealSimplifier.getFullContext());
-		out.println(idealSimplifier.getReducedContext());
-		//unsure if a numeric expression should reduce to a boolean because of having a binary value
-		//assertNotEquals(trueExpr, idealSimplifier.getReducedContext());
-		
+		//out.println(idealSimplifier.getFullContext());
+		//out.println(idealSimplifier.getReducedContext());
+	}
+	
+	/**
+	 * Test on idealsimplifier's ability to determine that a reducedContext is true
+	 * due to a value being solvable/ able to be determined
+	 */
+	@Test
+	public void simplifySolvableTest(){
 		assumption = preUniv.equals(rat0, yNE);
 		idealSimplifier = idealSimplifierFactory.newSimplifier(assumption);
-		out.println(idealSimplifier.getFullContext());
-		out.println(idealSimplifier.getReducedContext());
-		
-		
-		assumption = preUniv.equals(rat0, x4th);
-		idealSimplifier = idealSimplifierFactory.newSimplifier(assumption);
-		out.println(idealSimplifier.getFullContext());
-		out.println(idealSimplifier.getReducedContext());
-		assertEquals(rat0, idealSimplifier.apply(threeX4th));
+		//out.println("here: " + idealSimplifier.getFullContext());
+		//out.println(idealSimplifier.getReducedContext());
+		assertNotEquals(idealSimplifier.getFullContext(), idealSimplifier.getReducedContext());
+		assertEquals(trueExpr, idealSimplifier.getReducedContext());
 	}
 
+	/**
+	 * Tests idealSimplifer's ability to reduce a single-term variable of order > 1, when equal to 0
+	 */
+	@Test
+	public void singlePowerTermSimplifyTest(){
+		assumption = preUniv.equals(rat0, x4th);
+		idealSimplifier = idealSimplifierFactory.newSimplifier(assumption);
+		//out.println("here: " + idealSimplifier.getFullContext());
+		//out.println(idealSimplifier.getReducedContext());
+		assertEquals(rat0, idealSimplifier.apply(threeX4th));
+		//x^4 == 0 should be reduced to x == 0
+		assertNotEquals(idealSimplifier.getFullContext(), idealSimplifier.getReducedContext());
+	}
 }
