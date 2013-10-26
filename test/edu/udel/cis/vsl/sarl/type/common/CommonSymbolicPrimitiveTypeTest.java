1e52f1960732bcdb8be18d42b89693425446f8ae
jthakkar <jthakkar@7d651925-2d04-6a0e-8c93-cd1e20bcaab1>
2013-10-26 16:01:59 +0000

    Committing tests for CommonSymbolicPrimitiveType.

--- test/edu/udel/cis/vsl/sarl/type/common/CommonSymbolicPrimitiveTypeTest.java
+++ test/edu/udel/cis/vsl/sarl/type/common/CommonSymbolicPrimitiveTypeTest.java
@@ -8,7 +8,10 @@ import org.junit.Before;
 import org.junit.BeforeClass;
 import org.junit.Test;
 
+import edu.udel.cis.vsl.sarl.IF.type.SymbolicType.SymbolicTypeKind;
+
 public class CommonSymbolicPrimitiveTypeTest {
+	CommonSymbolicPrimitiveType bool1, bool2, char1;
 
 	@BeforeClass
 	public static void setUpBeforeClass() throws Exception {
@@ -20,6 +23,10 @@ public class CommonSymbolicPrimitiveTypeTest {
 
 	@Before
 	public void setUp() throws Exception {
+		
+		bool1 = new CommonSymbolicPrimitiveType(SymbolicTypeKind.BOOLEAN);
+		bool2 = new CommonSymbolicPrimitiveType(SymbolicTypeKind.BOOLEAN);
+		char1 = new CommonSymbolicPrimitiveType(SymbolicTypeKind.CHAR);
 	}
 
 	@After
@@ -28,33 +35,51 @@ public class CommonSymbolicPrimitiveTypeTest {
 	
 
 	@Test
-	public void testComputeHashCode() {
-		//fail("Not yet implemented");
+	public void testComputeHashCode() 
+	{
+		assertEquals(bool1.computeHashCode(), bool2.computeHashCode());
+		assertNotEquals(bool1.computeHashCode(), char1.computeHashCode());
 	}
+	
+	@Test
+	public void testTypeEquals() 
+	{
+		assertTrue(bool1.typeEquals(bool2));
+		assertEquals(bool1.typeEquals(char1), char1.typeEquals(bool2));
+	}
+	
 /*
 	@Test
-	public void testCanonizeChildren() {
-		fail("Not yet implemented");
-	}
-
-	@Test
-	public void testTypeEquals() {
-		fail("Not yet implemented");
-	}
-
-	@Test
-	public void testGetPureType() {
-		fail("Not yet implemented");
-	}
-
-	@Test
-	public void testCommonSymbolicPrimitiveType() {
-		fail("Not yet implemented");
-	}
-
-	@Test
-	public void testToStringBuffer() {
-		fail("Not yet implemented");
+	public void testCanonizeChildren() 
+	{
+		
 	}
 */
+	
+
+	@Test
+	public void testGetPureType() 
+	{
+		assertEquals(bool1.getPureType(), bool2.getPureType());
+		assertNotEquals(bool1.getPureType(), char1.getPureType());
+	}
+
+//	@Test
+//	public void testCommonSymbolicPrimitiveType() 
+//	{
+//		fail("Not yet implemented");
+//	}
+
+	/**
+	 * 
+	 */
+	@Test
+	public void testToStringBuffer() 
+	{
+		//System.out.print(bool1.toStringBuffer(true));
+		assertEquals(bool1.toStringBuffer(true).toString(),"boolean");
+		assertEquals(bool2.toStringBuffer(false).toString(),"boolean");
+		assertNotEquals(char1.toStringBuffer(true).toString(),"boolean");
+	}
+
 }
