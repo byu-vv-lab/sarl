4214a6218d7dcbb94ac95fd2de86443d327d3f89
ncarucci <ncarucci@7d651925-2d04-6a0e-8c93-cd1e20bcaab1>
2013-10-26 05:31:56 +0000

    Committing translatePower test with SymbolicConstants.

--- test/edu/udel/cis/vsl/sarl/prove/cvc/CVC3TranslatePowerTest.java
+++ test/edu/udel/cis/vsl/sarl/prove/cvc/CVC3TranslatePowerTest.java
@@ -10,7 +10,10 @@ import cvc3.Expr;
 import cvc3.ValidityChecker;
 import edu.udel.cis.vsl.sarl.IF.expr.BooleanExpression;
 import edu.udel.cis.vsl.sarl.IF.expr.NumericExpression;
+import edu.udel.cis.vsl.sarl.IF.expr.SymbolicConstant;
 import edu.udel.cis.vsl.sarl.IF.expr.SymbolicExpression.SymbolicOperator;
+import edu.udel.cis.vsl.sarl.IF.object.IntObject;
+import edu.udel.cis.vsl.sarl.IF.type.SymbolicIntegerType;
 import edu.udel.cis.vsl.sarl.IF.type.SymbolicRealType;
 import edu.udel.cis.vsl.sarl.expr.IF.ExpressionFactory;
 import edu.udel.cis.vsl.sarl.preuniverse.PreUniverses;
@@ -30,16 +33,22 @@ public class CVC3TranslatePowerTest {
 			.expressionFactory();
 	// types
 	private static SymbolicRealType realType = universe.realType();
+	private static SymbolicIntegerType intType = universe.integerType();
 	// expressions
 	private static NumericExpression five = universe.rational(5);
 	private static NumericExpression two = universe.rational(2);
 	private static BooleanExpression booleanExprTrue = universe
 			.trueExpression();
+	// SymbolicConstants
+	private static SymbolicConstant e = universe.symbolicConstant(
+			universe.stringObject("e"), intType);
+	private static SymbolicConstant f = universe.symbolicConstant(
+			universe.stringObject("f"), intType);
 	// Instance fields: instantiated before each test is run...
 	private TheoremProverFactory proverFactory;
 	private CVC3TheoremProver cvcProver;
 	private ValidityChecker vc;
-	
+
 	/**
 	 * Set up each test. This method is run before each test.
 	 * 
@@ -56,10 +65,10 @@ public class CVC3TranslatePowerTest {
 	@After
 	public void tearDown() throws Exception {
 	}
-	
+
 	@Test
-	public void testTranslatePower(){
-		Expr twoExpr = cvcProver.translate(two);	
+	public void testTranslatePower() {
+		Expr twoExpr = cvcProver.translate(two);
 		Expr fiveExpr = cvcProver.translate(five);
 
 		NumericExpression powerExp = (NumericExpression) expressionFactory
@@ -68,4 +77,17 @@ public class CVC3TranslatePowerTest {
 		Expr expected9 = vc.powExpr(twoExpr, fiveExpr);
 		assertEquals(expected9, expr9);
 	}
+
+	@Test
+	public void testTranslatePowerSymbolic() {
+		Expr eExpr = cvcProver.translate(e);
+		Expr fExpr = cvcProver.translate(f);
+
+		NumericExpression powerExp = (NumericExpression) expressionFactory
+				.expression(SymbolicOperator.POWER, intType, e, f);
+		Expr expr9 = cvcProver.translate(powerExp);
+		Expr expected9 = vc.powExpr(eExpr, fExpr);
+		assertEquals(expected9, expr9);
+	}
+
 }
