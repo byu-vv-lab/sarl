90cca771afbd804e2b8295ee500813e8c97a9d3b
malsulmi <malsulmi@7d651925-2d04-6a0e-8c93-cd1e20bcaab1>
2013-10-28 16:11:27 +0000

    adding package information

---
+++ src/edu/udel/cis/vsl/sarl/preuniverse/package-info.java
@@ -0,0 +1,18 @@
+/**
+ * This package provides an interface to  symbolic universe services except those for reasoning.
+ * {@link edu.udel.cis.vsl.sarl.preuniverse}.  That class provides 
+ * methods to produce new symbolic universes.
+ * Those methods should be the starting point for
+ * any typical user of SARL.
+ * 
+ * The "public interface" for SARL is defined in 
+ * {@link edu.udel.cis.vsl.sarl.IF} and its subpackages.  The interfaces
+ * there should be all that the typical user needs to know about.
+ * Only advanced users who wish to extend or modify SARL in some way
+ * need to look into the other (implementation) packages.
+ * 
+ *  @see edu.udel.cis.vsl.sarl.SARL
+ *  @see edu.udel.cis.vsl.sarl.IF
+ */
+package edu.udel.cis.vsl.sarl.preuniverse;
+
