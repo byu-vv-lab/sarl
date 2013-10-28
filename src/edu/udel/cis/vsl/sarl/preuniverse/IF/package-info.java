f46c63ba960365ec999b5c8bd2349813ab2ff807
malsulmi <malsulmi@7d651925-2d04-6a0e-8c93-cd1e20bcaab1>
2013-10-28 16:32:11 +0000

    adding package-info to preuniverse/IF

---
+++ src/edu/udel/cis/vsl/sarl/preuniverse/IF/package-info.java
@@ -0,0 +1,18 @@
+/**
+ * This package provides all of the functionality of a symbolic universe except reasoning.
+ * {@link edu.udel.cis.vsl.sarl.preuniverse.IF}.  add the description. 
+ * 
+ * Those methods should be the starting point for
+ * any typical user of SARL.
+ * 
+ * The "public interface" for SARL is defined in 
+ * {@link edu.udel.cis.vsl.sarl.preuniverse} and its subpackages.  The interfaces
+ * there should be all that the typical user needs to know about.
+ * Only advanced users who wish to extend or modify SARL in some way
+ * need to look into the other (implementation) packages.
+ * 
+ *  @see edu.udel.cis.vsl.sarl.SARL
+ *  @see edu.udel.cis.vsl.sarl.IF
+ */
+package edu.udel.cis.vsl.sarl.preuniverse.IF;
+
