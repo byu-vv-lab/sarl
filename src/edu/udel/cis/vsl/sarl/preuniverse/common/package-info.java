fd1be274289d9dca1f37b381b108960c387e3ba5
malsulmi <malsulmi@7d651925-2d04-6a0e-8c93-cd1e20bcaab1>
2013-10-28 16:28:11 +0000

    updating package-info in Preuniverse

--- src/edu/udel/cis/vsl/sarl/preuniverse/common/package-info.java
+++ src/edu/udel/cis/vsl/sarl/preuniverse/common/package-info.java
@@ -1,6 +1,6 @@
 /**
- * This package provides all of the functionality of a symbolic universe except reasoning.
- * {@link edu.udel.cis.vsl.sarl.preuniverse}.  add the description. 
+ * This package provides the implementation of PreUniverse module.
+ * {@link edu.udel.cis.vsl.sarl.preuniverse.common}.  add the description. 
  * 
  * Those methods should be the starting point for
  * any typical user of SARL.
