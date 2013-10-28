284726cdc7fd9d12eb2199d0aabe1fdd1c916176
ncarucci <ncarucci@7d651925-2d04-6a0e-8c93-cd1e20bcaab1>
2013-10-28 06:28:18 +0000

    Committing with correct formatting, SymbolicConstant names, and getting rid of numbers after assignments where applicable.

--- test/edu/udel/cis/vsl/sarl/prove/cvc/CVC3TranslateTest.java
+++ test/edu/udel/cis/vsl/sarl/prove/cvc/CVC3TranslateTest.java
@@ -24,7 +24,7 @@ import edu.udel.cis.vsl.sarl.prove.Prove;
 import edu.udel.cis.vsl.sarl.prove.IF.TheoremProverFactory;
 
 public class CVC3TranslateTest {
-	
+
 	// Static fields: instantiated once and used for all tests...
 	private static FactorySystem factorySystem = PreUniverses
 			.newIdealFactorySystem();
@@ -43,16 +43,20 @@ public class CVC3TranslateTest {
 	private static NumericExpression twoInt = universe.integer(2);
 	private static BooleanExpression booleanExprTrue = universe
 			.trueExpression();
-	//SymbolicConstants
-	private static SymbolicConstant eReal = universe.symbolicConstant(universe.stringObject("e"), realType);
-	private static SymbolicConstant eInt = universe.symbolicConstant(universe.stringObject("e"), intType);
-	private static SymbolicConstant fReal = universe.symbolicConstant(universe.stringObject("f"), realType);
-	private static SymbolicConstant fInt = universe.symbolicConstant(universe.stringObject("f"), intType);
+	// SymbolicConstants
+	private static SymbolicConstant xReal = universe.symbolicConstant(
+			universe.stringObject("x"), realType);
+	private static SymbolicConstant xInt = universe.symbolicConstant(
+			universe.stringObject("x"), intType);
+	private static SymbolicConstant yReal = universe.symbolicConstant(
+			universe.stringObject("y"), realType);
+	private static SymbolicConstant yInt = universe.symbolicConstant(
+			universe.stringObject("y"), intType);
 	// Instance fields: instantiated before each test is run...
 	private TheoremProverFactory proverFactory;
 	private CVC3TheoremProver cvcProver;
 	private ValidityChecker vc;
-	
+
 	/**
 	 * Set up each test. This method is run before each test.
 	 * 
@@ -69,146 +73,153 @@ public class CVC3TranslateTest {
 	@After
 	public void tearDown() throws Exception {
 	}
-	
+
 	/**
-	 * testTranslateCastRealToInt translates an integer numeric expression; and it also uses the
-	 * symbolic operator CAST to create a symbolic expression. The test compares the created
-	 * expr and the translated symbolic expression that was casted.
+	 * testTranslateCastRealToInt translates an integer numeric expression; and
+	 * it also uses the symbolic operator CAST to create a symbolic expression.
+	 * The test compares the created expr and the translated symbolic expression
+	 * that was casted.
 	 */
 
 	@Test
-	public void testTranslateCastRealToInt(){
+	public void testTranslateCastRealToInt() {
 		Expr oneIntExpr = cvcProver.translate(oneInt);
 
-		SymbolicExpression castExp = expressionFactory
-				.expression(SymbolicOperator.CAST, intType, one);
-		Expr expr4 = cvcProver.translate(castExp);
-		assertEquals(oneIntExpr, expr4);
+		SymbolicExpression castExp = expressionFactory.expression(
+				SymbolicOperator.CAST, intType, one);
+		Expr expr = cvcProver.translate(castExp);
+		assertEquals(oneIntExpr, expr);
 	}
-	
+
 	/**
-	 * testTranslateCastIntToReal translates an rational numeric expression; and it also uses the
-	 * symbolic operator CAST to create a symbolic expression. The test compares the created
-	 * expr and the translated symbolic expression that was casted.
+	 * testTranslateCastIntToReal translates an rational numeric expression; and
+	 * it also uses the symbolic operator CAST to create a symbolic expression.
+	 * The test compares the created expr and the translated symbolic expression
+	 * that was casted.
 	 */
-	
+
 	@Test
-	public void testTranslateCastIntToReal(){
+	public void testTranslateCastIntToReal() {
 		Expr oneRealExpr = cvcProver.translate(one);
 
-		SymbolicExpression castExp = expressionFactory
-				.expression(SymbolicOperator.CAST, realType, oneInt);
-		Expr expr4 = cvcProver.translate(castExp);
-		assertEquals(oneRealExpr, expr4);
+		SymbolicExpression castExp = expressionFactory.expression(
+				SymbolicOperator.CAST, realType, oneInt);
+		Expr expr = cvcProver.translate(castExp);
+		assertEquals(oneRealExpr, expr);
 	}
-	
+
 	/**
-	 * testTranslateCastDoubleToInt translates a double rational numeric expression; and it also uses the
-	 * symbolic operator CAST to create a symbolic expression. The test compares the created
-	 * expr and the translated symbolic expression that was casted.
+	 * testTranslateCastDoubleToInt translates a double rational numeric
+	 * expression; and it also uses the symbolic operator CAST to create a
+	 * symbolic expression. The test compares the created expr and the
+	 * translated symbolic expression that was casted.
 	 */
 
 	@Test
-	public void testTranslateCastDoubleToInt(){
+	public void testTranslateCastDoubleToInt() {
 		Expr oneFiveDoubleExpr = cvcProver.translate(oneFiveDouble);
-		
-		SymbolicExpression castExp = expressionFactory
-				.expression(SymbolicOperator.CAST, intType, oneFiveDouble);
-		Expr expr4 = cvcProver.translate(castExp);
-		Expr expr5 = vc.eqExpr(expr4, oneFiveDoubleExpr);
-		assertEquals(QueryResult.VALID, vc.query(expr5));
-	}
-	
-	/**
-	 * testTranslateCondSymbolicInt translates a boolean expression and two symbolic int constants.
-	 * The test creates a symbolic expression using the symbolic operator COND and the translated variables.
-	 * The test compares the validity checker using .iteExpr and the translation of the symbolic expression.
-	 */
-	
-	@Test
-	public void testTranslateCondSymbolicInt(){	
-		Expr trueExpr = cvcProver.translate(booleanExprTrue);
-		Expr eExpr = cvcProver.translate(eInt);
-		Expr fExpr = cvcProver.translate(fInt);
 
-		SymbolicExpression condExp = expressionFactory
-				.expression(SymbolicOperator.COND, boolType, booleanExprTrue, 
-						eInt, fInt);
-		Expr expr5 = cvcProver.translate(condExp);
-		Expr expected5 = vc.iteExpr(trueExpr, eExpr, fExpr);
-		assertEquals(expected5, expr5);
+		SymbolicExpression castExp = expressionFactory.expression(
+				SymbolicOperator.CAST, intType, oneFiveDouble);
+		Expr expr = cvcProver.translate(castExp);
+		Expr expr2 = vc.eqExpr(expr, oneFiveDoubleExpr);
+		assertEquals(QueryResult.VALID, vc.query(expr2));
 	}
-	
-	/**
-	 * testTranslateCondSymbolicInt translates a boolean expression and two symbolic real constants.
-	 * The test creates a symbolic expression using the symbolic operator COND and the translated variables.
-	 * The test compares the validity checker using .iteExpr and the translation of the symbolic expression.
-	 */
-	
-	@Test
-	public void testTranslateCondSymbolicReal(){	
-		Expr trueExpr = cvcProver.translate(booleanExprTrue);
-		Expr eExpr = cvcProver.translate(eReal);
-		Expr fExpr = cvcProver.translate(fReal);
 
-		SymbolicExpression condExp = expressionFactory
-				.expression(SymbolicOperator.COND, boolType, booleanExprTrue, 
-						eReal, fReal);
-		Expr expr5 = cvcProver.translate(condExp);
-		Expr expected5 = vc.iteExpr(trueExpr, eExpr, fExpr);
-		assertEquals(expected5, expr5);
-	}
-	
 	/**
-	 * testTranslateCondConcreteInt creates a symbolic expression using translated boolean expression
-	 * , translated int numeric constants, and the symbolic operator COND. It then compares the validity checker
-	 * using .iteExpr and the translation of that symbolic expression.
+	 * testTranslateCondSymbolicInt translates a boolean expression and two
+	 * symbolic int constants. The test creates a symbolic expression using the
+	 * symbolic operator COND and the translated variables. The test compares
+	 * the validity checker using .iteExpr and the translation of the symbolic
+	 * expression.
 	 */
-	
+
 	@Test
-	public void testTranslateCondConcreteInt(){	
+	public void testTranslateCondSymbolicInt() {
+		Expr trueExpr = cvcProver.translate(booleanExprTrue);
+		Expr xExpr = cvcProver.translate(xInt);
+		Expr yExpr = cvcProver.translate(yInt);
+
+		SymbolicExpression condExp = expressionFactory.expression(
+				SymbolicOperator.COND, boolType, booleanExprTrue, xInt, yInt);
+		Expr expr = cvcProver.translate(condExp);
+		Expr expected = vc.iteExpr(trueExpr, xExpr, yExpr);
+		assertEquals(expected, expr);
+	}
+
+	/**
+	 * testTranslateCondSymbolicInt translates a boolean expression and two
+	 * symbolic real constants. The test creates a symbolic expression using the
+	 * symbolic operator COND and the translated variables. The test compares
+	 * the validity checker using .iteExpr and the translation of the symbolic
+	 * expression.
+	 */
+
+	@Test
+	public void testTranslateCondSymbolicReal() {
+		Expr trueExpr = cvcProver.translate(booleanExprTrue);
+		Expr xExpr = cvcProver.translate(xReal);
+		Expr yExpr = cvcProver.translate(yReal);
+
+		SymbolicExpression condExp = expressionFactory.expression(
+				SymbolicOperator.COND, boolType, booleanExprTrue, xReal, yReal);
+		Expr expr = cvcProver.translate(condExp);
+		Expr expected = vc.iteExpr(trueExpr, xExpr, yExpr);
+		assertEquals(expected, expr);
+	}
+
+	/**
+	 * testTranslateCondConcreteInt creates a symbolic expression using
+	 * translated boolean expression , translated int numeric constants, and the
+	 * symbolic operator COND. It then compares the validity checker using
+	 * .iteExpr and the translation of that symbolic expression.
+	 */
+
+	@Test
+	public void testTranslateCondConcreteInt() {
 		Expr trueExpr = cvcProver.translate(booleanExprTrue);
 		Expr oneExpr = cvcProver.translate(oneInt);
 		Expr twoExpr = cvcProver.translate(twoInt);
 
-		SymbolicExpression condExp = expressionFactory
-				.expression(SymbolicOperator.COND, boolType, booleanExprTrue, 
-						oneInt, twoInt);
-		Expr expr5 = cvcProver.translate(condExp);
-		Expr expected5 = vc.iteExpr(trueExpr, oneExpr, twoExpr);
-		assertEquals(expected5, expr5);
+		SymbolicExpression condExp = expressionFactory.expression(
+				SymbolicOperator.COND, boolType, booleanExprTrue, oneInt,
+				twoInt);
+		Expr expr = cvcProver.translate(condExp);
+		Expr expected = vc.iteExpr(trueExpr, oneExpr, twoExpr);
+		assertEquals(expected, expr);
 	}
-	
+
 	/**
-	 * testTranslateCondConcreteInt creates a symbolic expression using translated boolean expression
-	 * , translated rational numeric constants, and the symbolic operator COND. It then compares the validity checker
+	 * testTranslateCondConcreteInt creates a symbolic expression using
+	 * translated boolean expression , translated rational numeric constants,
+	 * and the symbolic operator COND. It then compares the validity checker
 	 * using .iteExpr and the translation of that symbolic expression.
 	 */
-	
+
 	@Test
-	public void testTranslateCondConcreteReal(){	
+	public void testTranslateCondConcreteReal() {
 		Expr trueExpr = cvcProver.translate(booleanExprTrue);
 		Expr oneExpr = cvcProver.translate(one);
 		Expr twoExpr = cvcProver.translate(two);
 
-		SymbolicExpression condExp = expressionFactory
-				.expression(SymbolicOperator.COND, boolType, booleanExprTrue, 
-						one, two);
-		Expr expr5 = cvcProver.translate(condExp);
-		Expr expected5 = vc.iteExpr(trueExpr, oneExpr, twoExpr);
-		assertEquals(expected5, expr5);
+		SymbolicExpression condExp = expressionFactory.expression(
+				SymbolicOperator.COND, boolType, booleanExprTrue, one, two);
+		Expr expr = cvcProver.translate(condExp);
+		Expr expected = vc.iteExpr(trueExpr, oneExpr, twoExpr);
+		assertEquals(expected, expr);
 	}
-	
+
 	/**
-	 * testTranslateNotBoolean creates a symbolic expression using the symbolic operator NOT
-	 * anda booleanExprTrue. It then assesses the equality of the translation of the symbolic expression
-	 * and the validity checker using .notExpr and .trueExpr.
+	 * testTranslateNotBoolean creates a symbolic expression using the symbolic
+	 * operator NOT anda booleanExprTrue. It then assesses the equality of the
+	 * translation of the symbolic expression and the validity checker using
+	 * .notExpr and .trueExpr.
 	 */
-	
+
 	@Test
-	public void testTranslateNotBoolean(){
-		SymbolicExpression notExp = expressionFactory
-				.expression(SymbolicOperator.NOT, boolType, booleanExprTrue);
+	public void testTranslateNotBoolean() {
+		SymbolicExpression notExp = expressionFactory.expression(
+				SymbolicOperator.NOT, boolType, booleanExprTrue);
 		Expr translateResult = cvcProver.translate(notExp);
 		Expr expected = vc.notExpr(vc.trueExpr());
 		assertEquals(expected, translateResult);
