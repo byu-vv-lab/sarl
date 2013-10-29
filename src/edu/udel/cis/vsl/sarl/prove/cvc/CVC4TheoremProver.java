a20249b790bafcfd0ae15705cd130359ce67100f
andrew <andrew@7d651925-2d04-6a0e-8c93-cd1e20bcaab1>
2013-10-29 22:06:59 +0000

    Added constructor and skeleton for translate.

--- src/edu/udel/cis/vsl/sarl/prove/cvc/CVC4TheoremProver.java
+++ src/edu/udel/cis/vsl/sarl/prove/cvc/CVC4TheoremProver.java
@@ -2,6 +2,9 @@ package edu.udel.cis.vsl.sarl.prove.cvc;
 
 import java.io.PrintStream;
 
+import edu.nyu.acsys.CVC4.Expr;
+import edu.nyu.acsys.CVC4.ExprManager;
+import edu.nyu.acsys.CVC4.SmtEngine;
 import edu.udel.cis.vsl.sarl.IF.ValidityResult;
 import edu.udel.cis.vsl.sarl.IF.expr.BooleanExpression;
 import edu.udel.cis.vsl.sarl.preuniverse.IF.PreUniverse;
@@ -10,8 +13,26 @@ import edu.udel.cis.vsl.sarl.prove.IF.TheoremProver;
 public class CVC4TheoremProver implements TheoremProver{
 
 	private PreUniverse model;
+	private BooleanExpression context;
 	private PrintStream out = System.out;
 	private boolean showProverQueries = false;
+	private ExprManager em = new ExprManager();
+	private SmtEngine smt = new SmtEngine(em);
+	private Expr cvcAssumption;
+
+	CVC4TheoremProver(PreUniverse universe, BooleanExpression context) {
+		assert universe != null;
+		assert context != null;
+		this.model = universe;
+		this.context = context;
+		cvcAssumption = translate(context);
+		smt.assertFormula(cvcAssumption);
+	}
+	 
+	private Expr translate(BooleanExpression context) {
+		// TODO Auto-generated method stub
+		return null;
+	}
 
 	@Override
 	public PreUniverse universe() {
