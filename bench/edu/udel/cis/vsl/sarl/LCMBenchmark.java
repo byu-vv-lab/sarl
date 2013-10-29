ecca4388182c09a8fe5bca9cf3fcae90ce630e01
tmelguy <tmelguy@7d651925-2d04-6a0e-8c93-cd1e20bcaab1>
2013-10-29 22:05:35 +0000

    Added some more benchmarking for LCM. Trying to use very large numbers to get a better idea if there's a linear or exponential relationship between the size of the numbers and the performance of the method.

--- bench/edu/udel/cis/vsl/sarl/LCMBenchmark.java
+++ bench/edu/udel/cis/vsl/sarl/LCMBenchmark.java
@@ -22,7 +22,16 @@ public class LCMBenchmark {
 	private static BigInteger bigTwenty = new BigInteger("20"); 
 	private static BigInteger bigThousand = new BigInteger("1000");  
 	private static BigInteger bigTwoThousand = new BigInteger("2000"); 
-
+	private static BigInteger bigOneMillion = new BigInteger("1000000");
+	private static BigInteger bigTenMillion = new BigInteger("10000000");
+	private static BigInteger bigFiftyMillion = new BigInteger("50000000");
+	private static BigInteger bigOneHundredMillion = new BigInteger("100000000");
+	private static BigInteger bigOneBillion = new BigInteger("1000000000");
+	private static BigInteger bigFiveBillion = new BigInteger("5000000000");
+	private static BigInteger bigOneTrillion = new BigInteger("1000000000000");
+	private static BigInteger bigTenTrillion = new BigInteger("10000000000000");
+	private static BigInteger bigOneQuadrillion = new BigInteger("1000000000000000");
+	private static BigInteger bigTwoQuadrillion = new BigInteger("2000000000000000");
 
 	
 	
@@ -69,6 +78,71 @@ public void LCMBenchmark4() {
 	
 	IntegerNumber a = factory.integer(bigThousand);
 	IntegerNumber b = factory.integer(bigTwoThousand); 
+	long x = System.nanoTime();
+	
+	factory.lcm(a,b);
+	long y = System.nanoTime();
+
+	System.out.print(y-x);
+
+}
+
+public void LCMBenchmark5() { 
+	
+	IntegerNumber a = factory.integer(bigOneMillion);
+	IntegerNumber b = factory.integer(bigTenMillion); 
+	long x = System.nanoTime();
+	
+	factory.lcm(a,b);
+	long y = System.nanoTime();
+
+	System.out.print(y-x);
+
+}
+
+public void LCMBenchmark6() { 
+	
+	IntegerNumber a = factory.integer(bigFiftyMillion);
+	IntegerNumber b = factory.integer(bigOneHundredMillion); 
+	long x = System.nanoTime();
+	
+	factory.lcm(a,b);
+	long y = System.nanoTime();
+
+	System.out.print(y-x);
+
+}
+
+public void LCMBenchmark7() { 
+	
+	IntegerNumber a = factory.integer(bigOneBillion);
+	IntegerNumber b = factory.integer(bigFiveBillion); 
+	long x = System.nanoTime();
+	
+	factory.lcm(a,b);
+	long y = System.nanoTime();
+
+	System.out.print(y-x);
+
+}
+
+public void LCMBenchmark8() { 
+	
+	IntegerNumber a = factory.integer(bigOneTrillion);
+	IntegerNumber b = factory.integer(bigTenTrillion); 
+	long x = System.nanoTime();
+	
+	factory.lcm(a,b);
+	long y = System.nanoTime();
+
+	System.out.print(y-x);
+
+}
+
+public void LCMBenchmark9() { 
+	
+	IntegerNumber a = factory.integer(bigOneQuadrillion);
+	IntegerNumber b = factory.integer(bigTwoQuadrillion); 
 	long x = System.nanoTime();
 	
 	factory.lcm(a,b);
