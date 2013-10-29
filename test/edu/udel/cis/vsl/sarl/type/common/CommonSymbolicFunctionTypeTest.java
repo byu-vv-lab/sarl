e302ea6418b62d896b017d494e373d76bdb22733
jthakkar <jthakkar@7d651925-2d04-6a0e-8c93-cd1e20bcaab1>
2013-10-29 23:14:38 +0000

    Added and Modified tests for CommonSymbolicFunctionTypeTest

--- test/edu/udel/cis/vsl/sarl/type/common/CommonSymbolicFunctionTypeTest.java
+++ test/edu/udel/cis/vsl/sarl/type/common/CommonSymbolicFunctionTypeTest.java
@@ -10,19 +10,19 @@ import org.junit.Before;
 import org.junit.BeforeClass;
 import org.junit.Test;
 
-import edu.udel.cis.vsl.sarl.IF.type.SymbolicFunctionType;
 import edu.udel.cis.vsl.sarl.IF.type.SymbolicIntegerType.IntegerKind;
 import edu.udel.cis.vsl.sarl.IF.type.SymbolicRealType.RealKind;
-import edu.udel.cis.vsl.sarl.IF.type.SymbolicTypeSequence;
-import edu.udel.cis.vsl.sarl.preuniverse.common.PureTypeTest;
 
 public class CommonSymbolicFunctionTypeTest 
 {
 	CommonSymbolicFunctionType function, function1, function2;
+	
 	CommonSymbolicTypeSequence typeSequence;
+	
 	CommonSymbolicIntegerType idealIntKind, boundedIntKind;
+	
 	CommonSymbolicRealType idealRealKind, floatRealKind;
-	TypeComparator typeComparator;
+	
 	ArrayList<CommonSymbolicType> types;
 	
 	@BeforeClass
@@ -90,7 +90,7 @@ public class CommonSymbolicFunctionTypeTest
 //	}
 
 	/**
-	 this test aims to check the output type of a function. 
+	 this test aims to check the output type of each variable of the ArrayList of the CommonSymbolicType. 
 	 */
 	@Test
 	public void testOutputType() 
@@ -100,42 +100,54 @@ public class CommonSymbolicFunctionTypeTest
 	}
 
 	/**
-	 this test checks the string output of each function. 
+	 this test checks the string output of each variable of the ArrayList of the CommonSymbolicType. 
 	 */
 	@Test
 	public void testToStringBuffer() 
 	{
-//		System.out.print(function.toStringBuffer(true));
-//		System.out.print(function1.toStringBuffer(true));
-//		System.out.print(function2.toStringBuffer(true));
+//		System.out.println(function.toStringBuffer(true));
+//		System.out.println(function1.toStringBuffer(true));
+//		System.out.println(function1.toStringBuffer(false));
+//		System.out.println(function2.toStringBuffer(true));
 		assertEquals(function.toStringBuffer(true).toString(),"(<int,bounded,real,float>->int)");
 		assertEquals(function1.toStringBuffer(false).toString(),"<int,bounded,real,float>->int");
-		assertNotEquals(function2.toStringBuffer(true).toString(),"<int,bounded,real,float>->int");
+		assertNotEquals(function1.toStringBuffer(false).toString(),"(<int,bounded,real,float>->int)");
+		assertNotEquals(function2.toStringBuffer(true).toString(),"(<int,bounded,real,float>->int)");
 	}
 
 	/**
-	 this test check the how many input types the function has, in this particular test it should return true only for 4.   
+	 this test check the how many input types the function has, in this particular test the expected value is 4.   
 	 */
 	@Test
-	public void testInputTypes() {
+	public void testInputTypes() 
+	{
+//		System.out.println(function.inputTypes().numTypes());
+//		System.out.println(function1.inputTypes().numTypes());
+//		System.out.println(function2.inputTypes().numTypes());
 		assertEquals(function.inputTypes().numTypes(), 4);
 		assertEquals(function1.inputTypes().numTypes(), 4);
 		assertEquals(function2.inputTypes().numTypes(), 4);
 		assertNotEquals(function.inputTypes().numTypes(), 5);
 	}
 	
-//	@Test
-//	public void testGetPureType() 
-//	{
-//		assertTrue((function.getPureType().inputTypes()) instanceof SymbolicTypeSequence);
-//		assertNotEquals(function1.getPureType(), function2.getPureType());
-//
-//	}
+	@Test
+	public void testGetPureType() 
+	{
+//		System.out.println(function.getPureType());
+//		System.out.println(function1.getPureType());
+//		System.out.println(function2.getPureType());
+		assertEquals(function.getPureType(), function1.getPureType());
+		assertEquals(function1.getPureType(), function2.getPureType());
+	}
 
-//	@Test
-//	public void testSetPureType() 
-//	{
-//	
-//	}
+	@Test
+	public void testSetPureType() 
+	{
+//		System.out.println(function.getPureType());
+		assertNull(function.getPureType());
+		function.setPureType(function2);
+		assertNotNull(function.getPureType());
+//		System.out.println(function.getPureType());
+	}
 
 }
