7e907802f738cf55bbfd928fe6c7abeaa37e6edb
andrew <andrew@7d651925-2d04-6a0e-8c93-cd1e20bcaab1>
2013-10-29 21:47:57 +0000

    CVC4TheoremProver skeleton code.

---
+++ src/edu/udel/cis/vsl/sarl/prove/cvc/CVC4TheoremProver.java
@@ -0,0 +1,36 @@
+package edu.udel.cis.vsl.sarl.prove.cvc;
+
+import java.io.PrintStream;
+
+import edu.udel.cis.vsl.sarl.IF.ValidityResult;
+import edu.udel.cis.vsl.sarl.IF.expr.BooleanExpression;
+import edu.udel.cis.vsl.sarl.preuniverse.IF.PreUniverse;
+import edu.udel.cis.vsl.sarl.prove.IF.TheoremProver;
+
+public class CVC4TheoremProver implements TheoremProver{
+
+	@Override
+	public PreUniverse universe() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public ValidityResult valid(BooleanExpression predicate) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public ValidityResult validOrModel(BooleanExpression predicate) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public void setOutput(PrintStream out) {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
