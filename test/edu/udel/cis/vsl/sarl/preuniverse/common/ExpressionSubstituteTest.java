d5eac6369debb97c89cd6badbefcd73f6a3e0f4d
gunjan <gunjan@7d651925-2d04-6a0e-8c93-cd1e20bcaab1>
2013-10-29 03:39:34 +0000

    Added ExpressionSubstituteTest.java

---
+++ test/edu/udel/cis/vsl/sarl/preuniverse/common/ExpressionSubstituteTest.java
@@ -0,0 +1,89 @@
+/* @author Gunjan Majmudar */
+
+package edu.udel.cis.vsl.sarl.preuniverse.common;
+
+import static org.junit.Assert.assertEquals;
+
+import java.io.PrintStream;
+import java.util.Arrays;
+import java.util.LinkedList;
+
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Ignore;
+import org.junit.Test;
+
+import edu.udel.cis.vsl.sarl.IF.SARLException;
+import edu.udel.cis.vsl.sarl.IF.expr.BooleanExpression;
+import edu.udel.cis.vsl.sarl.IF.expr.NumericExpression;
+import edu.udel.cis.vsl.sarl.IF.expr.NumericSymbolicConstant;
+import edu.udel.cis.vsl.sarl.IF.expr.SymbolicConstant;
+import edu.udel.cis.vsl.sarl.IF.expr.SymbolicExpression;
+import edu.udel.cis.vsl.sarl.IF.type.SymbolicFunctionType;
+import edu.udel.cis.vsl.sarl.IF.type.SymbolicTupleType;
+import edu.udel.cis.vsl.sarl.IF.type.SymbolicType;
+import edu.udel.cis.vsl.sarl.IF.type.SymbolicType.SymbolicTypeKind;
+import edu.udel.cis.vsl.sarl.IF.type.SymbolicTypeSequence;
+import edu.udel.cis.vsl.sarl.collections.IF.CollectionFactory;
+import edu.udel.cis.vsl.sarl.expr.common.CommonSymbolicExpression;
+import edu.udel.cis.vsl.sarl.preuniverse.PreUniverses;
+import edu.udel.cis.vsl.sarl.preuniverse.IF.FactorySystem;
+import edu.udel.cis.vsl.sarl.preuniverse.IF.PreUniverse;
+import edu.udel.cis.vsl.sarl.type.IF.SymbolicTypeFactory;
+
+public class ExpressionSubstituteTest {
+
+	private static PreUniverse universe;
+
+	private static SymbolicType realType, integerType, booleanType;
+	
+	private static CollectionFactory factory1;
+	
+	private static SymbolicTypeFactory typeFactory1;
+	
+	private static ExpressionSubstituter expr1;
+	
+	@BeforeClass
+	public static void setUpBeforeClass() throws Exception {
+		FactorySystem test = PreUniverses.newIdealFactorySystem();
+		universe = new CommonPreUniverse(test);
+		factory1 = test.collectionFactory();
+		typeFactory1 = test.typeFactory();
+
+		// initializing symbolic types
+		integerType = universe.integerType();
+		booleanType = universe.booleanType();
+		realType = universe.realType();
+		
+		expr1 = new ExpressionSubstituter(universe,factory1,typeFactory1);
+
+		
+
+	}
+
+	@AfterClass
+	public static void tearDownAfterClass() throws Exception {
+	}
+
+	@Before
+	public void setUp() throws Exception {
+	}
+
+	@After
+	public void tearDown() throws Exception {
+	}
+
+	
+	@Test
+	public void expressionSubstituteTest() {
+		
+		
+		
+	}
+
+	
+	
+
+}
