1f6d2ae05cf4268d7ae8b935a1415ff2fcaa4b9a
danfried <danfried@7d651925-2d04-6a0e-8c93-cd1e20bcaab1>
2013-10-30 03:53:16 +0000

    incorporated DividesTest in suite scope

--- test/edu/udel/cis/vsl/sarl/ideal/simplify/IdealSimplifySuite.java
+++ test/edu/udel/cis/vsl/sarl/ideal/simplify/IdealSimplifySuite.java
@@ -5,7 +5,7 @@ import org.junit.runners.Suite;
 import org.junit.runners.Suite.SuiteClasses;
 
 @RunWith(Suite.class)
-@SuiteClasses({ SimpBoolTest.class, SimplifierIntervalTest.class, IdealSimplifierBBTest.class, SimplifyFPTest.class, IdealSimplifierTest.class,IdealSimplifierSimpExprTest.class, SimplifyEqualsZeroTest.class })
+@SuiteClasses({ SimpBoolTest.class, SimplifierIntervalTest.class, IdealSimplifierBBTest.class, SimplifyFPTest.class, IdealSimplifierTest.class,IdealSimplifierSimpExprTest.class, SimplifyEqualsZeroTest.class, DividesTest.class })
 public class IdealSimplifySuite {
 
 }
