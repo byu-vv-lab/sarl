58833d797d750811e2216f0699d183691386d713
jleczner <jleczner@7d651925-2d04-6a0e-8c93-cd1e20bcaab1>
2013-10-30 03:31:49 +0000

    implemented CVC4 valid and part of translateType

--- src/edu/udel/cis/vsl/sarl/prove/cvc/CVC4TheoremProver.java
+++ src/edu/udel/cis/vsl/sarl/prove/cvc/CVC4TheoremProver.java
@@ -1,22 +1,35 @@
 //package edu.udel.cis.vsl.sarl.prove.cvc;
 //
 //import java.io.PrintStream;
+//import java.util.Arrays;
+//import java.util.HashMap;
+//import java.util.LinkedList;
+//import java.util.List;
+//import java.util.Map;
 //
-///*
 //import edu.nyu.acsys.CVC4.Expr;
 //import edu.nyu.acsys.CVC4.ExprManager;
 //import edu.nyu.acsys.CVC4.Kind;
+//import edu.nyu.acsys.CVC4.Result;
 //import edu.nyu.acsys.CVC4.Rational;
 //import edu.nyu.acsys.CVC4.SmtEngine;
-//*/
+//import edu.nyu.acsys.CVC4.Type;
+//import edu.nyu.acsys.CVC4.Exception;
+//import edu.nyu.acsys.CVC4.vectorType;
 //import edu.udel.cis.vsl.sarl.IF.SARLInternalException;
 //import edu.udel.cis.vsl.sarl.IF.ValidityResult;
 //import edu.udel.cis.vsl.sarl.IF.expr.BooleanExpression;
 //import edu.udel.cis.vsl.sarl.IF.expr.SymbolicExpression;
+//import edu.udel.cis.vsl.sarl.IF.type.SymbolicArrayType;
+//import edu.udel.cis.vsl.sarl.IF.type.SymbolicCompleteArrayType;
+//import edu.udel.cis.vsl.sarl.IF.type.SymbolicFunctionType;
+//import edu.udel.cis.vsl.sarl.IF.type.SymbolicTupleType;
+//import edu.udel.cis.vsl.sarl.IF.type.SymbolicType;
+//import edu.udel.cis.vsl.sarl.IF.type.SymbolicTypeSequence;
+//import edu.udel.cis.vsl.sarl.IF.type.SymbolicUnionType;
+//import edu.udel.cis.vsl.sarl.IF.type.SymbolicType.SymbolicTypeKind;
 //import edu.udel.cis.vsl.sarl.IF.object.IntObject;
 //import edu.udel.cis.vsl.sarl.IF.object.SymbolicObject;
-//import edu.udel.cis.vsl.sarl.IF.type.SymbolicArrayType;
-//import edu.udel.cis.vsl.sarl.IF.type.SymbolicType;
 //import edu.udel.cis.vsl.sarl.collections.IF.SymbolicCollection;
 //import edu.udel.cis.vsl.sarl.preuniverse.IF.PreUniverse;
 //import edu.udel.cis.vsl.sarl.prove.IF.TheoremProver;
@@ -27,20 +40,31 @@
 //	private BooleanExpression context;
 //	private PrintStream out = null;
 //	private boolean showProverQueries = false;
-//	//private ExprManager em = new ExprManager();
-//	//private SmtEngine smt = new SmtEngine(em);
-//	//private Expr cvcAssumption;
+//	private ExprManager em = new ExprManager();
+//	private SmtEngine smt = new SmtEngine(em);
+//	private Expr cvcAssumption;
+//
+//	/**
+//	 * Mapping of SARL symbolic type to corresponding CVC3 type. Set in method
+//	 * reset().
+//	 */
+//	private Map<SymbolicType, Type> typeMap = new HashMap<SymbolicType, Type>();
 //
 //	CVC4TheoremProver(PreUniverse universe, BooleanExpression context) {
 //		assert universe != null;
 //		assert context != null;
 //		this.universe = universe;
 //		this.context = context;
-//		//cvcAssumption = translate(context);
-//		//smt.assertFormula(cvcAssumption);
+//		cvcAssumption = translate(context);
+//		smt.assertFormula(cvcAssumption); 
 //	}
 //
-//	
+//	public Expr translate(SymbolicExpression expr) {
+//		Expr result;
+//		result = translateWork(expr);
+//		return result;
+//	}
+//
 //	/**
 //	 * Symbolic expressions of incomplete array type are represented by ordered
 //	 * pairs (length, array). This method tells whether the given symbolic
@@ -73,10 +97,10 @@
 //	 *            CVC4 Expr of bigArray
 //	 * @return length of CVC4 Expr of incomplete array type
 //	 */
-////	private Expr bigArrayLength(Expr bigArray) {
-////		Expr constant = em.mkConst(new Rational(0));
-////		return em.mkExpr(Kind.TUPLE_SELECT, bigArray, constant);
-////	}
+//	private Expr bigArrayLength(Expr bigArray) {
+//		Expr constant = em.mkConst(new Rational(0));
+//		return em.mkExpr(Kind.TUPLE_SELECT, bigArray, constant);
+//	}
 //
 //	/**
 //	 * This methods takes any Expr that is of incomplete array type and returns
@@ -86,20 +110,10 @@
 //	 *            CVC4 Expr of bigArray
 //	 * @return value of CVC4 Expr of incomplete array type
 //	 */
-///*	private Expr bigArrayValue(Expr bigArray) {
+//	private Expr bigArrayValue(Expr bigArray) {
 //		Expr index = em.mkConst(new Rational(1));
 //		return em.mkExpr(Kind.TUPLE_SELECT, bigArray, index);
 //	}
-//	
-//	/**
-//	 * Translates a multiplication SymbolicExpression (a*b) into an 
-//	 * equivalent CVC4 multiplication Expr based upon number of 
-//	 * arguments given. 
-//	 * 
-//	 * @param expr
-//	 * 			a SARL SymbolicExpression of form a*b
-//	 * @return CVC4 Expr
-//	 */
 //
 //	private Expr translateMultiply(SymbolicExpression expr) {
 //		int numArgs = expr.numArguments();
@@ -120,12 +134,6 @@
 //		return result;
 //	}
 //
-//	/**
-//	 * Translates a SymbolicExpression of type (a || b) into an equivalent CVC4 Expr 
-//	 * @param expr
-//	 * @return CVC4 representation of expr
-//	 */
-//
 //	private Expr translateOr(SymbolicExpression expr) {
 //		int numArgs = expr.numArguments();
 //		Expr result;
@@ -142,17 +150,6 @@
 //					+ expr);
 //		return result;
 //	}
-//	
-//	/**
-//	 * Translates which operation to perform based upon the given 
-//	 * SymbolicExpression and the SymbolicOperator provided.  Depending 
-//	 * upon the number of arguments given, a different conditional will
-//	 * be executed.  The result will be a CVC4 Expr. 
-//	 * 
-//	 * @param expr
-//	 * 			a SymbolicExpression 
-//	 * @return
-//	 */
 //
 //	private Expr translateWork(SymbolicExpression expr) {
 //		int numArgs = expr.numArguments();
@@ -242,8 +239,8 @@
 //			if (exponent instanceof IntObject)
 //				result = em.mkExpr(Kind.POW,
 //						translate((SymbolicExpression) expr.argument(0)), em
-//								.mkConst(new Rational(((IntObject) exponent)
-//										.getInt())));
+//						.mkConst(new Rational(((IntObject) exponent)
+//								.getInt())));
 //			else
 //				result = em.mkExpr(Kind.POW,
 //						translate((SymbolicExpression) expr.argument(0)),
@@ -253,15 +250,15 @@
 //		case TUPLE_READ:
 //			result = em.mkExpr(Kind.TUPLE_SELECT,
 //					translate((SymbolicExpression) expr.argument(0)), em
-//							.mkConst(new Rational(
-//									((IntObject) expr.argument(1)).getInt())));
+//					.mkConst(new Rational(
+//							((IntObject) expr.argument(1)).getInt())));
 //			break;
 //		case TUPLE_WRITE:
 //			result = em.mkExpr(Kind.TUPLE_UPDATE,
 //					translate((SymbolicExpression) expr.argument(0)), em
-//							.mkConst(new Rational(
-//									((IntObject) expr.argument(1)).getInt())),
-//					translate((SymbolicExpression) expr.argument(2)));
+//					.mkConst(new Rational(
+//							((IntObject) expr.argument(1)).getInt())),
+//							translate((SymbolicExpression) expr.argument(2)));
 //			break;
 //		case SUBTRACT:
 //			result = em.mkExpr(Kind.MINUS,
@@ -273,27 +270,89 @@
 //			throw new SARLInternalException("unreachable");
 //		}
 //		return result;
-//
 //	}
-//	
-//	/**
-//	 * Translates expr from SARL to CVC4
-//	 */
-//	private Expr translate(SymbolicExpression expr) {
-//		Expr result;
-//		result = translateWork(expr);
+//
+//	public Type translateType(SymbolicType type) throws Exception
+//	{
+//		Type result = typeMap.get(type);
+//
+//		if (result != null)
+//			return result;
+//
+//		SymbolicTypeKind kind = type.typeKind();
+//
+//		switch (kind) {
+//
+//		case BOOLEAN:
+//			result = em.booleanType();
+//			break;
+//		case INTEGER:
+//			result = em.integerType();
+//			break;
+//		case REAL:
+//			result = em.realType();
+//			break;
+//		case ARRAY:
+//			result = em.mkArrayType(em.integerType(),
+//					translateType(((SymbolicArrayType) type).elementType()));
+//			if (!(type instanceof SymbolicCompleteArrayType))
+//				// tuple:<extent,array>
+//				result = em.mkTupleType((vectorType) Arrays
+//						.asList(em.integerType(), result));
+//			break;
+//			//		case TUPLE:
+//			//			result = em
+//			//					.tupleType(translateTypeSequence(((SymbolicTupleType) type)
+//			//							.sequence()));
+//			//			break;
+//			//		case FUNCTION:
+//			//			result = vc.funType(
+//			//					translateTypeSequence(((SymbolicFunctionType) type)
+//			//							.inputTypes()),
+//			//					translateType(((SymbolicFunctionType) type).outputType()));
+//			//			break;
+//			//		case UNION: {	
+//			//			SymbolicUnionType unionType = (SymbolicUnionType) type;
+//			//			List<String> constructors = new LinkedList<String>();
+//			//			List<List<String>> selectors = new LinkedList<List<String>>();
+//			//			List<List<Expr>> types = new LinkedList<List<Expr>>();
+//			//			SymbolicTypeSequence sequence = unionType.sequence();
+//			//			int index = 0;
+//			//
+//			//			for (SymbolicType t : sequence) {
+//			//				List<String> selectorList = new LinkedList<String>();
+//			//				List<Expr> typeList = new LinkedList<Expr>();
+//			//
+//			//				selectorList.add(selector(unionType, index));
+//			//				typeList.add(translateType(t).getExpr());
+//			//				selectors.add(selectorList);
+//			//				types.add(typeList);
+//			//				constructors.add(constructor(unionType, index));
+//			//				index++;
+//			//			}
+//			//			result = vc.dataType(unionType.name().getString(), constructors,
+//			//					selectors, types);
+//			//			break;
+//			//		}
+//		default:
+//			throw new RuntimeException("Unknown type: " + type);
+//		}
+//		typeMap.put(type, result);
 //		return result;
 //	}
-//	
+//
 //	@Override
 //	public PreUniverse universe() {
 //		return universe;
 //	}
 //
-//	@Override
-//	public ValidityResult valid(BooleanExpression predicate) {
-//		// TODO Auto-generated method stub
-//		return null;
+//	public ValidityResult valid(BooleanExpression symbolicPredicate)
+//	{
+//		Expr cvc3Predicate = translate(symbolicPredicate);
+//		Result result = smt.query(cvc3Predicate);
+//		smt.pop();
+//
+//		return (ValidityResult) result.asValidityResult();
 //	}
 //
 //	@Override
@@ -312,5 +371,4 @@
 //	public String toString() {
 //		return "CVC4TheoremProver";
 //	}
-//	
 //}
