3549484493564f705f9debc93537ec1a90118bc6
ncarucci <ncarucci@7d651925-2d04-6a0e-8c93-cd1e20bcaab1>
2013-10-30 02:40:37 +0000

    Committing tupleRead, tupleWrite, bigArrayLength and various other implementations.  Everything will be commented out in order to not break the coverage page.

--- src/edu/udel/cis/vsl/sarl/prove/cvc/CVC4TheoremProver.java
+++ src/edu/udel/cis/vsl/sarl/prove/cvc/CVC4TheoremProver.java
@@ -2,7 +2,6 @@ package edu.udel.cis.vsl.sarl.prove.cvc;
 
 import java.io.PrintStream;
 
-import cvc3.Expr;
 /*
 import edu.nyu.acsys.CVC4.Expr;
 import edu.nyu.acsys.CVC4.ExprManager;
@@ -10,33 +9,44 @@ import edu.nyu.acsys.CVC4.Kind;
 import edu.nyu.acsys.CVC4.Rational;
 import edu.nyu.acsys.CVC4.SmtEngine;
 */
+
 import edu.udel.cis.vsl.sarl.IF.SARLInternalException;
 import edu.udel.cis.vsl.sarl.IF.ValidityResult;
 import edu.udel.cis.vsl.sarl.IF.expr.BooleanExpression;
 import edu.udel.cis.vsl.sarl.IF.expr.SymbolicExpression;
+import edu.udel.cis.vsl.sarl.IF.object.IntObject;
+import edu.udel.cis.vsl.sarl.IF.object.SymbolicObject;
 import edu.udel.cis.vsl.sarl.collections.IF.SymbolicCollection;
 import edu.udel.cis.vsl.sarl.preuniverse.IF.PreUniverse;
 import edu.udel.cis.vsl.sarl.prove.IF.TheoremProver;
-
+/*
 public class CVC4TheoremProver implements TheoremProver {
 
 	private PreUniverse universe;
 	private BooleanExpression context;
 	private PrintStream out = null;
 	private boolean showProverQueries = false;
-/*	private ExprManager em = new ExprManager();
+	private ExprManager em = new ExprManager();
 	private SmtEngine smt = new SmtEngine(em);
 	private Expr cvcAssumption;
-*/
+
 	CVC4TheoremProver(PreUniverse universe, BooleanExpression context) {
 		assert universe != null;
 		assert context != null;
 		this.universe = universe;
 		this.context = context;
-/*		cvcAssumption = translate(context);
-		smt.assertFormula(cvcAssumption); */
+		cvcAssumption = translate(context);
+		smt.assertFormula(cvcAssumption);
+	
 	}
-/*
+
+	
+	private Expr bigArrayLength(Expr bigArray) {
+		Expr constant = em.mkConst(new Rational(0));
+		return em.mkExpr(Kind.TUPLE_SELECT, bigArray, constant);
+	}
+	
+
 	private Expr translateMultiply(SymbolicExpression expr) {
 		int numArgs = expr.numArguments();
 		Expr result;
@@ -56,11 +66,28 @@ public class CVC4TheoremProver implements TheoremProver {
 		return result;
 	}
 
+	private Expr translateOr(SymbolicExpression expr) {
+		int numArgs = expr.numArguments();
+		Expr result;
+
+		if (numArgs == 1)
+			// NEEDS TO BE ADDED FOR ONE ARG
+			return null;
+		else if (numArgs == 2)
+			result = em.mkExpr(Kind.OR,
+					translate((SymbolicExpression) expr.argument(0)),
+					translate((SymbolicExpression) expr.argument(1)));
+		else
+			throw new SARLInternalException("Wrong number of arguments to or: "
+					+ expr);
+		return result;
+	}
+
 	private Expr translateWork(SymbolicExpression expr) {
 		int numArgs = expr.numArguments();
 		Expr result;
-	
-			// NEED TO WORK ON
+
+		// NEED TO WORK ON
 		switch (expr.operator()) {
 		case ADD:
 			if (numArgs == 2)
@@ -68,10 +95,8 @@ public class CVC4TheoremProver implements TheoremProver {
 						translate((SymbolicExpression) expr.argument(0)),
 						translate((SymbolicExpression) expr.argument(1)));
 			else if (numArgs == 1)
-				result = null;
-			// result = em.mkExpr(Kind.PLUS,
-			// translateCollection((SymbolicCollection<?>) expr
-			// .argument(0)));
+				// NEEDS TO BE ADDED FOR ONE ARG
+				return null;
 			else
 				throw new SARLInternalException(
 						"Expected 1 or 2 arguments for ADD");
@@ -82,10 +107,8 @@ public class CVC4TheoremProver implements TheoremProver {
 						translate((SymbolicExpression) expr.argument(0)),
 						translate((SymbolicExpression) expr.argument(1)));
 			else if (numArgs == 1)
-				result = null;
-			// result = em.mkExpr(Kind.AND,
-			// translateCollection((SymbolicCollection<?>) expr
-			// .argument(0)));
+				// NEEDS TO BE ADDED FOR ONE ARG
+				return null;
 			else
 				throw new SARLInternalException(
 						"Expected 1 or 2 arguments for AND: " + expr);
@@ -103,6 +126,10 @@ public class CVC4TheoremProver implements TheoremProver {
 			result = em.mkExpr(Kind.DIVISION,
 					translate((SymbolicExpression) expr.argument(0)),
 					translate((SymbolicExpression) expr.argument(1)));
+			break;
+		case LENGTH:
+			result = bigArrayLength(translate((SymbolicExpression) expr
+					.argument(0)));
 			break;
 		case LESS_THAN:
 			result = em.mkExpr(Kind.LT,
@@ -131,18 +158,33 @@ public class CVC4TheoremProver implements TheoremProver {
 					translate((SymbolicExpression) expr.argument(0)));
 			break;
 		case OR:
-			if (numArgs == 1)
-				result = null;
-			// result = em.mkExpr(Kind.OR,
-			// translateCollection((SymbolicCollection<?>) expr
-			// .argument(0)));
-			else if (numArgs == 2)
-				result = em.mkExpr(Kind.OR,
-						translate((SymbolicExpression) expr.argument(0)),
-						translate((SymbolicExpression) expr.argument(1)));
+			result = translateOr(expr);
+			break;
+		case POWER: {
+			SymbolicObject exponent = expr.argument(1);
+
+			if (exponent instanceof IntObject)
+				result = em.mkExpr(Kind.POW,
+						translate((SymbolicExpression) expr.argument(0)), em
+								.mkConst(new Rational(((IntObject) exponent)
+										.getInt())));
 			else
-				throw new SARLInternalException(
-						"Wrong number of arguments for OR: " + expr);
+				result = em.mkExpr(Kind.POW,
+						translate((SymbolicExpression) expr.argument(0)),
+						translate((SymbolicExpression) exponent));
+			break;
+		}
+		case TUPLE_READ:
+			result = em.mkExpr(Kind.TUPLE_SELECT, 
+					translate((SymbolicExpression) expr.argument(0)),
+					em.mkConst(new Rational(((IntObject) expr.argument(1)).getInt())));
+			break;
+		case TUPLE_WRITE:
+			result = em.mkExpr(Kind.TUPLE_UPDATE,
+					translate((SymbolicExpression) expr.argument(0)),
+					em.mkConst(new Rational(((IntObject) expr.argument(1)).getInt())),
+					translate((SymbolicExpression) expr.argument(2)));
+			break;
 		case SUBTRACT:
 			result = em.mkExpr(Kind.MINUS,
 					translate((SymbolicExpression) expr.argument(0)),
@@ -153,15 +195,15 @@ public class CVC4TheoremProver implements TheoremProver {
 			throw new SARLInternalException("unreachable");
 		}
 		return result;
-	
+
 	}
 
 	private Expr translate(SymbolicExpression expr) {
 		Expr result;
-	//	result = translateWork(expr);
+		result = translateWork(expr);
 		return result;
 	}
-*/
+
 	@Override
 	public PreUniverse universe() {
 		return universe;
@@ -184,10 +226,10 @@ public class CVC4TheoremProver implements TheoremProver {
 		this.out = out;
 		showProverQueries = out != null;
 	}
-	
+
 	@Override
 	public String toString() {
 		return "CVC4TheoremProver";
 	}
-
-}
+	
+}*/
