44ba9170283052e19246e678976daab9b12261f5
zmanchun <zmanchun@7d651925-2d04-6a0e-8c93-cd1e20bcaab1>
2013-11-05 22:38:30 +0000

    @Ignore tag added since the test has some problem.

--- test/edu/udel/cis/vsl/sarl/ideal/simplify/DividesTest.java
+++ test/edu/udel/cis/vsl/sarl/ideal/simplify/DividesTest.java
@@ -1,4 +1,5 @@
-/* Copyright 2013 Stephen F. Siegel, University of Delaware
+/*
+ * Copyright 2013 Stephen F. Siegel, University of Delaware
  */
 package edu.udel.cis.vsl.sarl.ideal.simplify;
 
@@ -10,6 +11,7 @@ import java.util.ArrayList;
 import org.junit.After;
 import org.junit.Before;
 import org.junit.BeforeClass;
+import org.junit.Ignore;
 import org.junit.Test;
 
 import edu.udel.cis.vsl.sarl.IF.expr.BooleanExpression;
@@ -24,33 +26,35 @@ import edu.udel.cis.vsl.sarl.preuniverse.IF.FactorySystem;
 import edu.udel.cis.vsl.sarl.preuniverse.IF.PreUniverse;
 
 /**
- * Converted (failed) benchmark to allow analysis and submission for TRAC ticket on issues
- * Intended to perform performance analysis on polynomial-term expansion with "divides"
+ * Converted (failed) benchmark to allow analysis and submission for TRAC ticket
+ * on issues Intended to perform performance analysis on polynomial-term
+ * expansion with "divides"
+ * 
  * @author danfried
- *
+ * 
  */
 public class DividesTest {
-	
+
 	static long start, time;
-	
+
 	private static FactorySystem system;
 
 	private static PreUniverse preUniv;
-	
+
 	private static PrintStream out;
-	
+
 	private static BooleanExpression assumption, trueExpr, claim;
-	
+
 	private static IdealFactory idealFactory;
-	
+
 	private static IdealSimplifierFactory idealSimplifierFactory;
-	
+
 	private static IdealSimplifier idealSimplifier;
-	
-	//static NumericSymbolicConstant x, c, div;
-	
+
+	// static NumericSymbolicConstant x, c, div;
+
 	private static SymbolicType integerType;
-	
+
 	private static NumericExpression x, y, z, c, div, int3;
 
 	private static Polynomial divPoly;
@@ -110,132 +114,146 @@ public class DividesTest {
 	public void tearDown() throws Exception {
 	}
 
+	@Ignore
 	@Test
+	/** TODO The assumption "z==x/y (i.e. divide(x,y))" doesn't 
+	 * imply that 'z==x|y (i.e. divides(x, y))'.
+	 * Thus the assertion "assertEquals(z, idealSimplifier.apply(
+	 * preUniv.divides(y, x)))" fails.
+	 **/
 	public void test1() {
-		
+
 		/*
-		out.println("here:  " + preUniv.divides(div, x) + "  " 
-		+ preUniv.divides(div, x).type()); // + "  true? " + preUniv.divides(div, x)==trueExpr);
-		*/
+		 * out.println("here:  " + preUniv.divides(div, x) + "  " +
+		 * preUniv.divides(div, x).type()); // + "  true? " +
+		 * preUniv.divides(div, x)==trueExpr);
+		 */
 		assumption = preUniv.equals(z, preUniv.divide(x, y));
-		//assumption = preUniv.equals(preUniv.divide(x, y), z);
-		//assumption = preUniv.equals(preUniv.divides(div, x), preUniv.trueExpression()); //div == x * n
+		// assumption = preUniv.equals(preUniv.divide(x, y), z);
+		// assumption = preUniv.equals(preUniv.divides(div, x),
+		// preUniv.trueExpression()); //div == x * n
 		idealSimplifier = idealSimplifierFactory.newSimplifier(assumption);
-		
+
 		ArrayList<Polynomial> divCol = new ArrayList<Polynomial>();
-		
+
 		divCol.add(divPoly);
-		
-		//out.println(idealSimplifier.apply(divPoly));
-		//out.println(preUniv.divides(div, divPoly));
-		//out.println("true? : " + idealSimplifier.simplifyExpression(divPoly));
-		
-		//out.println("assumption: " + assumption);
-		//out.println("works???? " + idealSimplifier.apply(preUniv.divides(y, x)));
+
+		// out.println(idealSimplifier.apply(divPoly));
+		// out.println(preUniv.divides(div, divPoly));
+		// out.println("true? : " +
+		// idealSimplifier.simplifyExpression(divPoly));
+
+		// out.println("assumption: " + assumption);
+		// out.println("works???? " + idealSimplifier.apply(preUniv.divides(y,
+		// x)));
 		assertEquals(z, idealSimplifier.apply(preUniv.divides(y, x)));
-		//out.println(z == idealSimplifier.apply(preUniv.divides(y,x)));
-		//out.println(idealSimplifier.apply(preUniv.divide(x, y)));
-		
+		// out.println(z == idealSimplifier.apply(preUniv.divides(y,x)));
+		// out.println(idealSimplifier.apply(preUniv.divide(x, y)));
+
 		int size = 3;
-		
-		for(int i = 0; i < size; i++){
-			//divPoly = divPoly * (x + c)
-			//divPoly =  (Polynomial) preUniv.multiply(divPoly, x);
-			divPoly =  (Polynomial) preUniv.multiply(divPoly, preUniv.add(x, int3));
-			divCol.add((Polynomial)divPoly);
+
+		for (int i = 0; i < size; i++) {
+			// divPoly = divPoly * (x + c)
+			// divPoly = (Polynomial) preUniv.multiply(divPoly, x);
+			divPoly = (Polynomial) preUniv.multiply(divPoly,
+					preUniv.add(x, int3));
+			divCol.add((Polynomial) divPoly);
 		}
-		
-		for(Polynomial i : divCol){
+
+		for (Polynomial i : divCol) {
 			out.println(i + "  :  " + i.type() + "  :  " + i.degree());
-			//out.println(preUniv.divides(x, i));
-			out.println("apply:  " + idealSimplifier.apply((Polynomial)i));
+			// out.println(preUniv.divides(x, i));
+			out.println("apply:  " + idealSimplifier.apply((Polynomial) i));
 			out.println(preUniv.divide(i, x));
 			out.println("true? : " + idealSimplifier.simplifyExpression(i));
 			out.println();
 		}
-		
-		
-		/*for(NumericExpression i : divCol){
-			start = System.currentTimeMillis();
-			
-			out.print(i + " :  ");
-			idealSimplifier.apply((Polynomial)i);
-			
-			time = System.currentTimeMillis() - start;
-			out.println("  time: " + time);
-		}*/
-		
-		
-		
-		//claim = preUniv.
+
+		/*
+		 * for(NumericExpression i : divCol){ start =
+		 * System.currentTimeMillis();
+		 * 
+		 * out.print(i + " :  "); idealSimplifier.apply((Polynomial)i);
+		 * 
+		 * time = System.currentTimeMillis() - start; out.println("  time: " +
+		 * time); }
+		 */
+
+		// claim = preUniv.
 	}
-	
+
 	/**
-	 * Test on IdealSimplifier with increasing terms
-	 * of an integer-based polynomial when applying and evaluating 
-	 * "divides"
+	 * Test on IdealSimplifier with increasing terms of an integer-based
+	 * polynomial when applying and evaluating "divides"
 	 * 
 	 * @see IdealSimplifier
 	 */
+	@Ignore
 	@Test
-	public void dividBenchTest(){
+	/**
+	 * TODO This test fails because preUniv.divides(x, y) doesn't
+	 * necessarily equal to True.
+	 */
+	public void dividBenchTest() {
 		/*
-		out.println("here:  " + preUniv.divides(div, x) + "  " 
-		+ preUniv.divides(div, x).type()); // + "  true? " + preUniv.divides(div, x)==trueExpr);
-		*/
+		 * out.println("here:  " + preUniv.divides(div, x) + "  " +
+		 * preUniv.divides(div, x).type()); // + "  true? " +
+		 * preUniv.divides(div, x)==trueExpr);
+		 */
 		assumption = preUniv.equals(z, preUniv.divide(x, y));
-		//assumption = preUniv.equals(preUniv.divide(x, y), z);
-		//assumption = preUniv.equals(preUniv.divides(div, x), preUniv.trueExpression()); //div == x * n
+		// assumption = preUniv.equals(preUniv.divide(x, y), z);
+		// assumption = preUniv.equals(preUniv.divides(div, x),
+		// preUniv.trueExpression()); //div == x * n
 		idealSimplifier = idealSimplifierFactory.newSimplifier(assumption);
-		
+
 		ArrayList<Polynomial> divCol = new ArrayList<Polynomial>();
-		
+
 		divCol.add(divPoly);
-		
-		//out.println(idealSimplifier.apply(divPoly));
-		//out.println(preUniv.divides(div, divPoly));
-		//out.println("true? : " + idealSimplifier.simplifyExpression(divPoly));
-		
-		//out.println("assumption: " + assumption);
-		//out.println("works???? " + idealSimplifier.apply(preUniv.divides(y, x)));
-		//assertEquals(z, idealSimplifier.apply(preUniv.divides(y, x)));
-		//out.println(z == idealSimplifier.apply(preUniv.divides(y,x)));
-		//out.println(idealSimplifier.apply(preUniv.divide(x, y)));
-		
+
+		// out.println(idealSimplifier.apply(divPoly));
+		// out.println(preUniv.divides(div, divPoly));
+		// out.println("true? : " +
+		// idealSimplifier.simplifyExpression(divPoly));
+
+		// out.println("assumption: " + assumption);
+		// out.println("works???? " + idealSimplifier.apply(preUniv.divides(y,
+		// x)));
+		// assertEquals(z, idealSimplifier.apply(preUniv.divides(y, x)));
+		// out.println(z == idealSimplifier.apply(preUniv.divides(y,x)));
+		// out.println(idealSimplifier.apply(preUniv.divide(x, y)));
+
 		int size = 3;
-		
-		for(int i = 0; i < size; i++){
-			//divPoly = divPoly * (x + c)
-			//divPoly =  (Polynomial) preUniv.multiply(divPoly, x);
-			divPoly =  (Polynomial) preUniv.multiply(divPoly, preUniv.add(x, int3));
-			divCol.add((Polynomial)divPoly);
+
+		for (int i = 0; i < size; i++) {
+			// divPoly = divPoly * (x + c)
+			// divPoly = (Polynomial) preUniv.multiply(divPoly, x);
+			divPoly = (Polynomial) preUniv.multiply(divPoly,
+					preUniv.add(x, int3));
+			divCol.add((Polynomial) divPoly);
 		}
-		
-		for(Polynomial i : divCol){
+
+		for (Polynomial i : divCol) {
 			out.println(i + "  :  " + i.type() + "  :  " + i.degree());
-			//assertEquals(CommonObjects.onePxPxSqdP3x4th.type(), i.type());
-			//out.println(preUniv.divides(x, i));
-			out.println("apply:  " + idealSimplifier.apply((Polynomial)i));
+			// assertEquals(CommonObjects.onePxPxSqdP3x4th.type(), i.type());
+			// out.println(preUniv.divides(x, i));
+			out.println("apply:  " + idealSimplifier.apply((Polynomial) i));
 			assertEquals(preUniv.trueExpression(), preUniv.divides(i, y));
 			out.println(preUniv.divide(i, x));
 			out.println("true? : " + idealSimplifier.simplifyExpression(i));
 			out.println();
 		}
-		
-		
-		/*for(NumericExpression i : divCol){
-			start = System.currentTimeMillis();
-			
-			out.print(i + " :  ");
-			idealSimplifier.apply((Polynomial)i);
-			
-			time = System.currentTimeMillis() - start;
-			out.println("  time: " + time);
-		}*/
-		
-		
-		
-		//claim = preUniv.
+
+		/*
+		 * for(NumericExpression i : divCol){ start =
+		 * System.currentTimeMillis();
+		 * 
+		 * out.print(i + " :  "); idealSimplifier.apply((Polynomial)i);
+		 * 
+		 * time = System.currentTimeMillis() - start; out.println("  time: " +
+		 * time); }
+		 */
+
+		// claim = preUniv.
 	}
 
 }
