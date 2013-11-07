dce9a513725527590149d934fe320d5ff1454860
danfried <danfried@7d651925-2d04-6a0e-8c93-cd1e20bcaab1>
2013-11-07 02:14:12 +0000

    removed warning

--- test/edu/udel/cis/vsl/sarl/ideal/simplify/IdealSimplifierBBTest.java
+++ test/edu/udel/cis/vsl/sarl/ideal/simplify/IdealSimplifierBBTest.java
@@ -11,7 +11,6 @@ import org.junit.Before;
 import org.junit.BeforeClass;
 import org.junit.Test;
 
-import edu.udel.cis.vsl.sarl.IF.expr.NumericExpression;
 import edu.udel.cis.vsl.sarl.IF.expr.SymbolicExpression;
 
 /**
