ba46fed9384e8f85c9b4a63b49fba6247ebabf4c
revanthb <revanthb@7d651925-2d04-6a0e-8c93-cd1e20bcaab1>
2013-11-20 02:46:18 +0000

    Added java documentation for the class IdealDivideTest in edu.udel.cis.vsl.sarl.ideal package.

--- test/edu/udel/cis/vsl/sarl/ideal/IdealDivideTest.java
+++ test/edu/udel/cis/vsl/sarl/ideal/IdealDivideTest.java
@@ -43,8 +43,25 @@ import edu.udel.cis.vsl.sarl.preuniverse.IF.FactorySystem;
 import edu.udel.cis.vsl.sarl.type.IF.SymbolicTypeFactory;
 
 /**
- * IdealDivideTest tests methods found in the edu.udel.cis.vsl.sarl.ideal.common package using divisions
- * @author cboucher
+ * The class IdealDivideTest tests methods found in the edu.udel.cis.vsl.sarl.ideal.common package 
+ * using divisions among various combinations such as:
+ * 
+ * <ul>
+ * <li>Polynomial / Polynomial</li>
+ * <li>Polynomial / Monomial</li>
+ * <li>Monomial / Monomial</li>
+ * <li>Monomial / Monic</li>
+ * <li>Monimc / Monic</li>
+ * <li>Monic / PrimitivePower</li>
+ * <li>PrimitivePower / PrimitivePower</li>
+ * <li>Primitive / PrimitivePower</li>
+ * <li>Constant / Primitive</li>
+ * <li>RationalExpression / Polynomial</li>
+ * <li>RationalExpression / Monomial</li>
+ * <li>RationalExpression / Monic</li>
+ * <li>RationalExpression / Primitive</li>
+ * <li>RationalExpression / PrimitivePower</li>
+ * </ul>
  *
  */
 public class IdealDivideTest {
