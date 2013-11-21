d23bd99b33564d1da3ec0cfdac5af715d6cebef9
jthakkar <jthakkar@7d651925-2d04-6a0e-8c93-cd1e20bcaab1>
2013-11-21 22:28:45 +0000

    added tests and javadocs for CommonSymbolicRealTypeTest

--- test/edu/udel/cis/vsl/sarl/type/common/CommonSymbolicRealTypeTest.java
+++ test/edu/udel/cis/vsl/sarl/type/common/CommonSymbolicRealTypeTest.java
@@ -76,20 +76,40 @@ public class CommonSymbolicRealTypeTest {
 	public void testTypeComparator(){
 		assertEquals(typeComparator.compare(idealRealKind, idealRealKind2), 0);
 	}
-/*
+
+	/**
+	 * this test checks whether the variables are canonizable or not,
+	 * here they are not and return false.
+	 */
 	@Test
-	public void testCanonizeChildren() {
-		fail("Not yet implemented");
+	public void testCanonizeChildren() 
+	{
+//		System.out.println(idealRealKind.isCanonic());
+//		System.out.println(idealRealKind2.isCanonic());
+//		System.out.println(herbrandRealKind.isCanonic());
+//		System.out.println(floatRealKind.isCanonic());
+		assertEquals(idealRealKind.isCanonic(), floatRealKind.isCanonic());
+		assertEquals(idealRealKind2.isCanonic(), herbrandRealKind.isCanonic());
 	}
 
+	/**
+	 * this test aims check the output for the method getPureType and the PureType for 
+ 		different kind of variables should be different.
+	 */
 	@Test
-	public void testGetPureType() {
-		fail("Not yet implemented");
+	public void testGetPureType() 
+	{
+//		System.out.println(idealRealKind.getPureType());
+//		System.out.println(idealRealKind2.getPureType());
+//		System.out.println(floatRealKind.getPureType());
+//		System.out.println(herbrandRealKind.getPureType());
+		assertEquals(idealRealKind.getPureType(), idealRealKind2.getPureType());
+		assertNotEquals(floatRealKind.getPureType(), herbrandRealKind.getPureType());		
 	}
 	
 
 	
-*/
+
 
 	@Test
 	public void testToStringBuffer() {
