2b1757ccd9dd628a41e56562757ca993211b396a
schivi <schivi@7d651925-2d04-6a0e-8c93-cd1e20bcaab1>
2013-11-21 23:00:44 +0000

    added a Bench file for the purpose of benching canonic performance increases

---
+++ bench/edu/udel/cis/vsl/sarl/expr/LargeExpressionBenchesWithCanonic.java
@@ -0,0 +1,46 @@
+package edu.udel.cis.vsl.sarl.expr;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collection;
+
+import edu.udel.cis.vsl.sarl.IF.SymbolicUniverse;
+import edu.udel.cis.vsl.sarl.IF.expr.BooleanExpression;
+import edu.udel.cis.vsl.sarl.IF.type.SymbolicType;
+import edu.udel.cis.vsl.sarl.universe.Universes;
+
+public class LargeExpressionBenchesWithCanonic {
+
+	public static void main(String[] args) {
+		SymbolicUniverse sUniverse;
+		SymbolicType booleanType;
+		long start;
+		long end;
+		long mark;
+		int numexpr;
+		Collection<BooleanExpression> col1;
+		Collection<BooleanExpression> col2;
+		
+		numexpr = 250;
+		sUniverse = Universes.newIdealUniverse();
+		booleanType = sUniverse.booleanType();
+		BooleanExpression[] ExpressionList1 = {};
+		col1= new ArrayList<BooleanExpression>(Arrays.asList(ExpressionList1));
+		BooleanExpression[] ExpressionList2 = {};
+		col2= new ArrayList<BooleanExpression>(Arrays.asList(ExpressionList2));
+		for(int i = 0; i < numexpr; i++){
+			col1.add((BooleanExpression) sUniverse.symbolicConstant(sUniverse.stringObject(Integer.toString(i)), booleanType));
+		}
+		for(int i = 0; i < numexpr; i++){
+			col2.add((BooleanExpression) sUniverse.symbolicConstant(sUniverse.stringObject(Integer.toString(-i)), booleanType));
+		}
+		start = System.currentTimeMillis();
+			BooleanExpression s1 = sUniverse.and(col1);
+			BooleanExpression s2 = sUniverse.and(col2);
+			@SuppressWarnings("unused")
+			BooleanExpression s3 = sUniverse.or(s1,s2);
+		end = System.currentTimeMillis();
+		mark = end - start;
+		System.out.println(mark);
+			}
+}
