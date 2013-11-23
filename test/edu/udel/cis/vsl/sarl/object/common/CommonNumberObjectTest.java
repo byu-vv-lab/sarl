5f390feaeabf93fc8d128b3f44cea4b3fae2f163
siegel <siegel@7d651925-2d04-6a0e-8c93-cd1e20bcaab1>
2013-11-23 15:33:30 +0000

    Fixed CommonNumberObjectTest to use the number factory to create new numbers instead of trying to use "new".

--- test/edu/udel/cis/vsl/sarl/object/common/CommonNumberObjectTest.java
+++ test/edu/udel/cis/vsl/sarl/object/common/CommonNumberObjectTest.java
@@ -1,6 +1,8 @@
 package edu.udel.cis.vsl.sarl.object.common;
 
-import static org.junit.Assert.*;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
 
 import java.math.BigInteger;
 
@@ -12,14 +14,13 @@ import org.junit.runners.JUnit4;
 import edu.udel.cis.vsl.sarl.IF.number.IntegerNumber;
 import edu.udel.cis.vsl.sarl.IF.number.NumberFactory;
 import edu.udel.cis.vsl.sarl.IF.object.NumberObject;
-import edu.udel.cis.vsl.sarl.number.real.RealInteger;
 import edu.udel.cis.vsl.sarl.number.real.RealNumberFactory;
-import edu.udel.cis.vsl.sarl.number.real.RealRational;
 
 /**
  * Test class for CommonNumberObject
+ * 
  * @author jtirrell
- *
+ * 
  */
 @RunWith(JUnit4.class)
 public class CommonNumberObjectTest {
@@ -60,31 +61,33 @@ public class CommonNumberObjectTest {
 	 * Initialized to a CommonObjectFactory upon setUp
 	 */
 	CommonObjectFactory objectfactory;
-	
-	
-	// TODO: commenting out below. You should use a number Factory
-	// to create your RealIntegers.
-	
+
 	/**
-	 * Initializes realint, newrealint, realreational, newrealrational, zero, one, negint, realfactory
+	 * Initializes realint, newrealint, realreational, newrealrational, zero,
+	 * one, negint, realfactory
+	 * 
 	 * @throws Exception
 	 */
-	/*
 	@Before
 	public void setUp() throws Exception {
-		//must create RealIntegers through factory for hash codes to be equal
-		//should make RealInteger constructor private, or implement hash code.
+		// must create RealIntegers through factory for hash codes to be equal
+		// should make RealInteger constructor private, or implement hash code.
 		this.realfactory = new RealNumberFactory();
 		this.objectfactory = new CommonObjectFactory(this.realfactory);
-		this.realint =    new CommonNumberObject(new RealInteger(new BigInteger("12345678901234567890")));
-		this.newrealint = new CommonNumberObject(new RealInteger(new BigInteger("12345678901234567890")));
-		this.realrational = new CommonNumberObject(new RealRational(new BigInteger("1"), new BigInteger("2")));
-		this.newrealrational = new CommonNumberObject(new RealRational(new BigInteger("1"), new BigInteger("2")));
-		this.zero = new CommonNumberObject(new RealInteger(BigInteger.ZERO));
-		this.one = new CommonNumberObject(new RealInteger(BigInteger.ONE));
-		this.negint = new CommonNumberObject(new RealInteger(new BigInteger("-123")));
+		this.realint = new CommonNumberObject(
+				realfactory.integer(new BigInteger("12345678901234567890")));
+		this.newrealint = new CommonNumberObject(
+				realfactory.integer(new BigInteger("12345678901234567890")));
+		this.realrational = new CommonNumberObject(realfactory.rational(
+				new BigInteger("1"), new BigInteger("2")));
+		this.newrealrational = new CommonNumberObject(realfactory.rational(
+				new BigInteger("1"), new BigInteger("2")));
+		this.zero = new CommonNumberObject(realfactory.integer(BigInteger.ZERO));
+		this.one = new CommonNumberObject(realfactory.integer(BigInteger.ONE));
+		this.negint = new CommonNumberObject(
+				realfactory.integer(new BigInteger("-123")));
 	}
-*/
+
 	/**
 	 * Test computeHashCode() and hashCode()
 	 */
@@ -97,13 +100,17 @@ public class CommonNumberObjectTest {
 
 	@Test
 	public void testIntrinsicEquals() {
-		NumberObject num1 = this.objectfactory.numberObject(this.realfactory.number("1"));
-		NumberObject num2 = this.objectfactory.numberObject(this.realfactory.number("1"));
-		assertTrue(((CommonNumberObject)num1).intrinsicEquals((CommonNumberObject)num2));
+		NumberObject num1 = this.objectfactory.numberObject(this.realfactory
+				.number("1"));
+		NumberObject num2 = this.objectfactory.numberObject(this.realfactory
+				.number("1"));
+		assertTrue(((CommonNumberObject) num1)
+				.intrinsicEquals((CommonNumberObject) num2));
 	}
 
 	/**
-	 * Verifies that toString returns the correct string representation of CommonNumberObjects
+	 * Verifies that toString returns the correct string representation of
+	 * CommonNumberObjects
 	 */
 	@Test
 	public void testToString() {
@@ -112,16 +119,19 @@ public class CommonNumberObjectTest {
 	}
 
 	/**
-	 * Verifies that getNumber returns the correct Number representation of CommonNumberObjects
+	 * Verifies that getNumber returns the correct Number representation of
+	 * CommonNumberObjects
 	 */
 	@Test
 	public void testGetNumber() {
-		assertEquals("12345678901234567890", this.realint.getNumber().toString());
+		assertEquals("12345678901234567890", this.realint.getNumber()
+				.toString());
 		assertEquals("1/2", this.realrational.getNumber().toString());
 	}
 
 	/**
-	 * Verifies that signum returns 1 for CommonIntObjects with a positive value, -1 for negatives, 0 otherwise.
+	 * Verifies that signum returns 1 for CommonIntObjects with a positive
+	 * value, -1 for negatives, 0 otherwise.
 	 */
 	@Test
 	public void testSignum() {
@@ -131,7 +141,8 @@ public class CommonNumberObjectTest {
 	}
 
 	/**
-	 * Verifies that isZero returns true for CommonNumberObjects with a value of 0, and false otherwise
+	 * Verifies that isZero returns true for CommonNumberObjects with a value of
+	 * 0, and false otherwise
 	 */
 	@Test
 	public void testIsZero() {
@@ -140,7 +151,8 @@ public class CommonNumberObjectTest {
 	}
 
 	/**
-	 * Verifies that isOne returns true for CommonNumberObjects with a value of 1, and false otherwise
+	 * Verifies that isOne returns true for CommonNumberObjects with a value of
+	 * 1, and false otherwise
 	 */
 	@Test
 	public void testIsOne() {
@@ -149,7 +161,8 @@ public class CommonNumberObjectTest {
 	}
 
 	/**
-	 * Verifies that isInteger returns true if the CommonNumberObject represents an integer, false otherwise
+	 * Verifies that isInteger returns true if the CommonNumberObject represents
+	 * an integer, false otherwise
 	 */
 	@Test
 	public void testIsInteger() {
@@ -158,61 +171,67 @@ public class CommonNumberObjectTest {
 	}
 
 	/**
-	 * Verifies that isReal returns true if the CommonNumberObject represents a real(non-integer) number,
-	 * false otherwise
+	 * Verifies that isReal returns true if the CommonNumberObject represents a
+	 * real(non-integer) number, false otherwise
 	 */
 	@Test
 	public void testIsReal() {
 		assertFalse(this.realint.isReal());
 		assertTrue(this.realrational.isReal());
 	}
-	
+
 	/**
-	 * Verifies that compareTo returns 0 for equivalent CommonNumberObjects, <0 if the first is less than the 
-	 * second, and >0 if the first is greater than the second.
+	 * Verifies that compareTo returns 0 for equivalent CommonNumberObjects, <0
+	 * if the first is less than the second, and >0 if the first is greater than
+	 * the second.
 	 */
 	@Test
 	public void testCompareTo() {
 		assertEquals(0, this.realint.compareTo(this.newrealint));
-		assertTrue(0<this.realint.compareTo(this.zero));
-		assertTrue(0>this.zero.compareTo(this.realint));
-		assertFalse(0<this.zero.compareTo(this.realint));
+		assertTrue(0 < this.realint.compareTo(this.zero));
+		assertTrue(0 > this.zero.compareTo(this.realint));
+		assertFalse(0 < this.zero.compareTo(this.realint));
 	}
 
 	/**
-	 * Verifies that toStringBuffer returns the CommonNumberObject's value as a string buffer
+	 * Verifies that toStringBuffer returns the CommonNumberObject's value as a
+	 * string buffer
 	 */
 	@Test
 	public void testToStringBuffer() {
-		assertEquals("12345678901234567890", this.realint.toStringBuffer(false).toString());
+		assertEquals("12345678901234567890", this.realint.toStringBuffer(false)
+				.toString());
 		assertEquals("1/2", this.realrational.toStringBuffer(false).toString());
 	}
 
 	/**
-	 * Verifies that toStringBufferLong returns the CommonNumberObject's value as a string buffer
+	 * Verifies that toStringBufferLong returns the CommonNumberObject's value
+	 * as a string buffer
 	 */
 	@Test
 	public void testToStringBufferLong() {
-		assertEquals("12345678901234567890", this.realint.toStringBufferLong().toString());
+		assertEquals("12345678901234567890", this.realint.toStringBufferLong()
+				.toString());
 		assertEquals("1/2", this.realrational.toStringBufferLong().toString());
 	}
-	
+
 	/**
 	 * Test for CommonSymbolicObject.setOrder() and getOrder()
 	 */
 	@Test
 	public void testSetGetOrder() {
-		this.newrealint.setOrder( this.realfactory.oneRational() );
+		this.newrealint.setOrder(this.realfactory.oneRational());
 		assertEquals("1", this.newrealint.getOrder().toString());
 	}
-	
+
 	/**
 	 * Test for CommonSymbolicObject.equals()
 	 */
 	@Test
 	public void testEquals() {
-		assertFalse(this.objectfactory.intObject(1).equals(this.objectfactory.booleanObject(false)));
+		assertFalse(this.objectfactory.intObject(1).equals(
+				this.objectfactory.booleanObject(false)));
 		assertFalse(this.objectfactory.intObject(1).equals(1));
 	}
-	
+
 }
