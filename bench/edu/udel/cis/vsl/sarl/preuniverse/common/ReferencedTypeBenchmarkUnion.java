e2a863c2ffea463aa801bc40a01191880ca08e33
jpiane <jpiane@7d651925-2d04-6a0e-8c93-cd1e20bcaab1>
2013-12-03 20:42:22 +0000

    Committed shell for ReferencedTypeBenchmarkUnion.

---
+++ bench/edu/udel/cis/vsl/sarl/preuniverse/common/ReferencedTypeBenchmarkUnion.java
@@ -0,0 +1,15 @@
+package edu.udel.cis.vsl.sarl.preuniverse.common;
+
+import java.util.Arrays;
+
+import edu.udel.cis.vsl.sarl.IF.expr.ReferenceExpression;
+import edu.udel.cis.vsl.sarl.IF.object.IntObject;
+import edu.udel.cis.vsl.sarl.IF.type.SymbolicType;
+import edu.udel.cis.vsl.sarl.IF.type.SymbolicUnionType;
+import edu.udel.cis.vsl.sarl.preuniverse.PreUniverses;
+import edu.udel.cis.vsl.sarl.preuniverse.IF.FactorySystem;
+import edu.udel.cis.vsl.sarl.preuniverse.IF.PreUniverse;
+
+public class ReferencedTypeBenchmarkUnion {
+
+}
