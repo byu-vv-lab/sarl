1d884dce8183f1b0c04d61a3b0cd55260ce40f83
icwatk <icwatk@7d651925-2d04-6a0e-8c93-cd1e20bcaab1>
2013-12-03 23:22:07 +0000

    new multiply number benchmark with for loop

--- bench/edu/udel/cis/vsl/sarl/MultiplyNumberBenchmark.java
+++ bench/edu/udel/cis/vsl/sarl/MultiplyNumberBenchmark.java
@@ -25,8 +25,11 @@ public static void main(String args[]) {
 		
 		Number numberZero = factory.number("0");
 		Number numberOne = factory.integer("1"); 
-		long x = System.nanoTime();
-		factory.multiply(numberZero, numberOne); 
+		long x = System.nanoTime(); 
+		for(int i=0; i<999;i++){ 
+			factory.multiply(numberZero, numberOne); 
+
+		}
 		long y = System.nanoTime();
 
 		System.out.println(y-x);
@@ -36,78 +39,59 @@ public static void main(String args[]) {
 	
 	
 		
-		Number numberTwo = factory.integer("2");
-		long x2 = System.nanoTime();
-		factory.multiply(numberTwo, numberTwo); 
+		long x2 = System.nanoTime(); 
+		for(int i=0; i<9999;i++){ 
+			factory.multiply(numberZero, numberOne); 
+
+		}
 		long y2 = System.nanoTime();
 
 		System.out.println(y2-x2);
 
-	  
+		long x3 = System.nanoTime(); 
+		for(int i=0; i<99999;i++){ 
+			factory.multiply(numberZero, numberOne); 
 
-	
-	Number numberTen = factory.integer("10");
-	Number numberTwenty = factory.integer("20"); 
-	long x3 = System.nanoTime();
-	factory.multiply(numberTen, numberTwenty); 
-	long y3 = System.nanoTime();
+		}
+		long y3 = System.nanoTime();
 
-	System.out.println(y3-x3);
- 
-	
-	Number numberTwoThousand = factory.integer("2000");
-	Number numberThousand = factory.integer("1000"); 
-	long x4 = System.nanoTime();
-	factory.multiply(numberTwoThousand, numberThousand); 
-	long y4 = System.nanoTime();
+		System.out.println(y3-x3);
 
-	System.out.println(y4-x4);
+		
+		
+		long x4 = System.nanoTime(); 
+		for(int i=0; i<99999;i++){ 
+			factory.multiply(numberZero, numberOne); 
 
-	Number numberTenThousand = factory.integer("10000");
-	Number numberTwentyThousand = factory.integer("20000"); 
-	long x5 = System.nanoTime();
-	factory.multiply(numberTwentyThousand, numberTenThousand); 
-	long y5 = System.nanoTime();
+		}
+		long y4 = System.nanoTime();
 
-	System.out.println(y5-x5); 
-	
-	
-	Number numberTwoMillion = factory.integer("2000000");
-	Number numberOneMillion = factory.integer("1000000"); 
-	long x6 = System.nanoTime();
-	factory.multiply(numberTwoMillion, numberOneMillion); 
-	long y6 = System.nanoTime();
+		System.out.println(y4-x4); 
+		
+		
+		
+		long x5 = System.nanoTime(); 
+		for(int i=0; i<99999;i++){ 
+			factory.multiply(numberZero, numberOne); 
 
-	System.out.println(y6-x6); 
-	
-	
-	
-	Number numberTwoBillion = factory.integer("2000000000");
-	Number numberOneBillion = factory.integer("1000000000"); 
-	long x7 = System.nanoTime();
-	factory.multiply(numberTwoBillion, numberOneBillion); 
-	long y7 = System.nanoTime();
+		}
+		long y5 = System.nanoTime();
 
-	System.out.println(y7-x7); 
-	
-	
+		System.out.println(y5-x5); 
+		
+		
+		long x6 = System.nanoTime(); 
+		for(int i=0; i<99999;i++){ 
+			factory.multiply(numberZero, numberOne); 
 
-	Number numberTwoTrillion = factory.integer("2000000000000");
-	Number numberOneTrillion = factory.integer("1000000000000"); 
-	long x8 = System.nanoTime();
-	factory.multiply(numberTwoTrillion, numberOneTrillion); 
-	long y8 = System.nanoTime();
+		}
+		long y6 = System.nanoTime();
 
-	System.out.println(y8-x8); 
+		System.out.println(y6-x6);
 	
 	
-	Number numberTwoQuadrillion = factory.integer("2000000000000000");
-	Number numberOneQuadrillion = factory.integer("1000000000000000"); 
-	long x9 = System.nanoTime();
-	factory.multiply(numberTwoQuadrillion, numberOneQuadrillion); 
-	long y9 = System.nanoTime();
-
-	System.out.println(y9-x9);
+	
+	
 
 }  
 	
