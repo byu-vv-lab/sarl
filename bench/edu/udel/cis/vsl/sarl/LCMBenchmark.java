a84c9f943de6c07aaad9c4cea4d37a970c981fe5
icwatk <icwatk@7d651925-2d04-6a0e-8c93-cd1e20bcaab1>
2013-12-03 23:20:49 +0000

    lcm benchmark with looop

--- bench/edu/udel/cis/vsl/sarl/LCMBenchmark.java
+++ bench/edu/udel/cis/vsl/sarl/LCMBenchmark.java
@@ -19,18 +19,7 @@ public class LCMBenchmark {
 	
 	private static BigInteger bigThirty = new BigInteger("30"); 
 	private static BigInteger bigTwenty = new BigInteger("20"); 
-	private static BigInteger bigThousand = new BigInteger("1000");  
-	private static BigInteger bigTwoThousand = new BigInteger("2000"); 
-	private static BigInteger bigOneMillion = new BigInteger("1000000");
-	private static BigInteger bigTenMillion = new BigInteger("10000000");
-	private static BigInteger bigFiftyMillion = new BigInteger("50000000");
-	private static BigInteger bigOneHundredMillion = new BigInteger("100000000");
-	private static BigInteger bigOneBillion = new BigInteger("1000000000");
-	private static BigInteger bigFiveBillion = new BigInteger("5000000000");
-	private static BigInteger bigOneTrillion = new BigInteger("1000000000000");
-	private static BigInteger bigTenTrillion = new BigInteger("10000000000000");
-	private static BigInteger bigOneQuadrillion = new BigInteger("1000000000000000");
-	private static BigInteger bigTwoQuadrillion = new BigInteger("2000000000000000");
+	
 
 	
 public static void  main(String args[]){ 
@@ -38,83 +27,87 @@ public static void  main(String args[]){
 		
 		IntegerNumber numberThirty = factory.integer(bigThirty);
 		IntegerNumber numberTwenty = factory.integer(bigTwenty); 
-		long x = System.nanoTime();
-		factory.lcm(numberTwenty, numberThirty); 
+		long x = System.nanoTime(); 
+		for(int i=0; i< 999; i++){
+			factory.lcm(numberTwenty, numberThirty);  
+		}
 		long y = System.nanoTime();
 		
-		System.out.println(y-x);
+		System.out.println(y-x); 
+		
+		
+		long x2 = System.nanoTime(); 
+		for(int i=0; i< 9999; i++){
+			factory.lcm(numberTwenty, numberThirty);  
+		}
+		long y2 = System.nanoTime();
+		
+		System.out.println(y2-x2); 
+		
+		
+		long x3 = System.nanoTime(); 
+		for(int i=0; i< 9999; i++){
+			factory.lcm(numberTwenty, numberThirty);  
+		}
+		long y3 = System.nanoTime();
+		
+		System.out.println(y3-x3); 
+		
+		
+		
+		long x4 = System.nanoTime(); 
+		for(int i=0; i< 99999; i++){
+			factory.lcm(numberTwenty, numberThirty);  
+		}
+		long y4 = System.nanoTime();
+		
+		System.out.println(y4-x4); 
+		
+		
+		
+		long x5 = System.nanoTime(); 
+		for(int i=0; i< 999999; i++){
+			factory.lcm(numberTwenty, numberThirty);  
+		}
+		long y5 = System.nanoTime();
+		
+		System.out.println(y5-x5); 
+		
+		
+		long x6 = System.nanoTime(); 
+		for(int i=0; i< 9999999; i++){
+			factory.lcm(numberTwenty, numberThirty);  
+		}
+		long y6 = System.nanoTime();
+		
+		System.out.println(y6-x6); 
+		
+		
+		long x7 = System.nanoTime(); 
+		for(int i=0; i< 99999999; i++){
+			factory.lcm(numberTwenty, numberThirty);  
+		}
+		long y7 = System.nanoTime();
+		
+		System.out.println(y7-x7); 
+		
+		
+
+		long x8 = System.nanoTime(); 
+		for(int i=0; i< 999999999; i++){
+			factory.lcm(numberTwenty, numberThirty);  
+		}
+		long y8 = System.nanoTime();
+		
+		System.out.println(y8-x8);
+
+
+
 
 	
 	
 	
 
-	IntegerNumber numberThousand = factory.integer(bigThousand);
-	IntegerNumber numberTwoThousand = factory.integer(bigTwoThousand); 
-	long x2 = System.nanoTime();
-	 factory.lcm(numberThousand, numberTwoThousand); 
-	long y2 = System.nanoTime();
-
-	System.out.print(y2-x2);
-
-
-
-	
-	IntegerNumber numberOneMillion = factory.integer(bigOneMillion);
-	IntegerNumber numberTenMillion = factory.integer(bigTenMillion); 
-	long x3 = System.nanoTime();
-	
-	factory.lcm(numberOneMillion,numberTenMillion);
-	long y3 = System.nanoTime();
-
-	System.out.print(y3-x3);
-
-
-
-	
-	IntegerNumber numberFiftyMillion = factory.integer(bigFiftyMillion);
-	IntegerNumber numberHundredMillion = factory.integer(bigOneHundredMillion); 
-	long x4 = System.nanoTime();
-	
-	factory.lcm(numberFiftyMillion,numberHundredMillion);
-	long y4 = System.nanoTime();
-
-	System.out.print(y4-x4);
-
-
-
-	
-	IntegerNumber numberOneBillion = factory.integer(bigOneBillion);
-	IntegerNumber numberFiveBillion = factory.integer(bigFiveBillion); 
-	long x5 = System.nanoTime();
-	
-	factory.lcm(numberOneBillion,numberFiveBillion);
-	long y5 = System.nanoTime();
-
-	System.out.print(y5-x5);
-
-
-
-	
-	IntegerNumber numberOneTrillion = factory.integer(bigOneTrillion);
-	IntegerNumber numberTenTrillion = factory.integer(bigTenTrillion); 
-	long x6 = System.nanoTime();
-	
-	factory.lcm(numberOneTrillion,numberTenTrillion);
-	long y6 = System.nanoTime();
-
-	System.out.print(y6-x6);
-
-
-
-	
-	IntegerNumber numberOneQuadrillion = factory.integer(bigOneQuadrillion);
-	IntegerNumber numberTwoQuadrillion = factory.integer(bigTwoQuadrillion); 
-	long x7 = System.nanoTime();
-	
-	factory.lcm(numberOneQuadrillion,numberTwoQuadrillion);
-	long y7 = System.nanoTime();
-
-	System.out.print(y7-x7);
 
 }
 }
