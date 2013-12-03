3c105187cb0d4df0849637a7ef7a88ec18521178
mcmahoni <mcmahoni@7d651925-2d04-6a0e-8c93-cd1e20bcaab1>
2013-12-03 22:37:17 +0000

    Cleaned up ExponentiatorTest

--- test/edu/udel/cis/vsl/sarl/numbers/ExponentiatorTest.java
+++ test/edu/udel/cis/vsl/sarl/numbers/ExponentiatorTest.java
@@ -2,42 +2,26 @@ package edu.udel.cis.vsl.sarl.numbers;
 
 import static org.junit.Assert.assertEquals;
 
-//import java.math.BigInteger;
-
-
-
 import org.junit.Before;
 import org.junit.BeforeClass;
-//import org.junit.Ignore;
 import org.junit.Test;
 
 import edu.udel.cis.vsl.sarl.IF.number.IntegerNumber;
 import edu.udel.cis.vsl.sarl.IF.number.NumberFactory;
 import edu.udel.cis.vsl.sarl.number.Numbers;
 import edu.udel.cis.vsl.sarl.number.real.Exponentiator;
-//import edu.udel.cis.vsl.sarl.util.BinaryOperator;
 import edu.udel.cis.vsl.sarl.util.BinaryOperator;
 
 public class ExponentiatorTest {
 	
 	private static NumberFactory factory = Numbers.REAL_FACTORY;
-	//private static BigInteger bigTwo = new BigInteger("2");
-	//private static BigInteger bigThree = new BigInteger("3");
 	private static IntegerNumber inOne = factory.integer(1);
 	private static IntegerNumber inZero = factory.integer(0);
 	private static IntegerNumber inNegativeOne = factory.integer(-1);
 	private static IntegerNumber inTwo = factory.integer(2);
-	//private static IntegerNumber inThree = factory.integer(3);
-	//private static IntegerNumber inEight = factory.integer(8);
 	private static IntegerNumber inResult = factory.integer(9);
-
-	//private static Exponentiator<IntegerNumber> myXpo;
-	
-	
 	private BinaryOperator<IntegerNumber> multiplier;
 	private Exponentiator<IntegerNumber> myXpo = new Exponentiator<IntegerNumber>(multiplier , inOne);
-	
-	
 	
 	@BeforeClass
 	public static void setUpBeforeClass() throws Exception {
