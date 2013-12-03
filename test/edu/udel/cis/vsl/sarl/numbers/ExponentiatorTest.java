e5e3399f206ba30b3a3bbafc48ce26e021d36ee9
mcmahoni <mcmahoni@7d651925-2d04-6a0e-8c93-cd1e20bcaab1>
2013-12-03 22:06:12 +0000

    Added test for exponent of 1 in exponentiatorTest

--- test/edu/udel/cis/vsl/sarl/numbers/ExponentiatorTest.java
+++ test/edu/udel/cis/vsl/sarl/numbers/ExponentiatorTest.java
@@ -5,9 +5,10 @@ import static org.junit.Assert.assertEquals;
 //import java.math.BigInteger;
 
 
+
 import org.junit.Before;
 import org.junit.BeforeClass;
-import org.junit.Ignore;
+//import org.junit.Ignore;
 import org.junit.Test;
 
 import edu.udel.cis.vsl.sarl.IF.number.IntegerNumber;
@@ -15,6 +16,7 @@ import edu.udel.cis.vsl.sarl.IF.number.NumberFactory;
 import edu.udel.cis.vsl.sarl.number.Numbers;
 import edu.udel.cis.vsl.sarl.number.real.Exponentiator;
 //import edu.udel.cis.vsl.sarl.util.BinaryOperator;
+import edu.udel.cis.vsl.sarl.util.BinaryOperator;
 
 public class ExponentiatorTest {
 	
@@ -23,9 +25,17 @@ public class ExponentiatorTest {
 	//private static BigInteger bigThree = new BigInteger("3");
 	private static IntegerNumber inOne = factory.integer(1);
 	private static IntegerNumber inTwo = factory.integer(2);
-	private static IntegerNumber inThree = factory.integer(3);
-	private static IntegerNumber inEight = factory.integer(8);
-	private static Exponentiator<IntegerNumber> myXpo;
+	//private static IntegerNumber inThree = factory.integer(3);
+	//private static IntegerNumber inEight = factory.integer(8);
+	private static IntegerNumber inResult = factory.integer(9);
+
+	//private static Exponentiator<IntegerNumber> myXpo;
+	
+	
+	private BinaryOperator<IntegerNumber> multiplier;
+	private Exponentiator<IntegerNumber> myXpo = new Exponentiator<IntegerNumber>(multiplier , inOne);
+	
+	
 	
 	@BeforeClass
 	public static void setUpBeforeClass() throws Exception {
@@ -36,13 +46,14 @@ public class ExponentiatorTest {
 	}
 	
 	
-	@Ignore @Test
+	//@Ignore @Test
 	/* TODO This test fails because the variable myXpo is never
 	 * initialized before referenced.
 	 * */
-	public void ExpTest(){
-		inOne = myXpo.exp( inTwo , inThree );
-		assertEquals( inOne , inEight );
+	@Test
+	public void OneExpTest(){
+		inResult = myXpo.exp( inTwo , inOne );
+		assertEquals( inResult , inTwo );
 	}
 	
 }
