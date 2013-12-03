14f8edfb67df6aacfb3c691d02e4930150159212
ncarucci <ncarucci@7d651925-2d04-6a0e-8c93-cd1e20bcaab1>
2013-12-03 20:52:14 +0000

    Committing AndTest blackbox with CVC4TheoremProver commented out till it can be used.

---
+++ test/edu/udel/cis/vsl/sarl/prove/AndTest.java
@@ -0,0 +1,142 @@
+package edu.udel.cis.vsl.sarl.prove;
+
+import static org.junit.Assert.assertEquals;
+
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.LinkedList;
+import java.util.List;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Ignore;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.JUnit4;
+
+import edu.udel.cis.vsl.sarl.IF.SARLInternalException;
+import edu.udel.cis.vsl.sarl.IF.ValidityResult;
+import edu.udel.cis.vsl.sarl.IF.ValidityResult.ResultType;
+import edu.udel.cis.vsl.sarl.IF.expr.BooleanExpression;
+import edu.udel.cis.vsl.sarl.IF.expr.NumericExpression;
+import edu.udel.cis.vsl.sarl.IF.expr.NumericSymbolicConstant;
+import edu.udel.cis.vsl.sarl.IF.expr.SymbolicExpression.SymbolicOperator;
+import edu.udel.cis.vsl.sarl.IF.type.SymbolicType;
+import edu.udel.cis.vsl.sarl.collections.IF.SymbolicCollection;
+import edu.udel.cis.vsl.sarl.expr.IF.ExpressionFactory;
+import edu.udel.cis.vsl.sarl.preuniverse.PreUniverses;
+import edu.udel.cis.vsl.sarl.preuniverse.IF.FactorySystem;
+import edu.udel.cis.vsl.sarl.preuniverse.IF.PreUniverse;
+import edu.udel.cis.vsl.sarl.prove.IF.TheoremProver;
+
+@RunWith(JUnit4.class)
+public class AndTest {
+
+	// Static fields: instantiated once and used for all tests...
+
+	private static FactorySystem factorySystem = PreUniverses
+			.newIdealFactorySystem();
+
+	private static PreUniverse universe = PreUniverses
+			.newPreUniverse(factorySystem);
+
+	private static ExpressionFactory expressionFactory = factorySystem
+			.expressionFactory();
+
+	private static SymbolicType boolType = universe.booleanType();
+
+	private static SymbolicType integerType = universe.integerType();
+
+	private static BooleanExpression boolTrue = universe.trueExpression();
+
+	private static BooleanExpression boolFalse = universe.falseExpression();
+
+	private static NumericExpression five = universe.integer(5);
+
+	private static NumericSymbolicConstant x = (NumericSymbolicConstant) universe
+			.symbolicConstant(universe.stringObject("x"), integerType);
+
+	private static BooleanExpression context = universe.lessThan(x, five);
+
+	private static Collection<TheoremProver> provers;
+
+	@BeforeClass
+	public static void setUpBeforeClass() throws Exception {
+
+		provers = new LinkedList<TheoremProver>();
+
+		TheoremProver prover = Prove.newCVC3TheoremProverFactory(universe)
+				.newProver(context);
+
+		prover.setOutput(System.out); // for debugging
+		provers.add(prover);
+
+		// TODO: until this is working, don't commit it, as it causes
+		// subsequent tests to crash...
+
+//		TheoremProver cvc4prover = Prove.newCVC4TheoremProverFactory(universe)
+//				.newProver(context);
+//		cvc4prover.setOutput(System.out); // for debugging
+//		provers.add(cvc4prover);
+	}
+
+	@Before
+	public void setUp() throws Exception {
+	}
+
+	@After
+	public void tearDown() throws Exception {
+	}
+
+	/**
+	 * Checks that the result of applying the prover to the given predicate is
+	 * as expected.
+	 * 
+	 * @param expected
+	 *            expected result type (YES, NO, or MAYBE)
+	 * @param predicate
+	 *            boolean expression to be checked for validity
+	 */
+	private void check(ResultType expected, BooleanExpression predicate) {
+		for (TheoremProver prover : provers) {
+			assertEquals(prover.toString(), expected, prover.valid(predicate)
+					.getResultType());
+		}
+	}
+
+	@Test
+	@Ignore
+	public void testTranslateAndOneArg() {
+		List<BooleanExpression> s1 = new ArrayList<BooleanExpression>();
+		s1.add(boolFalse);
+		s1.add(boolTrue);
+
+		SymbolicCollection<BooleanExpression> boolList = universe
+				.basicCollection(s1);
+		BooleanExpression andExp = (BooleanExpression) expressionFactory
+				.expression(SymbolicOperator.AND, boolType, boolList);
+		for (TheoremProver prover : provers) {
+			ValidityResult result = prover.valid(andExp);
+			assertEquals(ResultType.NO, result.getResultType());
+		}
+	}
+	
+	@Test
+	public void translateTwoArgNotEqual() {
+		check(ResultType.NO, universe.and(boolTrue, boolFalse));
+	}
+
+	@Test
+	public void translateTwoArgEqual() {
+		check(ResultType.YES, universe.and(boolTrue, boolTrue));
+	}
+
+	@Test (expected = SARLInternalException.class)
+	public void translateThreeArg() {
+		BooleanExpression predicate = (BooleanExpression) expressionFactory
+				.expression(SymbolicOperator.AND, boolType, boolTrue, boolTrue,
+						boolFalse);
+		check(ResultType.NO, predicate);
+	}
+}
