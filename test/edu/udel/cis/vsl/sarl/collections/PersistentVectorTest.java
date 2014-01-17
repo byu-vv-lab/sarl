6eff8875a1c986122fd31bb1339759f27ba33e80
siegel <siegel@7d651925-2d04-6a0e-8c93-cd1e20bcaab1>
2014-01-17 03:01:48 +0000

    Simplifcation to persistent vector test revealing defect in clj-ds.

--- test/edu/udel/cis/vsl/sarl/collections/PersistentVectorTest.java
+++ test/edu/udel/cis/vsl/sarl/collections/PersistentVectorTest.java
@@ -1,12 +1,7 @@
 package edu.udel.cis.vsl.sarl.collections;
 
-import static org.junit.Assert.assertEquals;
-
 import java.util.Iterator;
 
-import org.junit.After;
-import org.junit.Before;
-import org.junit.BeforeClass;
 import org.junit.Test;
 
 import com.github.krukow.clj_ds.PersistentVector;
@@ -14,44 +9,24 @@ import com.github.krukow.clj_ds.Persistents;
 
 public class PersistentVectorTest {
 
-	@BeforeClass
-	public static void setUpBeforeClass() throws Exception {
-	}
-
-	@Before
-	public void setUp() throws Exception {
-	}
-
-	@After
-	public void tearDown() throws Exception {
-	}
-
 	private PersistentVector<Integer> makeVec(int n) {
 		PersistentVector<Integer> pv = Persistents.vector();
 
-		for (int i = 0; i < n; i++) {
+		for (int i = 0; i < n; i++)
 			pv = pv.plus(i);
-		}
 		return pv;
 	}
 
 	private void check(int n) {
-		PersistentVector<Integer> pv1 = makeVec(n), pv2 = makeVec(n);
-		Iterator<Integer> iter1 = pv1.iterator(), iter2 = pv2.iterator();
+		PersistentVector<Integer> pv = makeVec(n);
+		Iterator<Integer> iter = pv.iterator();
 
-		assertEquals(pv1.size(), pv2.size());
-		while (iter1.hasNext()) {
-			Integer a = iter1.next();
-			Integer b = iter2.next();
-
-			assertEquals(a, b);
-		}
+		for (int i = 0; i < n; i++)
+			System.out.println(iter.next());
 	}
 
 	@Test
 	public void test() {
-		for (int i = 0; i < 35; i++) {
-			check(i);
-		}
+		check(33);
 	}
 }
